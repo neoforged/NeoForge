4720811685bcb9b93c8498c27f6f7cce07f330c7
SizableShrimp <sizableshrimp@sizableshrimp.me>
2023-07-17 05:19:22 -0500

    Add `@CompileStatic` to task

--- buildSrc/src/main/groovy/net/minecraftforge/forge/tasks/FailGitChanges.groovy
+++ buildSrc/src/main/groovy/net/minecraftforge/forge/tasks/FailGitChanges.groovy
@@ -1,9 +1,11 @@
 package net.minecraftforge.forge.tasks
 
+import groovy.transform.CompileStatic
 import org.eclipse.jgit.api.Git
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
+@CompileStatic
 abstract class FailGitChanges extends DefaultTask {
     @TaskAction
     void run() {
