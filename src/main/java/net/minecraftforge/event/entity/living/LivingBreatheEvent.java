b5969c8bbf41b9b1cc2e62ca9be53dee3169cb11
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2023-07-23 16:31:31 -0700

    Major fixes for LivingBreathe/DrownEvent and other misc fixes for upstream code.
    * Fixed erroneous LivingEntity.java.patch line.
    * Fixed formatting for ForgeHooks.URL_PATTERN.
    * Added documentation for LivingBreatheEvent and LivingDrownEvent
    * Fixed an issue where the LivingBreatheEvent's implementation caused you to gain air while underwater.
    * Fixed an issue where the LivingBreatheEvent's implementation caused entities to not be dismounted in water.
    * Expanded the functionality of LivingDrownEvent to allow for controlling the damage amount and particle count.
    * Sorted new exc entries.
    * Minor style fixes.

--- src/main/java/net/minecraftforge/event/entity/living/LivingBreatheEvent.java
+++ src/main/java/net/minecraftforge/event/entity/living/LivingBreatheEvent.java
@@ -7,21 +7,20 @@ package net.minecraftforge.event.entity.living;
 
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.common.ForgeHooks;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.Cancelable;
 
 /**
  * LivingBreatheEvent is fired whenever a living entity ticks.<br>
  * <br>
- * This event is fired via the {@link ForgeHooks#onLivingBreathe(LivingEntity, int, int)}.<br>
- * <br>
- * {@link #canBreathe()} contains if this entity can breathe.<br>
- * {@link #getConsumeAirAmount()} contains the amount of air that will be consumed if this entity can't breathe.<br>
- * {@link #getRefillAirAmount()} contains the amount of air that will be refilled if this entity can breathe.<br>
+ * This event is fired via {@link ForgeHooks#onLivingBreathe(LivingEntity, int, int)}.<br>
  * <br>
  * This event is not {@link Cancelable}.<br>
  * <br>
  * This event does not have a result. {@link HasResult}
- **/
+ * <br>
+ * This event is fired on {@link MinecraftForge#EVENT_BUS}
+ */
 public class LivingBreatheEvent extends LivingEvent
 {
     private boolean canBreathe;
@@ -36,31 +35,57 @@ public class LivingBreatheEvent extends LivingEvent
         this.refillAirAmount = Math.max(refillAirAmount, 0);
     }
 
+    /**
+     * If the entity can breathe, their air value will be increased by {@link #getRefillAirAmount()}.<br>
+     * If the entity cannot breathe, their air value will be reduced by {@link #getConsumeAirAmount()}.
+     * @return True if the entity can breathe
+     */
     public boolean canBreathe()
     {
         return canBreathe;
     }
 
+    /**
+     * Sets if the entity can breathe or not.
+     * @param canBreathe The new value.
+     */
     public void setCanBreathe(boolean canBreathe)
     {
         this.canBreathe = canBreathe;
     }
 
+    /**
+     * @return The amount the entity's {@linkplain LivingEntity#getAirSupply() air supply} will be reduced by if the entity {@linkplain #canBreathe() cannot breathe}.
+     */
     public int getConsumeAirAmount()
     {
         return consumeAirAmount;
     }
 
+    /**
+     * Sets the new consumed air amount.
+     * @param consumeAirAmount The new value.
+     * @see #getConsumeAirAmount()
+     */
     public void setConsumeAirAmount(int consumeAirAmount)
     {
         this.consumeAirAmount = Math.max(consumeAirAmount, 0);
     }
 
+    /**
+     * @return The amount the entity's {@linkplain LivingEntity#getAirSupply() air supply} will be increased by if the entity {@linkplain #canBreathe() can breathe}.
+     */
     public int getRefillAirAmount()
     {
         return refillAirAmount;
     }
 
+    /**
+     * Sets the new refilled air amount.
+     * 
+     * @param refillAirAmount The new value.
+     * @see #getRefillAirAmount()
+     */
     public void setRefillAirAmount(int refillAirAmount)
     {
         this.refillAirAmount = Math.max(refillAirAmount, 0);
