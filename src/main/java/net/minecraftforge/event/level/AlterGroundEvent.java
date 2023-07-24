a80d50a02219e494c79e19f32b521b8f13673796
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2023-07-23 19:29:58 -0700

    Re-implement AlterGroundEvent in a more performant way
    This change makes the event fire once per invocation of the AlterGroundDecorator, where it may change the supplied BlockStateProvider (which is then used many times), instead of firing once per altered blockpos.

--- src/main/java/net/minecraftforge/event/level/AlterGroundEvent.java
+++ src/main/java/net/minecraftforge/event/level/AlterGroundEvent.java
@@ -5,89 +5,114 @@
 
 package net.minecraftforge.event.level;
 
+import java.util.Collections;
 import java.util.ConcurrentModificationException;
+import java.util.List;
+import java.util.Objects;
 
 import org.jetbrains.annotations.ApiStatus;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.LevelSimulatedReader;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 import net.minecraft.world.level.levelgen.feature.treedecorators.AlterGroundDecorator;
 import net.minecraft.world.level.levelgen.feature.treedecorators.TreeDecorator;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.ForgeEventFactory;
 import net.minecraftforge.eventbus.api.Cancelable;
 import net.minecraftforge.eventbus.api.Event;
 
 /**
- * This event is fired when {@link AlterGroundDecorator#placeBlockAt(TreeDecorator.Context, BlockPos)}
- * attempts to alter a ground block when generating a feature. An example of this would be large spruce trees converting grass blocks into podzol.
+ * This event is fired when {@link AlterGroundDecorator#placeBlockAt(TreeDecorator.Context, BlockPos)} attempts to alter a ground block when generating a feature.<br>
+ * An example of this would be large spruce trees converting grass blocks into podzol.
  * <p>
  * This event is not {@linkplain Cancelable cancellable}.
  * <p>
- * This event is fired on the {@linkplain MinecraftForge#EVENT_BUS main Forge event bus}
- * only on the {@linkplain net.minecraftforge.fml.LogicalSide#SERVER logical server}.
+ * This event is fired on the {@linkplain MinecraftForge#EVENT_BUS main Forge event bus} only on the {@linkplain net.minecraftforge.fml.LogicalSide#SERVER logical server}.
  * <p>
  * This event is fired on worker threads, meaning it is unsafe to access external global state.<br>
  * Doing so may induce {@link ConcurrentModificationException} or deadlocks.
  */
 public class AlterGroundEvent extends Event
 {
-    private final LevelSimulatedReader level;
-    private final RandomSource random;
-    private final BlockPos pos;
-    private final BlockState originalAltered;
-    private BlockState newAltered;
+    private final TreeDecorator.Context ctx;
+    private final List<BlockPos> positions;
 
+    private StateProvider provider;
+
+    /**
+     * @see {@link ForgeEventFactory#alterGround} as the API endpoint for firing this event.
+     */
     @ApiStatus.Internal
-    public AlterGroundEvent(LevelSimulatedReader level, RandomSource random, BlockPos pos, BlockState altered)
+    public AlterGroundEvent(TreeDecorator.Context ctx, List<BlockPos> positions, StateProvider provider)
     {
-        this.level = level;
-        this.random = random;
-        this.pos = pos;
-        this.originalAltered = altered;
-        this.newAltered = altered;
-    }
-
-    public LevelSimulatedReader getLevel()
-    {
-        return this.level;
-    }
-
-    public RandomSource getRandom()
-    {
-        return this.random;
+        this.ctx = ctx;
+        this.positions = Collections.unmodifiableList(positions);
+        this.provider = provider;
     }
 
     /**
-     * {@return the position of the block that will be altered}
+     * Gets the tree decoration context for the current alteration.
      */
-    public BlockPos getPos()
+    public TreeDecorator.Context getContext()
     {
-        return this.pos;
+        return this.ctx;
     }
 
     /**
-     * {@return the original block state that would be placed by the ground decorator}
+     * The list of positions that are considered roots is different depending on if the context has roots, logs, or both.
+     * <p>
+     * If {@linkplain TreeDecorator.Context#roots() roots} are not present, this list is equivalent to the {@linkplain TreeDecorator.Context#logs() logs}.<br>
+     * If there are roots, and the roots have the same y-level as the lowest log, both this list is the union of both lists.<br>
+     * Otherwise, this list is equal to only the roots.
+     * <p>
+     * In either case, only positions which match the y-level of the zeroth element will be used during placement.
+     * <p>
+     * This list is immutable.
+     * 
+     * @return The list of positions that will be used for alteration placement.
      */
-    public BlockState getOriginalAlteredState()
+    public List<BlockPos> getPositions()
     {
-        return this.originalAltered;
+        return positions;
     }
 
     /**
-     * {@return the new block state to be placed by the ground decorator}
+     * Gets the current {@link BlockStateProvider} that will be used by the {@link AlterGroundDecorator}.
+     * 
+     * @return The (possibly event-modified) state provider.
      */
-    public BlockState getNewAlteredState()
+    public StateProvider getStateProvider()
     {
-        return this.newAltered;
+        return this.provider;
     }
 
     /**
-     * @param newAltered the new block state to be placed by the ground decorator
+     * Sets the {@link BlockStateProvider} that will be used by the {@link AlterGroundDecorator}.<br>
+     * Because this may be modified by multiple mods, it is advisable to wrap {@linkplain #getStateProvider() the current provider}.
+     * <p>
+     * An example of wrapping the current provider is shown below: <code><pre>
+     * StateProvider old = event.getStateProvider();
+     * event.setStateProvider((rand, pos) -> {
+     *     BlockState state = old.getState(rand, pos);
+     *     return state.is(Blocks.PODZOL) ? Blocks.REDSTONE_BLOCK.defaultBlockState() : state;
+     * });
+     * </pre></code>
+     * 
+     * @param provider The new state provider.
      */
-    public void setNewAlteredState(BlockState newAltered)
+    public void setStateProvider(StateProvider provider)
     {
-        this.newAltered = newAltered;
+        this.provider = Objects.requireNonNull(provider);
+    }
+
+    @FunctionalInterface
+    public static interface StateProvider
+    {
+        /**
+         * Gets the BlockState that will be placed at the passed position.
+         */
+        public BlockState getState(RandomSource random, BlockPos state);
     }
 }
