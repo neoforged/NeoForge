50fb332a063ad250c8bbf7399adca591bec6c59d
AterAnimAvis <AterAnimAvis@gmail.com>
2023-08-01 14:11:55 +0100

    Add Javadoc, Improve Test Mod
    Signed-off-by: AterAnimAvis <AterAnimAvis@gmail.com>

--- src/test/java/net/minecraftforge/debug/entity/player/PlayerSpawnPhantomsEventTest.java
+++ src/test/java/net/minecraftforge/debug/entity/player/PlayerSpawnPhantomsEventTest.java
@@ -10,6 +10,12 @@ import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+/**
+ * This tests for {@link PlayerSpawnPhantomsEventTest} and fires when the normal Phantom Spawns could occur.
+ * In Game use, `/time set night`, then wait until natural spawns occur.
+ * This test modifies the number of spawns to 20 as well as forcing it to happen regardless of the players last sleep time.
+ * Vanilla has a minimum of 3 days since last sleep before a maximum of 4 (on hard mode) phantoms can spawn
+ */
 @Mod("player_spawn_phantoms_event_test")
 @Mod.EventBusSubscriber()
 public class PlayerSpawnPhantomsEventTest
@@ -21,7 +27,8 @@ public class PlayerSpawnPhantomsEventTest
     public static void onPhantomsSpawn(PlayerSpawnPhantomsEvent event)
     {
         if (!ENABLE) return;
-        event.setResult(Event.Result.DENY);
+        event.setPhantomsToSpawn(20);
+        event.setResult(Event.Result.ALLOW);
     }
 
 }
