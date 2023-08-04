e46c19d5a22fdeea3ce1393986d7f1c787cdda69
Joe <burtonjae@hotmail.co.uk>
2023-08-04 23:31:56 +0100

    Fix licenses

--- src/main/java/net/minecraftforge/event/entity/living/VillagerEvent.java
+++ src/main/java/net/minecraftforge/event/entity/living/VillagerEvent.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) NeoForged and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
 package net.minecraftforge.event.entity.living;
 
 import net.minecraft.world.entity.npc.Villager;
