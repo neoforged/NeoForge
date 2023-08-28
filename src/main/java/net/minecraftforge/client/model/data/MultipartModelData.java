0789da2b9f3bde048a82ff4f3b5a42abf24e8379
Technici4n <13494793+Technici4n@users.noreply.github.com>
2023-08-28 21:43:44 +0200

    Call `getModelData` on multipart submodels (#118)

--- src/main/java/net/minecraftforge/client/model/data/MultipartModelData.java
+++ src/main/java/net/minecraftforge/client/model/data/MultipartModelData.java
@@ -6,13 +6,25 @@
 package net.minecraftforge.client.model.data;
 
 import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.world.level.block.state.BlockState;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.BitSet;
 import java.util.IdentityHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 
+@ApiStatus.Internal
 public class MultipartModelData
 {
+    // Next BC window: don't remove but make private and change the type to ModelProperty<Map<BakedModel, ModelData>>.
+    @Deprecated(forRemoval = true)
     public static final ModelProperty<MultipartModelData> PROPERTY = new ModelProperty<>();
 
     private final Map<BakedModel, ModelData> partData;
@@ -22,6 +34,7 @@ public class MultipartModelData
         this.partData = partData;
     }
 
+    @Deprecated(forRemoval = true)
     @Nullable
     public ModelData get(BakedModel model)
     {
@@ -44,11 +57,38 @@ public class MultipartModelData
         return partData != null ? partData : modelData;
     }
 
+    public static ModelData create(List<Pair<Predicate<BlockState>, BakedModel>> selectors, BitSet bitset, BlockAndTintGetter level, BlockPos pos, BlockState state, ModelData tileModelData)
+    {
+        // Don't allocate memory if no submodel changes the model data
+        Map<BakedModel, ModelData> dataMap = null;
+
+        for (int i = 0; i < bitset.length(); ++i)
+        {
+            if (bitset.get(i))
+            {
+                var model = selectors.get(i).getRight();
+                var data = model.getModelData(level, pos, state, tileModelData);
+
+                if (data != tileModelData)
+                {
+                    if (dataMap == null)
+                        dataMap = new IdentityHashMap<>();
+
+                    dataMap.put(model, data);
+                }
+            }
+        }
+
+        return dataMap == null ? tileModelData : tileModelData.derive().with(PROPERTY, new MultipartModelData(dataMap)).build();
+    }
+
+    @Deprecated(forRemoval = true)
     public static Builder builder()
     {
         return new Builder();
     }
 
+    @Deprecated(forRemoval = true)
     public static final class Builder
     {
         private final Map<BakedModel, ModelData> partData = new IdentityHashMap<>();
