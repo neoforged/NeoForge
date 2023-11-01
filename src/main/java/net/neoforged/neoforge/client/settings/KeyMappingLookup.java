82959639d2404cde197749b095ff7cb77557e07c
Su5eD <su5ed@outlook.com>
2023-11-01 14:43:50 +0100

    Side effects suck

--- src/main/java/net/neoforged/neoforge/client/settings/KeyMappingLookup.java
+++ src/main/java/net/neoforged/neoforge/client/settings/KeyMappingLookup.java
@@ -63,24 +63,20 @@ public class KeyMappingLookup {
         KeyModifier activeModifier = KeyModifier.getActiveModifier();
         // Apply active modifier only if the pressed key is not the modifier itself
         // Otherwise, look for key bindings without modifiers
-        if (activeModifier == KeyModifier.NONE || activeModifier.matches(keyCode) || !findKeybinds(matchingBindings, keyCode, activeModifier)) {
-            findKeybinds(matchingBindings, keyCode, KeyModifier.NONE);
+        if (activeModifier == KeyModifier.NONE || activeModifier.matches(keyCode) || !matchingBindings.addAll(findKeybinds(keyCode, activeModifier))) {
+            matchingBindings.addAll(findKeybinds(keyCode, KeyModifier.NONE));
         }
         return matchingBindings;
     }
 
-    private boolean findKeybinds(List<KeyMapping> matchingBindings, InputConstants.Key keyCode, KeyModifier modifier) {
-        boolean found = false;
+    private List<KeyMapping> findKeybinds(InputConstants.Key keyCode, KeyModifier modifier) {
         Collection<KeyMapping> modifierBindings = map.get(modifier).get(keyCode);
         if (modifierBindings != null) {
-            for (KeyMapping binding : modifierBindings) {
-                if (binding.isActiveAndMatches(keyCode)) {
-                    matchingBindings.add(binding);
-                    found = true;
-                }
-            }
+            return modifierBindings.stream()
+                .filter(binding -> binding.isActiveAndMatches(keyCode))
+                .toList();
         }
-        return found;
+        return List.of();
     }
 
     public void put(InputConstants.Key keyCode, KeyMapping keyBinding) {
