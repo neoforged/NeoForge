2015d05f3ee97a5dda147512b19b9bbef7ab4eda
Su5eD <su5ed@outlook.com>
2023-11-07 19:25:06 +0100

    Fix formatting

--- src/main/java/net/neoforged/neoforge/common/NeoForgeConfig.java
+++ src/main/java/net/neoforged/neoforge/common/NeoForgeConfig.java
@@ -10,7 +10,6 @@ import net.neoforged.fml.Logging;
 import net.neoforged.fml.event.config.ModConfigEvent;
 import net.neoforged.neoforge.common.ModConfigSpec.BooleanValue;
 import net.neoforged.neoforge.common.ModConfigSpec.ConfigValue;
-import net.neoforged.neoforge.common.ModConfigSpec.DoubleValue;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 
