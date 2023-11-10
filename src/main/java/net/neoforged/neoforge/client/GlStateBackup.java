cb7c20a06258ee386730ae89711dab174aca9d39
XFactHD <xfacthd@gmx.de>
2023-11-10 01:44:56 +0100

    Backup GL state in ItemDecoratorHandler instead of blindly disabling blending after decorators are rendered

---
+++ src/main/java/net/neoforged/neoforge/client/GlStateBackup.java
@@ -0,0 +1,31 @@
+package net.neoforged.neoforge.client;
+
+public final class GlStateBackup {
+    public boolean blendEnabled;
+    public int blendSrcRgb;
+    public int blendDestRgb;
+    public int blendSrcAlpha;
+    public int blendDestAlpha;
+    public boolean depthEnabled;
+    public boolean depthMask;
+    public int depthFunc;
+    public boolean cullEnabled;
+    public boolean polyOffsetFillEnabled;
+    public boolean polyOffsetLineEnabled;
+    public float polyOffsetFactor;
+    public float polyOffsetUnits;
+    public boolean colorLogicEnabled;
+    public int colorLogicOp;
+    public int stencilFuncFunc;
+    public int stencilFuncRef;
+    public int stencilFuncMask;
+    public int stencilMask;
+    public int stencilFail;
+    public int stencilZFail;
+    public int stencilZPass;
+    public boolean scissorEnabled;
+    public boolean colorMaskRed;
+    public boolean colorMaskGreen;
+    public boolean colorMaskBlue;
+    public boolean colorMaskAlpha;
+}
