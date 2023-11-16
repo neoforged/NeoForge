5169469a3685db482eca52cea79fef07e66a4693
Technici4n <13494793+Technici4n@users.noreply.github.com>
2023-11-16 19:00:32 +0100

    Clarify why we don't init stuff in onAdd

--- src/main/java/net/neoforged/neoforge/registries/NeoForgeRegistryCallbacks.java
+++ src/main/java/net/neoforged/neoforge/registries/NeoForgeRegistryCallbacks.java
@@ -48,6 +48,8 @@ class NeoForgeRegistryCallbacks {
             // Vanilla does this in Blocks.<clinit>
 
             // Init cache and loot table for new blocks only (the cache init is expensive).
+            // State cache init cannot be done in onAdd because some of it might depend on other registries being populated in mod code.
+            // Loot table init cannot be done in onAdd because the loot table supplier might depend on blocks registered later.
             for (Block block : addedBlocks) {
                 block.getStateDefinition().getPossibleStates().forEach(BlockBehaviour.BlockStateBase::initCache);
                 block.getLootTable();
