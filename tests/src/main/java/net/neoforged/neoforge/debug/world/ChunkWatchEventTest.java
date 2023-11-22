46a85ff1cc200fdd457ba2505a32f5baf5962129
XFactHD <xfacthd@gmx.de>
2023-11-22 23:03:01 +0100

    Update chunk watch test mod

--- tests/src/main/java/net/neoforged/neoforge/debug/world/ChunkWatchEventTest.java
+++ tests/src/main/java/net/neoforged/neoforge/debug/world/ChunkWatchEventTest.java
@@ -5,6 +5,7 @@
 
 package net.neoforged.neoforge.debug.world;
 
+import com.mojang.logging.LogUtils;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import java.util.UUID;
@@ -14,42 +15,45 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.level.ChunkWatchEvent;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
 
 @Mod(ChunkWatchEventTest.MODID)
 public class ChunkWatchEventTest {
     public static final String MODID = "chunkwatchworldtest";
 
     private static final boolean ENABLED = false;
-    private static Logger logger;
-    private static Object2IntMap<UUID> watchedByPlayer = new Object2IntOpenHashMap<>();
+    private static final Logger LOGGER = LogUtils.getLogger();
+    private static final Object2IntMap<UUID> WATCHED_BY_PLAYER = new Object2IntOpenHashMap<>();
 
     public ChunkWatchEventTest() {
-        logger = LogManager.getLogger();
-
         if (ENABLED) {
             NeoForge.EVENT_BUS.register(ChunkWatchEventTest.class);
         }
     }
 
     @SubscribeEvent
-    public static void onUnwatch(ChunkWatchEvent.UnWatch event) {
-        int watched = watchedByPlayer.getInt(event.getPlayer().getUUID());
-        --watched;
-        watchedByPlayer.put(event.getPlayer().getUUID(), watched);
-        logger.info("Unwatching chunk {} in dimension {}. Player's dimension: {}, total chunks watched by player {}",
+    public static void onWatch(ChunkWatchEvent.Watch event) {
+        int watched = WATCHED_BY_PLAYER.getInt(event.getPlayer().getUUID());
+        ++watched;
+        WATCHED_BY_PLAYER.put(event.getPlayer().getUUID(), watched);
+        LOGGER.info("Watching chunk {} in dimension {}. Player's dimension: {}, total chunks watched by player {}",
                 event.getPos(), getDimensionName(event.getLevel()), getDimensionName(event.getPlayer().getCommandSenderWorld()),
                 watched);
     }
 
     @SubscribeEvent
-    public static void onWatch(ChunkWatchEvent.Watch event) {
-        int watched = watchedByPlayer.getInt(event.getPlayer().getUUID());
-        ++watched;
-        watchedByPlayer.put(event.getPlayer().getUUID(), watched);
-        logger.info("Watching chunk {} in dimension {}. Player's dimension: {}, total chunks watched by player {}",
+    public static void onSent(ChunkWatchEvent.Sent event) {
+        LOGGER.info("Watched chunk {} in dimension {} sent to client. Player's dimension: {}",
+                event.getPos(), getDimensionName(event.getLevel()), getDimensionName(event.getPlayer().getCommandSenderWorld()));
+    }
+
+    @SubscribeEvent
+    public static void onUnwatch(ChunkWatchEvent.UnWatch event) {
+        int watched = WATCHED_BY_PLAYER.getInt(event.getPlayer().getUUID());
+        --watched;
+        WATCHED_BY_PLAYER.put(event.getPlayer().getUUID(), watched);
+        LOGGER.info("Unwatching chunk {} in dimension {}. Player's dimension: {}, total chunks watched by player {}",
                 event.getPos(), getDimensionName(event.getLevel()), getDimensionName(event.getPlayer().getCommandSenderWorld()),
                 watched);
     }
