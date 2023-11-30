3088c7d030594a540091327ae8325a9f48ae4186
XFactHD <xfacthd@gmx.de>
2023-11-30 23:36:56 +0100

    Formatting and license

--- src/main/java/net/neoforged/neoforge/client/extensions/IBlockEntityRendererExtension.java
+++ src/main/java/net/neoforged/neoforge/client/extensions/IBlockEntityRendererExtension.java
@@ -1,11 +1,15 @@
+/*
+ * Copyright (c) NeoForged and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
 package net.neoforged.neoforge.client.extensions;
 
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.phys.AABB;
 
-public interface IBlockEntityRendererExtension<T extends BlockEntity>
-{
+public interface IBlockEntityRendererExtension<T extends BlockEntity> {
     AABB INFINITE_EXTENT_AABB = new net.minecraft.world.phys.AABB(Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
 
     /**
