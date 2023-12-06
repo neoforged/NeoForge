938ba8fcda0d0eb94a077aaa08d3bad7717245b4
Technici4n <13494793+Technici4n@users.noreply.github.com>
2023-12-07 00:41:13 +0100

    Remove leftover IItemExtension sharetag methods (#332)

--- src/main/java/net/neoforged/neoforge/common/extensions/IItemExtension.java
+++ src/main/java/net/neoforged/neoforge/common/extensions/IItemExtension.java
@@ -128,38 +128,6 @@ public interface IItemExtension {
     }
 
     /**
-     * Override this method to change the NBT data being sent to the client. You
-     * should ONLY override this when you have no other choice, as this might change
-     * behavior client side!
-     *
-     * Note that this will sometimes be applied multiple times, the following MUST
-     * be supported:
-     * Item item = stack.getItem();
-     * NBTTagCompound nbtShare1 = item.getNBTShareTag(stack);
-     * stack.setTagCompound(nbtShare1);
-     * NBTTagCompound nbtShare2 = item.getNBTShareTag(stack);
-     * assert nbtShare1.equals(nbtShare2);
-     *
-     * @param stack The stack to send the NBT tag for
-     * @return The NBT tag
-     */
-    @Nullable
-    default CompoundTag getShareTag(ItemStack stack) {
-        return stack.getTag();
-    }
-
-    /**
-     * Override this method to decide what to do with the NBT data received from
-     * getNBTShareTag().
-     *
-     * @param stack The stack that received NBT
-     * @param nbt   Received NBT, can be null
-     */
-    default void readShareTag(ItemStack stack, @Nullable CompoundTag nbt) {
-        stack.setTag(nbt);
-    }
-
-    /**
      * Called before a block is broken. Return true to prevent default block
      * harvesting.
      *
