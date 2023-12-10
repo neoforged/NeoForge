ae9bb08e6403aea622c29b347b585fafc9d63f9c
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-12-10 16:25:28 -0500

    Remove unused import

--- tests/src/main/java/net/neoforged/neoforge/oldtest/DataGeneratorTest.java
+++ tests/src/main/java/net/neoforged/neoforge/oldtest/DataGeneratorTest.java
@@ -14,7 +14,6 @@ import java.math.BigDecimal;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
