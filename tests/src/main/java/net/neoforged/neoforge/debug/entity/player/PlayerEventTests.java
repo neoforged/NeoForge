da89484b5b4006b1be72a3a5e62d0d0d193421cc
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-12-11 03:08:31 -0500

    [1.20.4] Restore serversided onInteractEntityAt event firing (#366)
    Fixes #363

--- tests/src/main/java/net/neoforged/neoforge/debug/entity/player/PlayerEventTests.java
+++ tests/src/main/java/net/neoforged/neoforge/debug/entity/player/PlayerEventTests.java
@@ -5,9 +5,16 @@
 
 package net.neoforged.neoforge.debug.entity.player;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.game.ServerboundInteractPacket;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.level.GameType;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.testframework.DynamicTest;
 import net.neoforged.testframework.annotation.ForEachTest;
 import net.neoforged.testframework.annotation.TestHolder;
@@ -33,6 +40,26 @@ public class PlayerEventTests {
                     "display name",
                     "hello world");
             helper.succeed();
+        });
+    }
+
+    @GameTest
+    @EmptyTemplate
+    @TestHolder(description = "Tests if the on entity interact event is fired")
+    static void entityInteractEvent(final DynamicTest test) {
+        test.eventListeners().forge().addListener((final PlayerInteractEvent.EntityInteractSpecific event) -> {
+            if (event.getTarget().getType() == EntityType.ILLUSIONER) {
+                String oldName = event.getTarget().getName().getString();
+                event.getTarget().setCustomName(Component.literal(oldName + " entityInteractEventTest"));
+            }
+        });
+
+        test.onGameTest(helper -> {
+            Mob illusioner = helper.spawnWithNoFreeWill(EntityType.ILLUSIONER, 1, 1, 1);
+            helper.startSequence(() -> helper.makeTickingMockServerPlayerInCorner(GameType.SURVIVAL))
+                    .thenExecute(player -> player.connection.handleInteract(ServerboundInteractPacket.createInteractionPacket(illusioner, player.isShiftKeyDown(), InteractionHand.MAIN_HAND, helper.absoluteVec(new BlockPos(1, 1, 1).getCenter()))))
+                    .thenExecute(player -> helper.assertTrue(illusioner.getName().getString().contains("entityInteractEventTest"), "Illager name did not get changed on player interact"))
+                    .thenSucceed();
         });
     }
 }
