2259220c93793d287650c0c5a9e91cfb89061729
Technici4n <13494793+Technici4n@users.noreply.github.com>
2023-12-11 19:15:28 +0100

    Implement fluid handler support for cauldrons

---
+++ src/main/java/net/neoforged/neoforge/fluids/CauldronFluidContent.java
@@ -0,0 +1,169 @@
+package net.neoforged.neoforge.fluids;
+
+import java.util.Collection;
+import java.util.IdentityHashMap;
+import java.util.Map;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.LayeredCauldronBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.Fluids;
+import net.neoforged.fml.ModLoader;
+import net.neoforged.neoforge.capabilities.Capabilities;
+import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
+import net.neoforged.neoforge.fluids.capability.wrappers.CauldronWrapper;
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Fluid content information for cauldrons.
+ *
+ * <p>Empty, water and lava cauldrons are registered by default,
+ * and additional cauldrons must be registered with {@link RegisterCauldronFluidContentEvent}.
+ * Contents can be queried with {@link #getForBlock} and {@link #getForFluid}.
+ *
+ * <p>The {@code CauldronFluidContent} itself defines:
+ * <ul>
+ * <li>The block of the cauldron.</li>
+ * <li>The fluid that can be accepted by the cauldron. NBT is discarded when entering the cauldron.</li>
+ * <li>Which fluid amounts can be stored in the cauldron, and how they map to the level property of the cauldron.
+ * If {@link #levelProperty} is {@code null}, then {@code maxLevel = 1}, and there is only one level.
+ * Otherwise, the levels are all the integer values between {@code 1} and {@link #maxLevel} (included).
+ * </li>
+ * <li>{@link #totalAmount} defines how much fluid (in millibuckets) there is in one level of the cauldron.</li>
+ * </ul>
+ */
+public final class CauldronFluidContent {
+    /**
+     * Block of the cauldron.
+     */
+    public final Block block;
+    /**
+     * Fluid stored inside the cauldron.
+     */
+    public final Fluid fluid;
+    /**
+     * Amount of {@code #fluid} in millibuckets in the entire full cauldron.
+     */
+    public final int totalAmount;
+    /**
+     * Maximum level for {@link #levelProperty}. {@code 1} if {@code levelProperty} is null, otherwise a number {@code >= 1}.
+     * The minimum level is always 1.
+     */
+    public final int maxLevel;
+    /**
+     * Property storing the level of the cauldron. If it's {@code null}, only one level is possible.
+     */
+    @Nullable
+    public final IntegerProperty levelProperty;
+
+    /**
+     * Return the current level of the cauldron given its block state, or 0 if it's an empty cauldron.
+     */
+    public int currentLevel(BlockState state) {
+        if (fluid == Fluids.EMPTY) {
+            return 0;
+        } else if (levelProperty == null) {
+            return 1;
+        } else {
+            return state.getValue(levelProperty);
+        }
+    }
+
+    private CauldronFluidContent(Block block, Fluid fluid, int totalAmount, int maxLevel, @Nullable IntegerProperty levelProperty) {
+        this.block = block;
+        this.fluid = fluid;
+        this.totalAmount = totalAmount;
+        this.maxLevel = maxLevel;
+        this.levelProperty = levelProperty;
+    }
+
+    private static final Map<Block, CauldronFluidContent> BLOCK_TO_CAULDRON = new IdentityHashMap<>();
+    private static final Map<Fluid, CauldronFluidContent> FLUID_TO_CAULDRON = new IdentityHashMap<>();
+
+    /**
+     * Get the cauldron fluid content for a cauldron block, or {@code null} if none was registered (yet).
+     */
+    @Nullable
+    public static CauldronFluidContent getForBlock(Block block) {
+        return BLOCK_TO_CAULDRON.get(block);
+    }
+
+    /**
+     * Get the cauldron fluid content for a fluid, or {@code null} if no cauldron was registered for that fluid (yet).
+     */
+    @Nullable
+    public static CauldronFluidContent getForFluid(Fluid fluid) {
+        return FLUID_TO_CAULDRON.get(fluid);
+    }
+
+    @ApiStatus.Internal
+    public static void init() {
+        var registerEvent = new RegisterCauldronFluidContentEvent();
+        // Vanilla registrations
+        registerEvent.register(Blocks.CAULDRON, Fluids.EMPTY, 1000, null);
+        registerEvent.register(Blocks.WATER_CAULDRON, Fluids.WATER, 1000, LayeredCauldronBlock.LEVEL);
+        registerEvent.register(Blocks.LAVA_CAULDRON, Fluids.LAVA, 1000, null);
+        // Modded registrations
+        ModLoader.get().postEvent(registerEvent);
+    }
+
+    /**
+     * Do not try to call, use the {@link RegisterCauldronFluidContentEvent} event instead.
+     */
+    static void register(Block block, Fluid fluid, int totalAmount, @Nullable IntegerProperty levelProperty) {
+        if (BLOCK_TO_CAULDRON.get(block) != null) {
+            throw new IllegalArgumentException("Duplicate cauldron registration for block %s.".formatted(block));
+        }
+        if (FLUID_TO_CAULDRON.get(fluid) != null) {
+            throw new IllegalArgumentException("Duplicate cauldron registration for fluid %s.".formatted(fluid));
+        }
+        if (totalAmount <= 0) {
+            throw new IllegalArgumentException("Cauldron total amount %d should be positive.".formatted(totalAmount));
+        }
+
+        CauldronFluidContent data;
+
+        if (levelProperty == null) {
+            data = new CauldronFluidContent(block, fluid, totalAmount, 1, null);
+        } else {
+            Collection<Integer> levels = levelProperty.getPossibleValues();
+            if (levels.isEmpty()) {
+                throw new IllegalArgumentException("Cauldron should have at least one possible level.");
+            }
+
+            int minLevel = Integer.MAX_VALUE;
+            int maxLevel = 0;
+
+            for (int level : levels) {
+                minLevel = Math.min(minLevel, level);
+                maxLevel = Math.max(maxLevel, level);
+            }
+
+            if (minLevel != 1) {
+                throw new IllegalStateException("Minimum level should be 1, and maximum level should be >= 1.");
+            }
+
+            data = new CauldronFluidContent(block, fluid, totalAmount, maxLevel, levelProperty);
+        }
+
+        BLOCK_TO_CAULDRON.put(block, data);
+        FLUID_TO_CAULDRON.put(fluid, data);
+    }
+
+    @ApiStatus.Internal
+    public static void registerCapabilities(RegisterCapabilitiesEvent event) {
+        if (BLOCK_TO_CAULDRON.isEmpty()) {
+            throw new IllegalStateException("CauldronFluidContent.init() should have been called before the capability event!");
+        }
+
+        for (Block block : BLOCK_TO_CAULDRON.keySet()) {
+            event.registerBlock(
+                    Capabilities.FluidHandler.BLOCK,
+                    (level, pos, state, be, context) -> new CauldronWrapper(level, pos),
+                    block);
+        }
+    }
+}
---
+++ tests/src/main/java/net/neoforged/neoforge/debug/capabilities/VanillaHandlersTests.java
@@ -0,0 +1,146 @@
+package net.neoforged.neoforge.debug.capabilities;
+
+import static net.neoforged.neoforge.fluids.capability.IFluidHandler.FluidAction.EXECUTE;
+import static net.neoforged.neoforge.fluids.capability.IFluidHandler.FluidAction.SIMULATE;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.gametest.framework.GameTest;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.LayeredCauldronBlock;
+import net.minecraft.world.level.material.Fluids;
+import net.neoforged.neoforge.capabilities.BlockCapabilityCache;
+import net.neoforged.neoforge.capabilities.Capabilities;
+import net.neoforged.neoforge.eventtest.internal.TestsMod;
+import net.neoforged.neoforge.fluids.FluidStack;
+import net.neoforged.testframework.DynamicTest;
+import net.neoforged.testframework.annotation.ForEachTest;
+import net.neoforged.testframework.annotation.TestHolder;
+import org.apache.commons.lang3.mutable.MutableInt;
+
+@ForEachTest(groups = "capabilities.vanillahandlers")
+public class VanillaHandlersTests {
+    @TestHolder(description = {
+            "Tests that composter capabilities get invalidated correctly"
+    })
+    @GameTest(template = TestsMod.TEMPLATE_3x3)
+    public static void testComposterInvalidation(DynamicTest test) {
+        test.onGameTest(helper -> {
+            var composterPos = new BlockPos(1, 1, 1);
+
+            MutableInt invalidationCount = new MutableInt();
+            var capCache = BlockCapabilityCache.create(
+                    Capabilities.ItemHandler.BLOCK,
+                    helper.getLevel(),
+                    helper.absolutePos(composterPos),
+                    Direction.UP,
+                    () -> true,
+                    invalidationCount::increment);
+
+            if (capCache.getCapability() != null)
+                helper.fail("Expected no capability", composterPos);
+            if (capCache.getCapability() != null) // check again just in case
+                helper.fail("Expected no capability", composterPos);
+            if (invalidationCount.getValue() != 0)
+                helper.fail("Should not have been invalidated yet", composterPos);
+
+            // The cache should only be invalidated once until it is queried again
+            helper.setBlock(composterPos, Blocks.COMPOSTER.defaultBlockState());
+            if (invalidationCount.getValue() != 1)
+                helper.fail("Should have invalidated once");
+
+            helper.setBlock(composterPos, Blocks.AIR.defaultBlockState());
+            if (invalidationCount.getValue() != 1) // capability not re-queried, so no invalidation
+                helper.fail("Should have invalidated once");
+
+            helper.setBlock(composterPos, Blocks.COMPOSTER.defaultBlockState());
+            if (invalidationCount.getValue() != 1) // capability not re-queried, so no invalidation
+                helper.fail("Should have invalidated once");
+
+            // Should be ok to query now
+            if (capCache.getCapability() == null)
+                helper.fail("Expected capability", composterPos);
+            if (invalidationCount.getValue() != 1)
+                helper.fail("Should have invalidated once");
+
+            // Should be notified of disappearance if the composter is removed
+            helper.setBlock(composterPos, Blocks.AIR.defaultBlockState());
+
+            if (invalidationCount.getValue() != 2)
+                helper.fail("Should have invalidated a second time");
+            if (capCache.getCapability() != null)
+                helper.fail("Expected no capability", composterPos);
+
+            helper.succeed();
+        });
+    }
+
+    @TestHolder(description = "Test cauldron interactions via the fluid handler capability")
+    @GameTest(template = TestsMod.TEMPLATE_3x3)
+    public static void testCauldronCapability(DynamicTest test) {
+        test.onGameTest(helper -> {
+            var cauldronPos = new BlockPos(1, 1, 1);
+
+            MutableInt invalidationCount = new MutableInt();
+            var capCache = BlockCapabilityCache.create(
+                    Capabilities.FluidHandler.BLOCK,
+                    helper.getLevel(),
+                    helper.absolutePos(cauldronPos),
+                    Direction.UP,
+                    () -> true,
+                    invalidationCount::increment);
+
+            // Capability should be absent
+            helper.assertTrue(capCache.getCapability() == null, "Expected no capability");
+
+            // Should invalidate once when setting the block
+            helper.setBlock(cauldronPos, Blocks.CAULDRON);
+            var wrapper = capCache.getCapability();
+            helper.assertTrue(wrapper != null, "Expected fluid handler");
+            helper.assertTrue(invalidationCount.intValue() == 1, "Expected 1 invalidation only");
+
+            helper.assertTrue(wrapper.getTanks() == 1, "Got %d tanks".formatted(wrapper.getTanks()));
+
+            // Simulate filling with water
+            var fillResult = wrapper.fill(new FluidStack(Fluids.WATER, 2000), SIMULATE);
+            helper.assertTrue(fillResult == 1000, "Filled " + fillResult);
+            helper.assertBlockPresent(Blocks.CAULDRON, cauldronPos);
+            // Can't fill with less than 1000 though...
+            helper.assertTrue(wrapper.fill(new FluidStack(Fluids.WATER, 999), SIMULATE) == 0, "Expected 0 fill result");
+
+            // Action!
+            fillResult = wrapper.fill(new FluidStack(Fluids.WATER, 2000), EXECUTE);
+            helper.assertTrue(fillResult == 1000, "Filled " + fillResult);
+            helper.assertBlockState(cauldronPos, state -> state.is(Blocks.WATER_CAULDRON) && state.getValue(LayeredCauldronBlock.LEVEL) == 3, () -> "Expected level 3 cauldron");
+
+            helper.assertTrue(wrapper.getFluidInTank(0).equals(new FluidStack(Fluids.WATER, 1000)), "Expected 1000 water");
+
+            // Try to empty as well
+            helper.assertTrue(wrapper.drain(new FluidStack(Fluids.LAVA, 1000), EXECUTE).isEmpty(), "Cannot drain lava");
+            helper.assertTrue(wrapper.drain(new FluidStack(Fluids.WATER, 999), EXECUTE).isEmpty(), "Cannot drain less than 1000 water");
+            helper.assertTrue(wrapper.drain(new FluidStack(Fluids.WATER, 1000), EXECUTE).equals(new FluidStack(Fluids.WATER, 1000)), "Expected drain of 1000 water");
+
+            helper.assertBlockPresent(Blocks.CAULDRON, cauldronPos);
+
+            // Try lava cauldron
+            helper.setBlock(cauldronPos, Blocks.LAVA_CAULDRON);
+            helper.assertTrue(wrapper.getFluidInTank(0).equals(new FluidStack(Fluids.LAVA, 1000)), "Expected 1000 lava");
+            helper.assertTrue(wrapper.drain(1000, EXECUTE).equals(new FluidStack(Fluids.LAVA, 1000)), "Expected drain of 1000 lava");
+            helper.assertBlockPresent(Blocks.CAULDRON, cauldronPos);
+
+            // Try partial water filling
+            helper.setBlock(cauldronPos, Blocks.WATER_CAULDRON.defaultBlockState().setValue(LayeredCauldronBlock.LEVEL, 2));
+            helper.assertTrue(wrapper.getFluidInTank(0).equals(new FluidStack(Fluids.WATER, 666)), "Expected 666 water");
+            helper.assertTrue(wrapper.drain(1000, EXECUTE).isEmpty(), "Expected no water drain from partial cauldron");
+            helper.assertTrue(wrapper.fill(new FluidStack(Fluids.WATER, 1000), EXECUTE) == 0, "Expected no water fill to partial cauldron");
+
+            // None of this should have invalidated the capability
+            helper.assertTrue(invalidationCount.intValue() == 1, "Expected 1 invalidation only after the whole test");
+            // But if we change the block to a non-cauldron, it should invalidate
+            helper.destroyBlock(cauldronPos);
+            helper.assertTrue(invalidationCount.intValue() == 2, "Expected a second invalidation after cauldron destruction");
+
+            helper.succeed();
+        });
+    }
+}
