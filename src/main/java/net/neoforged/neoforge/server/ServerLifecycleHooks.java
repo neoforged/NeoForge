ae55da0ddcd3137c5cc26a97f0c1ed9c7d731567
Sara Freimer <sara@freimer.com>
2023-12-12 15:28:21 -0600

    Add a brief readme to the serverconfig folder to explain to users what it is for

--- src/main/java/net/neoforged/neoforge/server/ServerLifecycleHooks.java
+++ src/main/java/net/neoforged/neoforge/server/ServerLifecycleHooks.java
@@ -6,6 +6,7 @@
 package net.neoforged.neoforge.server;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
@@ -79,6 +80,18 @@ public class ServerLifecycleHooks {
         if (!Files.isDirectory(serverConfig)) {
             try {
                 Files.createDirectories(serverConfig);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        final Path explanation = serverConfig.resolve("readme.txt");
+        if (!Files.exists(explanation)) {
+            try {
+                Files.writeString(explanation, """
+                        Any server configs put in this folder will override the corresponding server config from <instance path>/config/<config path>.
+                        If the config being transferred is in a subfolder of the base config folder make sure to include that folder here in the path to the file you are overwriting.
+                        For example if you are overwriting a config with the path <instance path>/config/ExampleMod/config-server.toml, you would need to put it in serverconfig/ExampleMod/config-server.toml
+                        """, StandardCharsets.UTF_8);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
