997cdc6892a8706d3c416bdd0c96505cc62afe42
Brennan Ward <Bward7864@gmail.com>
2023-12-13 04:22:21 -0500

    [1.20.x] Fix Reach Issues (and eclipse compilation issues) (#346)
    Co-authored-by: Technici4n <13494793+Technici4n@users.noreply.github.com>

--- src/main/java/net/neoforged/neoforge/capabilities/BlockCapability.java
+++ src/main/java/net/neoforged/neoforge/capabilities/BlockCapability.java
@@ -126,7 +126,9 @@ public final class BlockCapability<T, C> extends BaseCapability<T, C> {
     }
 
     // INTERNAL
-    private static final CapabilityRegistry<BlockCapability<?, ?>> registry = new CapabilityRegistry<>(BlockCapability::new);
+
+    // Requires explicitly-typed constructor due to ECJ inference failure.
+    private static final CapabilityRegistry<BlockCapability<?, ?>> registry = new CapabilityRegistry<BlockCapability<?, ?>>(BlockCapability::new);
 
     private BlockCapability(ResourceLocation name, Class<T> typeClass, Class<C> contextClass) {
         super(name, typeClass, contextClass);
