7e89418a824fc3d82265fb5bd5a14d0b5e67c848
Marc Hermans <marc.hermans@ldtteam.com>
2023-12-14 12:35:50 +0100

    Fix NSF and create endpoints to request connection and whether the configuration phase is talking to a vanilla client.

--- src/main/java/net/neoforged/neoforge/common/extensions/IServerConfigurationPacketListenerExtension.java
+++ src/main/java/net/neoforged/neoforge/common/extensions/IServerConfigurationPacketListenerExtension.java
@@ -1,7 +1,12 @@
 package net.neoforged.neoforge.common.extensions;
 
+import net.minecraft.network.Connection;
 import net.minecraft.server.network.ConfigurationTask;
 
 public interface IServerConfigurationPacketListenerExtension {
     void finishCurrentTask(ConfigurationTask.Type p_294853_);
+    
+    boolean isVanillaConnection();
+    
+    Connection getConnection();
 }
--- src/main/java/net/neoforged/neoforge/internal/NeoForgeStatesProvider.java
+++ src/main/java/net/neoforged/neoforge/internal/NeoForgeStatesProvider.java
@@ -5,14 +5,15 @@
 
 package net.neoforged.neoforge.internal;
 
-import java.util.List;
 import net.neoforged.fml.IModLoadingState;
 import net.neoforged.fml.IModStateProvider;
 import net.neoforged.fml.ModLoadingPhase;
 import net.neoforged.fml.ModLoadingState;
-import net.neoforged.neoforge.network.NetworkRegistry;
+import net.neoforged.neoforge.network.registration.NetworkRegistry;
 import net.neoforged.neoforge.registries.GameData;
 import net.neoforged.neoforge.registries.RegistryManager;
+
+import java.util.List;
 
 public class NeoForgeStatesProvider implements IModStateProvider {
     final ModLoadingState CREATE_REGISTRIES = ModLoadingState.withInline("CREATE_REGISTRIES", "CONSTRUCT", ModLoadingPhase.GATHER, ml -> RegistryManager.postNewRegistryEvent());
@@ -20,7 +21,7 @@ public class NeoForgeStatesProvider implements IModStateProvider {
     final ModLoadingState LOAD_REGISTRIES = ModLoadingState.withInline("LOAD_REGISTRIES", "UNFREEZE_DATA", ModLoadingPhase.GATHER, ml -> GameData.postRegisterEvents());
     final ModLoadingState FREEZE = ModLoadingState.withInline("FREEZE_DATA", "LOAD_REGISTRIES", ModLoadingPhase.GATHER, ml -> GameData.freezeData());
     final ModLoadingState REGISTRATION_EVENTS = ModLoadingState.withInline("REGISTRATION_EVENTS", "SIDED_SETUP", ModLoadingPhase.LOAD, ml -> RegistrationEvents.init());
-    final ModLoadingState NETLOCK = ModLoadingState.withInline("NETWORK_LOCK", "COMPLETE", ModLoadingPhase.COMPLETE, ml -> NetworkRegistry.lock());
+    final ModLoadingState NETLOCK = ModLoadingState.withInline("NETWORK_LOCK", "COMPLETE", ModLoadingPhase.COMPLETE, ml -> NetworkRegistry.getInstance().setup());
 
     @Override
     public List<IModLoadingState> getAllStates() {
