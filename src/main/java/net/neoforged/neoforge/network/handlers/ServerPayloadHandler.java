eb2c8452d52b152e04ec823af94f250c6a27fed7
Marc Hermans <marc.hermans@ldtteam.com>
2023-12-18 17:14:19 +0100

    Fix licences

--- src/main/java/net/neoforged/neoforge/network/handlers/ServerPayloadHandler.java
+++ src/main/java/net/neoforged/neoforge/network/handlers/ServerPayloadHandler.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) NeoForged and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
 package net.neoforged.neoforge.network.handlers;
 
 import net.neoforged.neoforge.network.configuration.SyncRegistries;
--- src/main/java/net/neoforged/neoforge/network/handling/ISynchronizedWorkHandler.java
+++ src/main/java/net/neoforged/neoforge/network/handling/ISynchronizedWorkHandler.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) NeoForged and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
 package net.neoforged.neoforge.network.handling;
 
 import java.util.concurrent.CompletableFuture;
