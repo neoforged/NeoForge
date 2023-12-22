27ffe4a8fcd56cf4a31476658a0d7a3e97b570b2
Nekretaur <84230130+Nekretaur@users.noreply.github.com>
2023-12-22 22:19:56 +0530

    Rename RegisterCapabilitiesEvent to RegisterCapabilityProvidersEvent
    This feels like an appropriate rename

--- src/main/java/net/neoforged/neoforge/capabilities/RegisterCapabilitiesEvent.java
+++ src/main/java/net/neoforged/neoforge/capabilities/RegisterCapabilitiesEvent.java
@@ -23,7 +23,7 @@ import net.neoforged.fml.event.IModBusEvent;
 /**
  * Fired to register capability providers at an appropriate time.
  */
-public class RegisterCapabilitiesEvent extends Event implements IModBusEvent {
+public class RegisterCapabilityProvidersEvent extends Event implements IModBusEvent {
     RegisterCapabilitiesEvent() {}
 
     // BLOCKS
