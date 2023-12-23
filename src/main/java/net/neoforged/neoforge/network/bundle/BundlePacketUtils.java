538e4d2ee8126fcc2f9a3c65317b2424d50e14e9
Marc Hermans <marc.hermans@ldtteam.com>
2023-12-23 10:55:43 +0100

    Fix licenses

--- src/main/java/net/neoforged/neoforge/network/bundle/BundlePacketUtils.java
+++ src/main/java/net/neoforged/neoforge/network/bundle/BundlePacketUtils.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) NeoForged and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
 package net.neoforged.neoforge.network.bundle;
 
 import com.google.common.collect.Lists;
