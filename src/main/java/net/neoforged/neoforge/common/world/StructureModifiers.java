72d7cef7447245d4cacc7be80dd3bc185a2e6652
Nekretaur <84230130+Nekretaur@users.noreply.github.com>
2023-12-29 19:03:47 +0530

    add a space?

--- src/main/java/net/neoforged/neoforge/common/world/StructureModifiers.java
+++ src/main/java/net/neoforged/neoforge/common/world/StructureModifiers.java
@@ -2,6 +2,7 @@
  * Copyright (c) Forge Development LLC and contributors
  * SPDX-License-Identifier: LGPL-2.1-only
  */
+
 package net.neoforged.neoforge.common.world;
 
 import com.mojang.serialization.Codec;
