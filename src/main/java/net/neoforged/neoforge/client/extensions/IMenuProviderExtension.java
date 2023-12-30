7383365f066b89eefd15ea57d94f5aa26a848cb1
Marc Hermans <marc.hermans@ldtteam.com>
2023-12-30 13:02:43 +0100

    Update src/main/java/net/neoforged/neoforge/client/extensions/IMenuProviderExtension.java
    Co-authored-by: Matyrobbrt <65940752+Matyrobbrt@users.noreply.github.com>

--- src/main/java/net/neoforged/neoforge/client/extensions/IMenuProviderExtension.java
+++ src/main/java/net/neoforged/neoforge/client/extensions/IMenuProviderExtension.java
@@ -13,7 +13,7 @@ public interface IMenuProviderExtension {
     /**
      * {@return true if the existing container should be closed on the client side when opening a new one, false otherwise}
      * 
-     * @implNote Returning false, prevents the mouse from being (re-)centered when opening a new container.
+     * @implNote Returning false prevents the mouse from being (re-)centered when opening a new container.
      */
     default boolean shouldTriggerClientSideContainerClosingOnOpen() {
         return true;
