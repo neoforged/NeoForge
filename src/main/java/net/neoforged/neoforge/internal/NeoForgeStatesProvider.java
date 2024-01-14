9d9f0ab97de651d6f8909f781d661e3f9a3b68cb
Nekretaur <84230130+Nekretaur@users.noreply.github.com>
2024-01-14 19:09:40 +0000

    Add RegisterMenuScreensEvent and its functions

--- src/main/java/net/neoforged/neoforge/internal/NeoForgeStatesProvider.java
+++ src/main/java/net/neoforged/neoforge/internal/NeoForgeStatesProvider.java
@@ -10,6 +10,7 @@ import net.neoforged.fml.IModLoadingState;
 import net.neoforged.fml.IModStateProvider;
 import net.neoforged.fml.ModLoadingPhase;
 import net.neoforged.fml.ModLoadingState;
+import net.neoforged.neoforge.client.ClientRegistrationEvents;
 import net.neoforged.neoforge.network.registration.NetworkRegistry;
 import net.neoforged.neoforge.registries.GameData;
 import net.neoforged.neoforge.registries.RegistryManager;
@@ -20,10 +21,11 @@ public class NeoForgeStatesProvider implements IModStateProvider {
     final ModLoadingState LOAD_REGISTRIES = ModLoadingState.withInline("LOAD_REGISTRIES", "UNFREEZE_DATA", ModLoadingPhase.GATHER, ml -> GameData.postRegisterEvents());
     final ModLoadingState FREEZE = ModLoadingState.withInline("FREEZE_DATA", "LOAD_REGISTRIES", ModLoadingPhase.GATHER, ml -> GameData.freezeData());
     final ModLoadingState REGISTRATION_EVENTS = ModLoadingState.withInline("REGISTRATION_EVENTS", "SIDED_SETUP", ModLoadingPhase.LOAD, ml -> RegistrationEvents.init());
+    final ModLoadingState CLIENT_REGISTRATION_EVENTS = ModLoadingState.withInline("CLIENT_REGISTRATION_EVENTS", "SIDED_SETUP", ModLoadingPhase.LOAD, ml -> ClientRegistrationEvents.init());
     final ModLoadingState NETLOCK = ModLoadingState.withInline("NETWORK_LOCK", "COMPLETE", ModLoadingPhase.COMPLETE, ml -> NetworkRegistry.getInstance().setup());
 
     @Override
     public List<IModLoadingState> getAllStates() {
-        return List.of(CREATE_REGISTRIES, UNFREEZE, LOAD_REGISTRIES, FREEZE, REGISTRATION_EVENTS, NETLOCK);
+        return List.of(CREATE_REGISTRIES, UNFREEZE, LOAD_REGISTRIES, FREEZE, REGISTRATION_EVENTS, CLIENT_REGISTRATION_EVENTS, NETLOCK);
     }
 }
