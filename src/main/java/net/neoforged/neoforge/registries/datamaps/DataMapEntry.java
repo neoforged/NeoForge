8a1a40855861f92dc36b8b74cab67e29ca409ed9
Matyrobbrt <matyrobbrt@gmail.com>
2024-01-15 16:21:55 +0200

    Update DataMapEntry.java

--- src/main/java/net/neoforged/neoforge/registries/datamaps/DataMapEntry.java
+++ src/main/java/net/neoforged/neoforge/registries/datamaps/DataMapEntry.java
@@ -9,6 +9,8 @@ import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import java.util.Optional;
+import java.util.function.Function;
+
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.TagKey;
 import net.minecraft.util.ExtraCodecs;
@@ -19,11 +21,12 @@ public record DataMapEntry<T>(T value, boolean replace) {
     }
 
     public static <T> Codec<DataMapEntry<T>> codec(DataMapType<T, ?, ?> type) {
-        return ExtraCodecs.withAlternative(
-                RecordCodecBuilder.create(i -> i.group(
+        return ExtraCodecs.either(
+                RecordCodecBuilder.<DataMapEntry<T>>create(i -> i.group(
                         type.codec().fieldOf("value").forGetter(DataMapEntry::value),
                         ExtraCodecs.strictOptionalField(Codec.BOOL, "replace", false).forGetter(DataMapEntry::replace)).apply(i, DataMapEntry::new)),
-                type.codec().xmap(DataMapEntry::new, DataMapEntry::value));
+                type.codec())
+                .xmap(e -> e.map(Function.identity(), DataMapEntry::new), entry -> entry.replace() ? Either.left(entry) : Either.right(entry.value()));
     }
 
     public record Removal<T, R>(Either<TagKey<R>, ResourceKey<R>> key,
