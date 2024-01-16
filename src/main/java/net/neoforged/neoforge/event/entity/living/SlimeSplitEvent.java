d0333c1dd667aa71a76c2ad1e59d1a83ea264dc1
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2024-01-15 17:25:22 -0800

    license

--- src/main/java/net/neoforged/neoforge/event/entity/living/SlimeSplitEvent.java
+++ src/main/java/net/neoforged/neoforge/event/entity/living/SlimeSplitEvent.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) NeoForged and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
 package net.neoforged.neoforge.event.entity.living;
 
 import net.minecraft.world.entity.Entity.RemovalReason;
