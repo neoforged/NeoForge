fe470a25f3812019c4fa308acccf17acc648d6ef
IchHabeHunger54 <kingdavidaut@gmail.com>
2024-01-16 16:14:09 +0100

    remove EventHooks#doToolCheck

--- src/main/java/net/neoforged/neoforge/common/CommonHooks.java
+++ src/main/java/net/neoforged/neoforge/common/CommonHooks.java
@@ -212,7 +212,8 @@ public class CommonHooks {
 
     public static boolean isCorrectToolForDrops(BlockGetter level, BlockPos pos, BlockState state, Player player) {
         if (level instanceof LevelAccessor accessor) { //BlockEvent expects a LevelAccessor
-            BlockEvent.ToolCheck event = EventHooks.doToolCheck(accessor, pos, state, player, player.getMainHandItem());
+            BlockEvent.ToolCheck event = new BlockEvent.ToolCheck(accessor, pos, state, player, player.getMainHandItem());
+            NeoForge.EVENT_BUS.post(event);
             if (event.getResult() != Event.Result.DEFAULT)
                 return event.getResult() == Event.Result.ALLOW;
         }
