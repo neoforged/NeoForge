db1d8e5ce8a75d924bef2b12c2abc3cef37e737c
Nolij <16640162+Nolij@users.noreply.github.com>
2024-01-18 09:30:51 -0500

    Add `CalculateTurnPlayerValuesEvent` (#534)

--- src/main/java/net/neoforged/neoforge/client/ClientHooks.java
+++ src/main/java/net/neoforged/neoforge/client/ClientHooks.java
@@ -130,6 +130,7 @@ import net.neoforged.bus.api.Event;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModLoader;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.event.CalculatePlayerTurnEvent;
 import net.neoforged.neoforge.client.event.ClientChatEvent;
 import net.neoforged.neoforge.client.event.ClientChatReceivedEvent;
 import net.neoforged.neoforge.client.event.ClientPauseUpdatedEvent;
@@ -340,6 +341,12 @@ public class ClientHooks {
         ViewportEvent.ComputeFov event = new ViewportEvent.ComputeFov(renderer, camera, partialTick, fov, usedConfiguredFov);
         NeoForge.EVENT_BUS.post(event);
         return event.getFOV();
+    }
+
+    public static CalculatePlayerTurnEvent getTurnPlayerValues(double mouseSensitivity, boolean cinematicCameraEnabled) {
+        var event = new CalculatePlayerTurnEvent(mouseSensitivity, cinematicCameraEnabled);
+        NeoForge.EVENT_BUS.post(event);
+        return event;
     }
 
     /**
