5e1e9fad1f16b73d716b5d51a6a653594150c987
agnor99 <36055315+agnor99@users.noreply.github.com>
2024-02-08 22:38:59 +0100

    Send full dataslot value on neo<->neo connections (#599)

--- src/main/java/net/neoforged/neoforge/network/handlers/ClientPayloadHandler.java
+++ src/main/java/net/neoforged/neoforge/network/handlers/ClientPayloadHandler.java
@@ -31,6 +31,7 @@ import net.neoforged.neoforge.network.ConfigSync;
 import net.neoforged.neoforge.network.handling.ConfigurationPayloadContext;
 import net.neoforged.neoforge.network.handling.PlayPayloadContext;
 import net.neoforged.neoforge.network.payload.AdvancedAddEntityPayload;
+import net.neoforged.neoforge.network.payload.AdvancedContainerSetDataPayload;
 import net.neoforged.neoforge.network.payload.AdvancedOpenScreenPayload;
 import net.neoforged.neoforge.network.payload.AuxiliaryLightDataPayload;
 import net.neoforged.neoforge.network.payload.ConfigFilePayload;
@@ -152,5 +153,9 @@ public class ClientPayloadHandler {
             context.packetHandler().disconnect(Component.translatable("neoforge.network.aux_light_data.failed", msg.pos(), e.getMessage()));
             return null;
         });
+    }
+
+    public void handle(AdvancedContainerSetDataPayload msg, PlayPayloadContext context) {
+        context.packetHandler().handle(msg.toVanillaPacket());
     }
 }
