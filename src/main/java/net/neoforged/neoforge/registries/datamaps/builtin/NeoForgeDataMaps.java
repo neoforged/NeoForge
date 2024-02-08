4feb1daa87fc7ddf5e92d84507ee4143e6756c20
Matyrobbrt <matyrobbrt@gmail.com>
2024-02-08 20:33:14 +0200

    Review

--- src/main/java/net/neoforged/neoforge/registries/datamaps/builtin/NeoForgeDataMaps.java
+++ src/main/java/net/neoforged/neoforge/registries/datamaps/builtin/NeoForgeDataMaps.java
@@ -39,19 +39,6 @@ public class NeoForgeDataMaps {
             id("compostables"), Registries.ITEM, Compostable.CODEC).synced(Compostable.CHANCE_CODEC, false).build();
 
     /**
-     * The {@linkplain GameEvent} data map that replaces {@link VibrationSystem#VIBRATION_FREQUENCY_FOR_EVENT}.
-     * <p>
-     * The location of this data map is {@code neoforge/data_maps/game_event/vibration_frequencies.json}, and the values are objects with 1 field:
-     * <ul>
-     * <li>{@code frequency}, an integer between {@code 1} and {@code 15} (inclusive) - the vibration frequency of the game event</li>
-     * </ul>
-     *
-     * The use of an integer as the value is also possible, though discouraged in case more options are added in the future.
-     */
-    public static final DataMapType<GameEvent, VibrationFrequency> VIBRATION_FREQUENCIES = DataMapType.builder(
-            id("vibration_frequencies"), Registries.GAME_EVENT, VibrationFrequency.CODEC).synced(VibrationFrequency.FREQUENCY_CODEC, false).build();
-
-    /**
      * The {@linkplain EntityType} data map that replaces {@link Parrot#MOB_SOUND_MAP}.
      * <p>
      * The location of this data map is {@code neoforge/data_maps/entity_type/parrot_imitations.json}, and the values are objects with 1 field:
@@ -64,6 +51,19 @@ public class NeoForgeDataMaps {
     public static final DataMapType<EntityType<?>, ParrotImitation> PARROT_IMITATIONS = DataMapType.builder(
             id("parrot_imitations"), Registries.ENTITY_TYPE, ParrotImitation.CODEC).synced(ParrotImitation.SOUND_CODEC, false).build();
 
+    /**
+     * The {@linkplain GameEvent} data map that replaces {@link VibrationSystem#VIBRATION_FREQUENCY_FOR_EVENT}.
+     * <p>
+     * The location of this data map is {@code neoforge/data_maps/game_event/vibration_frequencies.json}, and the values are objects with 1 field:
+     * <ul>
+     * <li>{@code frequency}, an integer between {@code 1} and {@code 15} (inclusive) - the vibration frequency of the game event</li>
+     * </ul>
+     *
+     * The use of an integer as the value is also possible, though discouraged in case more options are added in the future.
+     */
+    public static final DataMapType<GameEvent, VibrationFrequency> VIBRATION_FREQUENCIES = DataMapType.builder(
+            id("vibration_frequencies"), Registries.GAME_EVENT, VibrationFrequency.CODEC).synced(VibrationFrequency.FREQUENCY_CODEC, false).build();
+
     private static ResourceLocation id(final String name) {
         return new ResourceLocation(NeoForgeVersion.MOD_ID, name);
     }
@@ -71,7 +71,7 @@ public class NeoForgeDataMaps {
     @SubscribeEvent
     private static void register(final RegisterDataMapTypesEvent event) {
         event.register(COMPOSTABLES);
-        event.register(VIBRATION_FREQUENCIES);
         event.register(PARROT_IMITATIONS);
+        event.register(VIBRATION_FREQUENCIES);
     }
 }
