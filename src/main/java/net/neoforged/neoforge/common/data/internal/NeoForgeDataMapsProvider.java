09e74b2284bcfe9e1f6eef8f022e79669c46f676
Matyrobbrt <65940752+Matyrobbrt@users.noreply.github.com>
2024-02-11 01:01:32 +0200

    Allow configuring furnace fuels as a data map (#590)

--- src/main/java/net/neoforged/neoforge/common/data/internal/NeoForgeDataMapsProvider.java
+++ src/main/java/net/neoforged/neoforge/common/data/internal/NeoForgeDataMapsProvider.java
@@ -11,14 +11,17 @@ import java.util.concurrent.CompletableFuture;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.animal.Parrot;
 import net.minecraft.world.level.block.ComposterBlock;
+import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.level.gameevent.vibrations.VibrationSystem;
 import net.neoforged.fml.util.ObfuscationReflectionHelper;
 import net.neoforged.neoforge.common.data.DataMapProvider;
 import net.neoforged.neoforge.registries.datamaps.builtin.Compostable;
+import net.neoforged.neoforge.registries.datamaps.builtin.FurnaceFuel;
 import net.neoforged.neoforge.registries.datamaps.builtin.NeoForgeDataMaps;
 import net.neoforged.neoforge.registries.datamaps.builtin.ParrotImitation;
 import net.neoforged.neoforge.registries.datamaps.builtin.VibrationFrequency;
@@ -32,6 +35,12 @@ public class NeoForgeDataMapsProvider extends DataMapProvider {
     protected void gather() {
         final var compostables = builder(NeoForgeDataMaps.COMPOSTABLES);
         ComposterBlock.COMPOSTABLES.forEach((item, chance) -> compostables.add(item.asItem().builtInRegistryHolder(), new Compostable(chance), false));
+
+        final var fuels = builder(NeoForgeDataMaps.FURNACE_FUELS);
+        AbstractFurnaceBlockEntity.buildFuels((value, time) -> value.ifLeft(item -> fuels.add(item.builtInRegistryHolder(), new FurnaceFuel(time), false))
+                .ifRight(tag -> fuels.add(tag, new FurnaceFuel(time), false)));
+        // Mojang decided to use an exclusion tag for nether wood
+        fuels.remove(ItemTags.NON_FLAMMABLE_WOOD);
 
         final var vibrationFrequencies = builder(NeoForgeDataMaps.VIBRATION_FREQUENCIES);
         ((Object2IntMap<GameEvent>) VibrationSystem.VIBRATION_FREQUENCY_FOR_EVENT)
--- src/main/java/net/neoforged/neoforge/registries/datamaps/builtin/NeoForgeDataMaps.java
+++ src/main/java/net/neoforged/neoforge/registries/datamaps/builtin/NeoForgeDataMaps.java
@@ -10,7 +10,10 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.animal.Parrot;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.block.ComposterBlock;
+import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.level.gameevent.vibrations.VibrationSystem;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -38,6 +41,21 @@ public class NeoForgeDataMaps {
     public static final DataMapType<Item, Compostable> COMPOSTABLES = DataMapType.builder(
             id("compostables"), Registries.ITEM, Compostable.CODEC).synced(Compostable.CHANCE_CODEC, false).build();
 
+    /**
+     * The {@linkplain Item} data map that replaces {@link AbstractFurnaceBlockEntity#getFuel()}.
+     * <p>
+     * The location of this data map is {@code neoforge/data_maps/item/furnace_fuels.json}, and the values are objects with 1 field:
+     * <ul>
+     * <li>{@code burn_time}, a positive integer - how long the item will burn, in ticks</li>
+     * </ul>
+     *
+     * The use of a integer as the value is also possible, though discouraged in case more options are added in the future.
+     *
+     * @apiNote Use {@link net.neoforged.neoforge.common.extensions.IItemExtension#getBurnTime(ItemStack, RecipeType)} for NBT-sensitive burn times. That method takes precedence over the data map.
+     * @implNote This data map will be empty when connected to a Vanilla server.
+     */
+    public static final DataMapType<Item, FurnaceFuel> FURNACE_FUELS = DataMapType.builder(
+            id("furnace_fuels"), Registries.ITEM, FurnaceFuel.CODEC).synced(FurnaceFuel.BURN_TIME_CODEC, false).build();
     /**
      * The {@linkplain EntityType} data map that replaces {@link Parrot#MOB_SOUND_MAP}.
      * <p>
@@ -71,6 +89,7 @@ public class NeoForgeDataMaps {
     @SubscribeEvent
     private static void register(final RegisterDataMapTypesEvent event) {
         event.register(COMPOSTABLES);
+        event.register(FURNACE_FUELS);
         event.register(PARROT_IMITATIONS);
         event.register(VIBRATION_FREQUENCIES);
     }
