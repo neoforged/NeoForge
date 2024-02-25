ae5042c6633cb0dd897e1e9d8a1380b92f1150e8
Apex <robsonlawson3@gmail.com>
2024-02-25 23:37:30 +0000

    Add test item to ExperimentalItemTest to test new FeatureElement extension using server config

--- tests/src/main/java/net/neoforged/neoforge/oldtest/ExperimentalItemTest.java
+++ tests/src/main/java/net/neoforged/neoforge/oldtest/ExperimentalItemTest.java
@@ -12,11 +12,15 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.common.ModConfigSpec;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+// TODO: Remove FeatureFlags.MOD_EXPERIMENTAL
 @Mod(ExperimentalItemTest.ID)
 public final class ExperimentalItemTest {
     public static final String ID = "experimental_item_test";
@@ -29,9 +33,31 @@ public final class ExperimentalItemTest {
     public static final DeferredItem<Item> EXPERIMENTAL_ITEM = ITEMS.registerSimpleItem("experimental_item", new Item.Properties().requiredFeatures(FeatureFlags.MOD_EXPERIMENTAL));
     public static final DeferredBlock<Block> EXPERIMENTAL_BLOCK = BLOCKS.registerBlock("experimental_block", Block::new, BlockBehaviour.Properties.ofFullCopy(Blocks.STONE).requiredFeatures(FeatureFlags.MOD_EXPERIMENTAL));
     public static final DeferredItem<BlockItem> EXPERIMENTAL_BLOCK_ITEM = ITEMS.registerSimpleBlockItem(EXPERIMENTAL_BLOCK);
+    public static final DeferredItem<ConfigItem> CONFIG_ITEM = ITEMS.registerItem("config_item", ConfigItem::new);
+
+    public static final ModConfigSpec.BooleanValue ENABLE_EXP_ITEM;
+
+    static {
+        var builder = new ModConfigSpec.Builder();
+        ENABLE_EXP_ITEM = builder
+                .comment("Enable the experimental item: '%s'".formatted(CONFIG_ITEM.getId()))
+                .define("enable.%s".formatted(CONFIG_ITEM.getId().getPath()), false);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, builder.build());
+    }
 
     public ExperimentalItemTest(IEventBus bus) {
         ITEMS.register(bus);
         BLOCKS.register(bus);
+    }
+
+    public static final class ConfigItem extends Item {
+        public ConfigItem(Properties properties) {
+            super(properties);
+        }
+
+        @Override
+        public boolean isDisabled() {
+            return !ENABLE_EXP_ITEM.get();
+        }
     }
 }
