fd98e95647046149a00b15f43d599e2a517b85c8
Apex <robsonlawson3@gmail.com>
2024-02-26 22:27:31 +0000

    Add comments to ConfigFlagItemTest

--- tests/src/main/java/net/neoforged/neoforge/oldtest/ConfigFlagItemTest.java
+++ tests/src/main/java/net/neoforged/neoforge/oldtest/ConfigFlagItemTest.java
@@ -22,12 +22,23 @@ import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+/*
+    Current known issues
+        When playing on a server elements do not sync their enabled states correctly
+            Since server configs are currently only synced during player login
+            If the config changes live while you are connected to a server
+            elements will remain in their previous enabled state until player relogs or server restarts
+            '/reload' does not fix this issue
+ */
 @Mod(ConfigFlagItemTest.ID)
 public final class ConfigFlagItemTest {
     public static final String ID = "config_flag_item_test";
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(ID);
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(ID);
 
+    // the following elements are always registered to the game
+    // but override the new 'isDisabled' method from IFeatureElementExtension
+    // telling the game to only enable them when the config returns 'true'
     public static final DeferredItem<ConfigItem> CONFIG_ITEM = ITEMS.registerItem("config_item", ConfigItem::new);
     public static final DeferredBlock<ConfigBlock> CONFIG_BLOCK = BLOCKS.registerBlock("config_block", ConfigBlock::new, BlockBehaviour.Properties.ofLegacyCopy(Blocks.STONE));
 
@@ -41,6 +52,10 @@ public final class ConfigFlagItemTest {
                 .comment("Enable the Item: '%s'".formatted(id))
                 .define("enable.%s".formatted(id.getPath()), false);
 
+        // these flags return 'true' when item is enabled
+        // and 'false' when item is disabled
+        // they will need negating in the 'isDisabled' methods
+        // to match the logic 'true' -> 'disabled' not 'true' -> 'enabled'
         ENABLE_CONFIG_ITEM = config.apply(CONFIG_ITEM.getId());
         ENABLE_CONFIG_BLOCK = config.apply(CONFIG_BLOCK.getId());
 
@@ -48,11 +63,21 @@ public final class ConfigFlagItemTest {
     }
 
     public ConfigFlagItemTest(IEventBus bus) {
+        // take note on how we register a bar minimum block item for our block
+        // this item will still be marked as disabled via the config
+        // as BlockItem delegates its 'isDisabled' to the associated block
+        // this logic should also be true for 'SpawnEgg -> EntityType' bindings
         BLOCKS.getEntries().forEach(ITEMS::registerSimpleBlockItem);
 
         ITEMS.register(bus);
         BLOCKS.register(bus);
 
+        // add our elements to creative mode tabs
+        // while we could be checking if the items are enabled
+        // the internal creative mode tab logic already does this for us
+        // so no need to check enabled state here
+        //
+        // disabled items are not visible in creative mode tabs
         bus.addListener(BuildCreativeModeTabContentsEvent.class, event -> {
             if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
                 event.accept(CONFIG_ITEM);
