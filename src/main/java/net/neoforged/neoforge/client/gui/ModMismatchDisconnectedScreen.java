11ae1af33e9b57c3f1b0354c9e1e44139a105f3f
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2024-02-29 16:22:29 +0100

    Removed code that mapped a channel to the mod with the same namespace
    also updated some javadoc

--- src/main/java/net/neoforged/neoforge/client/gui/ModMismatchDisconnectedScreen.java
+++ src/main/java/net/neoforged/neoforge/client/gui/ModMismatchDisconnectedScreen.java
@@ -6,7 +6,6 @@
 package net.neoforged.neoforge.client.gui;
 
 import com.mojang.blaze3d.vertex.Tesselator;
-import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -24,7 +23,6 @@ import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.MultiLineLabel;
 import net.minecraft.client.gui.narration.NarrationElementOutput;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.HoverEvent;
 import net.minecraft.network.chat.HoverEvent.Action;
@@ -33,8 +31,6 @@ import net.minecraft.network.chat.Style;
 import net.minecraft.network.chat.TextColor;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.FormattedCharSequence;
-import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.ModList;
 import net.neoforged.fml.loading.FMLPaths;
 import net.neoforged.neoforge.client.gui.widget.ScrollPanel;
 import net.neoforged.neoforge.common.I18nExtension;
@@ -107,7 +103,7 @@ public class ModMismatchDisconnectedScreen extends Screen {
             //The raw list of the strings in a table row, the components may still be too long for the final table and will be split up later. The first row element may have a style assigned to it that will be used for the whole content row.
             List<Row> rows = new ArrayList<>();
             if (!collapsedChannelData.isEmpty()) {
-                //Each table row contains the mod name and the reason for the corresponding channel mismatch.
+                //Each table row contains the channel namespace and the reason for the corresponding channel mismatch.
                 rows.add(new Row(Component.translatable("fml.modmismatchscreen.table.channelname"), Component.translatable("fml.modmismatchscreen.table.reason")));
                 int i = 0;
                 for (Map.Entry<ResourceLocation, Pair<Integer, Component>> channelData : collapsedChannelData.entrySet()) {
@@ -127,13 +123,14 @@ public class ModMismatchDisconnectedScreen extends Screen {
 
         /**
          * Collapses quasi-duplicate channel mismatch entries into single list entries to reduce repetition of entries in the final list.
-         * Quasi-duplicate channel mismatch entries share the same channel namespace and mismatch reasons, and it is thus very likely that they are caused
-         * by one and the same mod (that has registered all of these channels) missing/mismatching between client and server.
+         * Quasi-duplicate channel mismatch entries share the same channel namespace and mismatch reasons. Even though these mismatches do not need to be causally related,
+         * there is a not inconsiderable likelihood that they are, and to increase usability of the screen, we summarize them into one entry to allow mod users
+         * to focus on the likely important parts of the channel mismatch data (which is the namespace of the mismatching channels and the exact mismatch reason).
          *
          * @param mismatchedChannelData The raw mismatched channel data received from the server, which might contain quasi-duplicate entries
          * @return A map containing deduplicated channel mismatch entries. For each quasi-duplicate group, only the first encountered channel id is kept,
          *         and all other quasi-duplicate channels then increment the associated repetition count that is mapped to that first channel id.
-         *         Finally, the (unchanged) mismatch reason (which is the same for all quasi-duplicate entries) also gets mapped to the channel id.
+         *         The mismatch reason (which remains unchanged and is the same for all quasi-duplicate entries) also gets mapped to the channel id.
          */
         private Map<ResourceLocation, Pair<Integer, Component>> collapseChannelData(Map<ResourceLocation, Component> mismatchedChannelData) {
             Map<ResourceLocation, Pair<Integer, Component>> repetitions = new LinkedHashMap<>();
@@ -160,11 +157,11 @@ public class ModMismatchDisconnectedScreen extends Screen {
         }
 
         /**
-         * Splits the raw mod name and mismatch reason strings, making them use multiple lines if needed, to fit within the table dimensions.
+         * Splits the raw channel namespace and mismatch reason strings, making them use multiple lines if needed, to fit within the table dimensions.
          * The style assigned to the name element is then applied to the entire content row.
          *
-         * @param name   The first element of the content row, usually representing a table section header or the name of a mod entry
-         * @param reason The second element of the content row, usually representing the reason why the mod is mismatched
+         * @param name   The first element of the content row, usually representing a table section header or a channel namespace entry
+         * @param reason The second element of the content row, usually representing the reason why the channel is mismatched
          * @return A list of table rows consisting of 2 elements each which consist of the same content as was given by the parameters, but split up to fit within the table dimensions.
          */
         private List<Pair<FormattedCharSequence, FormattedCharSequence>> splitLineToWidth(MutableComponent name, MutableComponent reason) {
@@ -181,32 +178,22 @@ public class ModMismatchDisconnectedScreen extends Screen {
         }
 
         /**
-         * Uses the given channel id to return a component with the name of the mod that likely owns the channel. If no such mod is found, the namespace of the channel id is used instead.
-         * The style assigned to the returned component contains the color of the entry, a hover event containing the channel id, and an optional click event which, if present, opens the homepage of the mod.
+         * Returns a component with the namespace of the given channel id. The component is colored in the given color, which will be used for the whole content row,
+         * and has a hover text mentioning an exemplary mismatching channel id plus how many other channels with the same namespace were mismatching with the same reason.
          *
-         * @param id              The id of the mismatched channel. Used to query the name of the mod that has likely registered the channel in order to use and display its name and homepage URL.
-         * @param repetitionCount How many other channels of the same mod failed negotiation with the same error message. Displayed in the hover tooltip.
-         * @param color           Defines the color of the returned channel name component.
-         * @return A component with the mod name (if available) as the main text component, and an assigned color which will be used for the whole content row.
+         * @param id              The id of the exemplary mismatched channel. Its namespace is used for the component text.
+         * @param repetitionCount How many channels with the same namespace failed negotiation with the same error message. Displayed in the hover tooltip.
+         * @param color           Defines the color of the returned channel namespace component.
+         * @return A component with the channel namespace as the main text component, and an assigned color which will be used for the whole content row.
          */
         private MutableComponent toChannelNameComponent(ResourceLocation id, int repetitionCount, ChatFormatting color) {
-            String modId = id.getNamespace();
-            Optional<? extends ModContainer> mod = ModList.get().getModContainerById(modId);
-            String name = mod.map(m -> m.getModInfo().getDisplayName()).orElse(modId);
-            String url = mod.flatMap(container -> container.getModInfo().getModURL())
-                    .map(URL::toString)
-                    .orElse("");
-            MutableComponent result = Component.literal(name).withStyle(color);
+            MutableComponent namespaceComponent = Component.literal(id.getNamespace()).withStyle(color);
             MutableComponent hoverText = Component.literal(id.toString());
             if (repetitionCount > 1)
                 hoverText.append(Component.literal(" [+%s more]".formatted(repetitionCount)).withStyle(ChatFormatting.GRAY));
-            if (!url.isEmpty()) {
-                hoverText.append(Component.literal("\n").append(Component.translatable("fml.modmismatchscreen.table.visit.mod_page", id.toString())));
-                result = result.withStyle(s -> s.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, url)));
-            }
 
-            result = result.withStyle(s -> s.withHoverEvent(new HoverEvent(Action.SHOW_TEXT, hoverText)));
-            return result;
+            namespaceComponent = namespaceComponent.withStyle(s -> s.withHoverEvent(new HoverEvent(Action.SHOW_TEXT, hoverText)));
+            return namespaceComponent;
         }
 
         @Override
