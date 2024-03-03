9fd14d8ba2a48ecea8cca83f93c952d822dff8fb
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2024-03-02 22:13:00 -0800

    revert some more task stuff

--- src/main/java/net/neoforged/neoforge/network/configuration/ModdedConfigurationPhaseCompleted.java
+++ src/main/java/net/neoforged/neoforge/network/configuration/ModdedConfigurationPhaseCompleted.java
@@ -6,9 +6,8 @@
 package net.neoforged.neoforge.network.configuration;
 
 import java.util.function.Consumer;
-import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.network.ConfigurationTask;
 import net.minecraft.server.network.ServerConfigurationPacketListenerImpl;
 import net.neoforged.neoforge.internal.versions.neoforge.NeoForgeVersion;
 import org.jetbrains.annotations.ApiStatus;
@@ -19,12 +18,12 @@ import org.jetbrains.annotations.ApiStatus;
  * @param listener The listener that is used to configure the client.
  */
 @ApiStatus.Internal
-public record ModdedConfigurationPhaseCompleted(ServerConfigurationPacketListenerImpl listener) implements ConfigurationTask {
+public record ModdedConfigurationPhaseCompleted(ServerConfigurationPacketListenerImpl listener) implements ICustomConfigurationTask {
     private static final ResourceLocation ID = new ResourceLocation(NeoForgeVersion.MOD_ID, "modded_configuration_phase_completed");
     public static final Type TYPE = new Type(ID);
 
     @Override
-    public void start(Consumer<Packet<?>> sender) {
+    public void run(Consumer<CustomPacketPayload> sender) {
         listener().onModdedConfigurationPhaseEnded();
     }
 
