neoforged/NeoForge

commit 8be0f8fc7e7713c84d8fb89afc843c90d7582b4d

Remove `shouldDrawSurvivalElements()` condition in `MOUNT_HEALTH` (#627)

src/main/java/net/neoforged/neoforge/client/gui/overlay/VanillaGuiOverlay.java
@@ -105,7 +105,7 @@ public enum VanillaGuiOverlay {
         }
     }),
     MOUNT_HEALTH("mount_health", (gui, guiGraphics, partialTick, screenWidth, screenHeight) -> {
-        if (!gui.getMinecraft().options.hideGui && gui.shouldDrawSurvivalElements()) {
+        if (!gui.getMinecraft().options.hideGui) {
             gui.setupOverlayRenderState(true, false);
             gui.renderHealthMount(screenWidth, screenHeight, guiGraphics);
         }

commit 2207080fe127c100178fdacd4d0dc80ccb02dc14

Fix bug with non Player Camera Entities causing crashes introduced by #627

src/main/java/net/neoforged/neoforge/client/gui/overlay/ExtendedGui.java
@@ -119,6 +119,10 @@ public class ExtendedGui extends Gui {
         return minecraft.gameMode.canHurtPlayer() && minecraft.getCameraEntity() instanceof Player;
     }
 
+    public boolean shouldDrawPlayerElements() {
+        return minecraft.getCameraEntity() instanceof Player;
+    }
+
     protected void renderSubtitles(GuiGraphics guiGraphics) {
         this.subtitleOverlay.render(guiGraphics);
     }

src/main/java/net/neoforged/neoforge/client/gui/overlay/VanillaGuiOverlay.java
@@ -105,7 +105,7 @@ public enum VanillaGuiOverlay {
         }
     }),
     MOUNT_HEALTH("mount_health", (gui, guiGraphics, partialTick, screenWidth, screenHeight) -> {
-        if (!gui.getMinecraft().options.hideGui) {
+        if (!gui.getMinecraft().options.hideGui && gui.shouldDrawPlayerElements()) {
             gui.setupOverlayRenderState(true, false);
             gui.renderHealthMount(screenWidth, screenHeight, guiGraphics);
         }