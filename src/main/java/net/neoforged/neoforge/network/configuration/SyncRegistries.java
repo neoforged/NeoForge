dedb507d0e41ca9eb6d9d46980c2b3c78cfd3868
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-03-05 14:22:24 -0500

    Force tags to be synced again after registries

--- src/main/java/net/neoforged/neoforge/network/configuration/SyncRegistries.java
+++ src/main/java/net/neoforged/neoforge/network/configuration/SyncRegistries.java
@@ -6,12 +6,17 @@
 package net.neoforged.neoforge.network.configuration;
 
 import java.util.function.Consumer;
+
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.common.ClientboundUpdateTagsPacket;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagNetworkSerialization;
 import net.neoforged.neoforge.internal.versions.neoforge.NeoForgeVersion;
 import net.neoforged.neoforge.network.payload.FrozenRegistrySyncCompletedPayload;
 import net.neoforged.neoforge.network.payload.FrozenRegistrySyncStartPayload;
 import net.neoforged.neoforge.registries.RegistryManager;
+import net.neoforged.neoforge.server.ServerLifecycleHooks;
 import org.jetbrains.annotations.ApiStatus;
 
 /**
@@ -27,6 +32,15 @@ public record SyncRegistries() implements ICustomConfigurationTask {
         sender.accept(new FrozenRegistrySyncStartPayload(RegistryManager.getRegistryNamesForSyncToClient()));
         RegistryManager.generateRegistryPackets(false).forEach(sender);
         sender.accept(new FrozenRegistrySyncCompletedPayload());
+    }
+
+    @Override
+    public void start(Consumer<Packet<?>> sender) {
+        ICustomConfigurationTask.super.start(sender);
+        // Sync tags a second time. This is needed in 20.4 because tags use integer IDs for syncing, and we do the
+        // registry sync after vanilla sends the tags packet.
+        // TODO remove in 20.5, where this is fixed properly
+        sender.accept(new ClientboundUpdateTagsPacket(TagNetworkSerialization.serializeTagsToNetwork(ServerLifecycleHooks.getCurrentServer().registries())));
     }
 
     @Override
