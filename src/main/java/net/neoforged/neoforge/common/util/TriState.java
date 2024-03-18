71107ae0e336fb8f07f46b9f86bb7af50d4ef2c7
XFactHD <xfacthd@gmx.de>
2024-03-18 23:31:05 +0100

    Dots

--- src/main/java/net/neoforged/neoforge/common/util/TriState.java
+++ src/main/java/net/neoforged/neoforge/common/util/TriState.java
@@ -6,19 +6,19 @@
 package net.neoforged.neoforge.common.util;
 
 /**
- * Represents a boolean value that can be {@code true}, {@code false} or refer to a default value
+ * Represents a boolean value that can be {@code true}, {@code false} or refer to a default value.
  */
 public enum TriState {
     /**
-     * Represents the boolean value {@code true}
+     * Represents the boolean value {@code true}.
      */
     TRUE,
     /**
-     * Represents a "default" value, often used as a fallback
+     * Represents a "default" value, often used as a fallback.
      */
     DEFAULT,
     /**
-     * Represents the boolean value {@code false}
+     * Represents the boolean value {@code false}.
      */
     FALSE
 }
