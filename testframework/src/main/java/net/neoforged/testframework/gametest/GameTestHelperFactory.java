6f7b4e45abb1ad54c23fbfca5b1ce55c3e453497
Sirttas <calloch.gael@gmail.com>
2024-03-31 20:51:55 +0200

    License

--- testframework/src/main/java/net/neoforged/testframework/gametest/GameTestHelperFactory.java
+++ testframework/src/main/java/net/neoforged/testframework/gametest/GameTestHelperFactory.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) NeoForged and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
 package net.neoforged.testframework.gametest;
 
 import java.lang.invoke.MethodType;
