016c8ef36316ce2692c0d3fb57ecbcd7692f3fa5
Tom Tanticle <113810920+Tanticle@users.noreply.github.com>
2024-04-07 22:41:10 +0200

    Reworked Javadoc.

--- src/main/java/net/neoforged/neoforge/event/level/BlockDropsEvent.java
+++ src/main/java/net/neoforged/neoforge/event/level/BlockDropsEvent.java
@@ -10,12 +10,10 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
+
 /**
  * Fired when a block is destroyed AFTER drops have been determined, but have not yet spawned.
- * - Cancellation will result in the drops not being spawned.
- * - Breaker may be null if the block was not destroyed by an entity.
- * - The drops list is immutable and cannot be modified. Use LootTableModifiers instead
- * - It is safe to modify the Block in this event, as it has already been replaced.
+ * It is safe to modify the Block in this event, as it has already been replaced.
  */
 public class BlockDropsEvent extends BlockEvent implements ICancellableEvent {
 
@@ -25,6 +23,9 @@ public class BlockDropsEvent extends BlockEvent implements ICancellableEvent {
 
     private boolean dropXpWhenCancelled;
 
+    /**
+     * Cancellation will result in the drops not being spawned.
+     */
     public BlockDropsEvent(LevelAccessor level, BlockPos pos, BlockState state, List<ItemStack> drops, @Nullable Entity destroyer, ItemStack tool) {
         super(level, pos, state);
         this.drops = drops;
@@ -33,18 +34,34 @@ public class BlockDropsEvent extends BlockEvent implements ICancellableEvent {
         this.dropXpWhenCancelled = true;
     }
 
+    /**
+     * Sets whether XP Orb Entities should still be spawned when the event is cancelled.
+     * @param shouldDrop Whether XP should be spawned.
+     */
     public void setDropXpWhenCancelled(boolean shouldDrop) {
         this.dropXpWhenCancelled = shouldDrop;
     }
 
+    /**
+     * Returns a list of drops determined for this broken block.
+     * @return An immutable list of ItemStacks.
+     */
     public List<ItemStack> getDrops() {
         return drops;
     }
 
-    public Entity getDestroyingEntity() {
+    /**
+     * Returns the entity associated with this broken block. Might be null.
+     * @return The entity responsible for the breaking of this block, or null.
+     */
+    public @Nullable Entity getDestroyingEntity() {
         return destroyingEntity;
     }
 
+    /**
+     * Returns the tool associated with this broken block.
+     * @return The used tool as ItemStack, or ItemStack.EMPTY
+     */
     public ItemStack getTool() {
         return tool;
     }
