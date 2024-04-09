1a9847ffacc3e2211bad09012c65244f92234fa9
Apex <robsonlawson3@gmail.com>
2024-04-09 06:42:22 +0100

    Move cancellation javadocs to setCancelled overridden method

--- src/main/java/net/neoforged/neoforge/event/entity/item/ItemAllowPickupEvent.java
+++ src/main/java/net/neoforged/neoforge/event/entity/item/ItemAllowPickupEvent.java
@@ -7,22 +7,19 @@ package net.neoforged.neoforge.event.entity.item;
 
 import net.minecraft.world.entity.item.ItemEntity;
 import net.neoforged.bus.api.ICancellableEvent;
-import net.neoforged.neoforge.common.NeoForge;
 import org.jetbrains.annotations.ApiStatus;
 
-/**
- * Event which is fired to determine whether item pickup should be allowed.
- * <p>
- * This event is {@linkplain ICancellableEvent cancellable}.
- * When cancelled item pickup is disallowed.
- * <p>
- * This event does not have a {@linkplain Result result}.
- * <p>
- * This event is fired on {@linkplain NeoForge#EVENT_BUS}
- */
 public class ItemAllowPickupEvent extends ItemEvent implements ICancellableEvent {
     @ApiStatus.Internal
     public ItemAllowPickupEvent(ItemEntity itemEntity) {
         super(itemEntity);
+    }
+
+    /**
+     * Cancelling this event will disallow item from being picked up.
+     */
+    @Override
+    public void setCanceled(boolean canceled) {
+        ICancellableEvent.super.setCanceled(canceled);
     }
 }
