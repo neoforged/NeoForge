08fa4f580b6c629afed223e4cdf534095877ca9f
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-04-11 14:23:36 -0400

    linter

--- src/main/java/net/neoforged/neoforge/server/command/DumpCommand.java
+++ src/main/java/net/neoforged/neoforge/server/command/DumpCommand.java
@@ -14,7 +14,6 @@ import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
 import com.mojang.logging.LogUtils;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
