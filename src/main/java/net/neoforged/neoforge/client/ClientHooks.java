8d4c9c73668a2a183cc218df8de5b52a595ef95d
Luke Bemish <lukebemish@lukebemish.dev>
2024-04-15 08:29:53 -0500

    Apply spotless

--- src/main/java/net/neoforged/neoforge/client/ClientHooks.java
+++ src/main/java/net/neoforged/neoforge/client/ClientHooks.java
@@ -104,7 +104,6 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.tooltip.TooltipComponent;
 import net.minecraft.world.item.ArmorMaterial;
