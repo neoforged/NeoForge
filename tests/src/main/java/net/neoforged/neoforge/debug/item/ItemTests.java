3e9009715aa377071f8913a3b47354b49f02cb22
Minecraftschurli <minecraftschurli@gmail.com>
2024-04-18 12:07:34 +0200

    Remove commented out spawnsEntity method because it just called super anyway

--- tests/src/main/java/net/neoforged/neoforge/debug/item/ItemTests.java
+++ tests/src/main/java/net/neoforged/neoforge/debug/item/ItemTests.java
@@ -129,12 +129,6 @@ public class ItemTests {
                 }
                 return sup;
             }
-
-            // Porting 1.20.5 replacement?
-//            @Override
-//            public boolean spawnsEntity(@Nullable CompoundTag p_43231_, EntityType<?> p_43232_) {
-//                return super.spawnsEntity(p_43231_, p_43232_);
-//            }
         })
                 .tab(CreativeModeTabs.SPAWN_EGGS).withModel(builder -> builder.parent(new ModelFile.UncheckedModelFile(new ResourceLocation("minecraft:item/template_spawn_egg"))));
 
