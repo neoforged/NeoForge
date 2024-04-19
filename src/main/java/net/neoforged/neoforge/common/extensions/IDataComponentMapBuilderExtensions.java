5c211f791c4bfa344f449ada85f0ef4f06e90625
Apex <robsonlawson3@gmail.com>
2024-04-19 13:57:24 +0100

    Convert patches to extension ifaces where possible

---
+++ src/main/java/net/neoforged/neoforge/common/extensions/IDataComponentMapBuilderExtensions.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) NeoForged and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
+package net.neoforged.neoforge.common.extensions;
+
+import java.util.function.Supplier;
+import javax.annotation.Nullable;
+import net.minecraft.core.component.DataComponentMap;
+import net.minecraft.core.component.DataComponentType;
+
+public interface IDataComponentMapBuilderExtensions {
+    private DataComponentMap.Builder self() {
+        return (DataComponentMap.Builder) this;
+    }
+
+    default <T> DataComponentMap.Builder set(Supplier<? extends DataComponentType<T>> componentType, @Nullable T value) {
+        return self().set(componentType.get(), value);
+    }
+}
