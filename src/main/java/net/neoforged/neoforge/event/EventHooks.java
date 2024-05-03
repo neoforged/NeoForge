eba12980f466db885074d6a2d4e52aa0f52f615c
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-05-03 19:36:32 -0400

    remove unused import

--- src/main/java/net/neoforged/neoforge/event/EventHooks.java
+++ src/main/java/net/neoforged/neoforge/event/EventHooks.java
@@ -66,7 +66,6 @@ import net.minecraft.world.entity.projectile.ThrownEnderpearl;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.ItemStackLinkedSet;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.item.crafting.RecipeType;
