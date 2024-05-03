54a5b3f038999e89c59f05d980e953cefeebbe3d
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-05-03 19:25:20 -0400

    Allow multiple of same itemstack for BuildCreativeModeTabContentsEvent
    Fixes https://github.com/neoforged/NeoForge/issues/886

--- src/main/java/net/neoforged/neoforge/event/EventHooks.java
+++ src/main/java/net/neoforged/neoforge/event/EventHooks.java
@@ -989,13 +989,7 @@ public class EventHooks {
      */
     @ApiStatus.Internal
     public static void onCreativeModeTabBuildContents(CreativeModeTab tab, ResourceKey<CreativeModeTab> tabKey, CreativeModeTab.DisplayItemsGenerator originalGenerator, CreativeModeTab.ItemDisplayParameters params, CreativeModeTab.Output output) {
-        final var entries = new MutableHashedLinkedMap<ItemStack, CreativeModeTab.TabVisibility>(ItemStackLinkedSet.TYPE_AND_TAG,
-                (key, left, right) -> {
-                    //throw new IllegalStateException("Accidentally adding the same item stack twice " + key.getDisplayName().getString() + " to a Creative Mode Tab: " + tab.getDisplayName().getString());
-                    // Vanilla adds enchanting books twice in both visibilities.
-                    // This is just code cleanliness for them. For us lets just increase the visibility and merge the entries.
-                    return CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS;
-                });
+        final var entries = new MutableHashedLinkedMap<ItemStack, CreativeModeTab.TabVisibility>();
 
         originalGenerator.accept(params, (stack, vis) -> {
             if (stack.getCount() != 1)
