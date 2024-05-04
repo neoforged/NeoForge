4bac1bcc9fa6d2518032008a4612b1bd964b260c
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-05-04 09:06:45 -0400

    Add helper method for registering builtin-pack easier

--- src/main/java/net/neoforged/neoforge/event/AddPackFindersEvent.java
+++ src/main/java/net/neoforged/neoforge/event/AddPackFindersEvent.java
@@ -5,12 +5,21 @@
 
 package net.neoforged.neoforge.event;
 
+import java.util.Optional;
 import java.util.function.Consumer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.PackLocationInfo;
+import net.minecraft.server.packs.PackSelectionConfig;
 import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.PathPackResources;
+import net.minecraft.server.packs.repository.BuiltInPackSource;
 import net.minecraft.server.packs.repository.Pack;
 import net.minecraft.server.packs.repository.PackRepository;
+import net.minecraft.server.packs.repository.PackSource;
 import net.minecraft.server.packs.repository.RepositorySource;
 import net.neoforged.bus.api.Event;
+import net.neoforged.fml.ModList;
 import net.neoforged.fml.event.IModBusEvent;
 
 /**
@@ -43,5 +52,27 @@ public class AddPackFindersEvent extends Event implements IModBusEvent {
      */
     public PackType getPackType() {
         return packType;
+    }
+
+    /**
+     * Helper method to register a pack found under the `resources/` folder.
+     *
+     * @param packLocation    Location of the pack to load. Namespace should be the modid of the pack owner and path is the location under `resources/` folder
+     * @param packType        Whether pack is a resourcepack or datapack
+     * @param packNameDisplay The text that shows for the pack on the pack selection screen
+     * @param alwaysActive    Whether the pack is forced active always. If false, players have to manually activate the pack themselves
+     */
+    public void addPackFinders(ResourceLocation packLocation, PackType packType, Component packNameDisplay, boolean alwaysActive) {
+        if (getPackType() == packType) {
+            var resourcePath = ModList.get().getModFileById(packLocation.getNamespace()).getFile().findResource(packLocation.getPath());
+
+            var pack = Pack.readMetaAndCreate(
+                    new PackLocationInfo("builtin/" + packLocation.getNamespace(), packNameDisplay, PackSource.BUILT_IN, Optional.empty()),
+                    BuiltInPackSource.fromName((path) -> new PathPackResources(path, resourcePath)),
+                    packType,
+                    new PackSelectionConfig(alwaysActive, Pack.Position.BOTTOM, false));
+
+            addRepositorySource((packConsumer) -> packConsumer.accept(pack));
+        }
     }
 }
