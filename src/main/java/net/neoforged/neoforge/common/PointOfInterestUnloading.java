49b8bb93e4ee43abf162e61e4c4b24937b771eb0
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-05-06 20:52:53 -0400

    Fixed POI fix properly

--- src/main/java/net/neoforged/neoforge/common/PointOfInterestUnloading.java
+++ src/main/java/net/neoforged/neoforge/common/PointOfInterestUnloading.java
@@ -6,16 +6,11 @@
 package net.neoforged.neoforge.common;
 
 import net.minecraft.core.SectionPos;
-import net.minecraft.server.level.ServerChunkCache;
+import net.minecraft.server.level.ChunkMap;
 import net.minecraft.world.entity.ai.village.poi.PoiManager;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.chunk.ChunkAccess;
-import net.minecraft.world.level.chunk.ChunkSource;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.event.level.ChunkEvent;
 
-@EventBusSubscriber(modid = "neoforge", bus = EventBusSubscriber.Bus.GAME)
 public class PointOfInterestUnloading {
     /**
      * The goal here is to fix the POI memory leak that happens due to
@@ -23,21 +18,16 @@ public class PointOfInterestUnloading {
      * actually removing POIs long after they become irrelevant. We do it here in chunk unload event
      * so that chunk that are fully unloaded now gets the POI removed from the POI cached storage map.
      */
-    @SubscribeEvent
-    public static void OnChunkUnload(ChunkEvent.Unload event) {
-        ChunkSource chunkSource = event.getLevel().getChunkSource();
-        if (chunkSource instanceof ServerChunkCache serverChunkCache) {
-            ChunkAccess chunkAccess = event.getChunk();
-            ChunkPos chunkPos = chunkAccess.getPos();
-            PoiManager poiManager = serverChunkCache.chunkMap.poiManager();
-            poiManager.flush(chunkPos); // Make sure all POI in chunk are saved to disk first.
+    public static void OnChunkUnload(ChunkMap chunkMap, ChunkAccess chunkAccess) {
+        ChunkPos chunkPos = chunkAccess.getPos();
+        PoiManager poiManager = chunkMap.poiManager();
+        poiManager.flush(chunkPos); // Make sure all POI in chunk are saved to disk first.
 
-            // Remove the cached POIs for this chunk's location.
-            int SectionPosMinY = SectionPos.blockToSectionCoord(chunkAccess.getMinBuildHeight());
-            for (int currentSectionY = 0; currentSectionY < chunkAccess.getSectionsCount(); currentSectionY++) {
-                long sectionPosKey = SectionPos.asLong(chunkPos.x, SectionPosMinY + currentSectionY, chunkPos.z);
-                poiManager.remove(sectionPosKey);
-            }
+        // Remove the cached POIs for this chunk's location.
+        int SectionPosMinY = SectionPos.blockToSectionCoord(chunkAccess.getMinBuildHeight());
+        for (int currentSectionY = 0; currentSectionY < chunkAccess.getSectionsCount(); currentSectionY++) {
+            long sectionPosKey = SectionPos.asLong(chunkPos.x, SectionPosMinY + currentSectionY, chunkPos.z);
+            poiManager.remove(sectionPosKey);
         }
     }
 }
