989ed3b542febc47ce542ec07f2f56fee6870df7
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-05-09 13:40:34 -0400

    [1.20.6] Fix several NeoForge command crashes and allow chat OpenFile click action on Integrated Server  (#915)

---
+++ src/main/java/net/neoforged/neoforge/client/command/ClientConfigCommand.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) NeoForged and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
+package net.neoforged.neoforge.client.command;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.builder.ArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import java.io.File;
+import net.minecraft.ChatFormatting;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.ClickEvent;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.neoforged.fml.config.ConfigTracker;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.server.command.ModIdArgument;
+
+public class ClientConfigCommand {
+    public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
+        dispatcher.register(
+                Commands.literal("config").then(ShowFile.register()));
+    }
+
+    public static class ShowFile {
+        static ArgumentBuilder<CommandSourceStack, ?> register() {
+            return Commands.literal("showfileclient").requires(cs -> cs.hasPermission(0)).then(Commands.argument("mod", ModIdArgument.modIdArgument()).executes(ShowFile::showFile));
+        }
+
+        private static int showFile(final CommandContext<CommandSourceStack> context) {
+            final String modId = context.getArgument("mod", String.class);
+            final ModConfig.Type type = ModConfig.Type.CLIENT;
+            final String configFileName = ConfigTracker.INSTANCE.getConfigFileName(modId, type);
+            if (configFileName != null) {
+                File f = new File(configFileName);
+                MutableComponent fileComponent = Component.literal(f.getName()).withStyle(ChatFormatting.UNDERLINE)
+                        .withStyle((style) -> style.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_FILE, f.getAbsolutePath())));
+
+                context.getSource().sendSuccess(() -> Component.translatable("commands.config.getwithtype",
+                        modId, type.toString(), fileComponent), true);
+            } else {
+                context.getSource().sendSuccess(() -> Component.translatable("commands.config.noconfig", modId, type.toString()),
+                        true);
+            }
+            return 0;
+        }
+    }
+}
