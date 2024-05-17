70d54913208173bc59f6e924418c59dbbd29e992
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-05-17 19:36:56 -0400

    Add more javadoc and villagerCanPlantItem hook

--- src/main/java/net/neoforged/neoforge/common/SpecialPlantable.java
+++ src/main/java/net/neoforged/neoforge/common/SpecialPlantable.java
@@ -12,7 +12,7 @@ import net.minecraft.world.level.LevelReader;
 
 /**
  * Intended for mods to help mark if a modded item allows for placing modded plants of any kind or size.
- * Also allows Villagers to properly plant items with this interface on if the Villager has this item in inventory.
+ * Also allows Villagers to properly plant items with this interface on and villagerCanPlantItem returning true.
  * <p></p>
  * People trying to plant modded items should check if item implements this interface.
  * Then check for true from canPlacePlantAtPosition first before calling spawnPlantAtPosition.
@@ -21,7 +21,23 @@ import net.minecraft.world.level.LevelReader;
  * (Note: Vanilla plantable items are BlockItem where you can get their states directly and call canSurvive)
  */
 public interface SpecialPlantable {
-    Boolean canPlacePlantAtPosition(LevelReader level, BlockPos pos, @Nullable Direction direction);
 
+    /**
+     * Checks location if this item can spawn a plant with the given direction attachment point.
+     * @return If plant can spawn
+     */
+    boolean canPlacePlantAtPosition(LevelReader level, BlockPos pos, @Nullable Direction direction);
+
+    /**
+     * Spawns the plant with the given direction attachment point at location.
+     * Ideally called after canPlacePlantAtPosition returns true.
+     */
     void spawnPlantAtPosition(LevelReader level, BlockPos pos, @Nullable Direction direction);
+
+    /**
+     * Whether Villagers can pick up this item and plant it down on any block that extends FarmBlock.
+     */
+    default boolean villagerCanPlantItem() {
+        return false;
+    };
 }
