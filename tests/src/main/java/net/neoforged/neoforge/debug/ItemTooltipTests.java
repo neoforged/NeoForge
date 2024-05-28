e91ff4f2c5c945158d3226d5fb337b53a94c63e6
Apex <robsonlawson3@gmail.com>
2024-05-28 01:43:20 +0100

    Fix item tooltip tests failing

--- tests/src/main/java/net/neoforged/neoforge/debug/ItemTooltipTests.java
+++ tests/src/main/java/net/neoforged/neoforge/debug/ItemTooltipTests.java
@@ -5,27 +5,22 @@
 
 package net.neoforged.neoforge.debug;
 
-import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.network.chat.Component;
+import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
-import net.neoforged.testframework.DynamicTest;
+import net.neoforged.testframework.TestFramework;
 import net.neoforged.testframework.annotation.ForEachTest;
-import net.neoforged.testframework.annotation.TestHolder;
-import net.neoforged.testframework.gametest.EmptyTemplate;
+import net.neoforged.testframework.annotation.OnInit;
 
 @ForEachTest(groups = "item.tooltip.event")
 public class ItemTooltipTests {
-    @GameTest
-    @EmptyTemplate
-    @TestHolder(description = "Tests if tooltips are added in correct orderings")
-    static void itemTooltipTest(DynamicTest test) {
-        var forge = test.eventListeners().forge();
-
-        forge.addListener((ItemTooltipEvent.AfterName event) -> event.getToolTip().add(Component.literal("After Name")));
-        forge.addListener((ItemTooltipEvent.BeforeComponents event) -> event.getToolTip().add(Component.literal("Before Components")));
-        forge.addListener((ItemTooltipEvent.AfterComponents event) -> event.getToolTip().add(Component.literal("After Components")));
-        forge.addListener((ItemTooltipEvent.BeforeDebug event) -> event.getToolTip().add(Component.literal("Before Debug")));
-        forge.addListener((ItemTooltipEvent.AfterDebug event) -> event.getToolTip().add(Component.literal("After Debug")));
-        forge.addListener((ItemTooltipEvent.AfterAll event) -> event.getToolTip().add(Component.literal("After All")));
+    @OnInit
+    static void itemTooltipTest(TestFramework framework) {
+        NeoForge.EVENT_BUS.addListener((ItemTooltipEvent.AfterName event) -> event.getToolTip().add(Component.literal("After Name")));
+        NeoForge.EVENT_BUS.addListener((ItemTooltipEvent.BeforeComponents event) -> event.getToolTip().add(Component.literal("Before Components")));
+        NeoForge.EVENT_BUS.addListener((ItemTooltipEvent.AfterComponents event) -> event.getToolTip().add(Component.literal("After Components")));
+        NeoForge.EVENT_BUS.addListener((ItemTooltipEvent.BeforeDebug event) -> event.getToolTip().add(Component.literal("Before Debug")));
+        NeoForge.EVENT_BUS.addListener((ItemTooltipEvent.AfterDebug event) -> event.getToolTip().add(Component.literal("After Debug")));
+        NeoForge.EVENT_BUS.addListener((ItemTooltipEvent.AfterAll event) -> event.getToolTip().add(Component.literal("After All")));
     }
 }
