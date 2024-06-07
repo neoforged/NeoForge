d5a0312becd760fc019e90b9a05933eaf92ef6c9
RaymondBlaze <raymond-blaze@outlook.com>
2024-06-08 01:46:29 +0800

    Move EffectCure defaults to data map

--- src/main/java/net/neoforged/neoforge/common/EffectCure.java
+++ src/main/java/net/neoforged/neoforge/common/EffectCure.java
@@ -6,6 +6,7 @@
 package net.neoforged.neoforge.common;
 
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import io.netty.buffer.ByteBuf;
 import java.util.Collection;
 import java.util.Collections;
@@ -17,6 +18,7 @@ import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
+import net.neoforged.neoforge.common.util.NeoForgeExtraCodecs;
 import net.neoforged.neoforge.event.entity.living.MobEffectEvent;
 
 /**
@@ -27,7 +29,11 @@ import net.neoforged.neoforge.event.entity.living.MobEffectEvent;
 public final class EffectCure {
     private static final Map<String, EffectCure> CURES = new ConcurrentHashMap<>();
 
-    public static Codec<EffectCure> CODEC = Codec.STRING.xmap(EffectCure::get, EffectCure::name);
+    public static final Codec<EffectCure> CODEC = Codec.STRING.xmap(EffectCure::get, EffectCure::name);
+    public static final Codec<Set<EffectCure>> CURES_CODEC = Codec
+            .withAlternative(NeoForgeExtraCodecs.setOf(EffectCure.CODEC), EffectCure.CODEC, Set::of);
+    public static final Codec<Set<EffectCure>> DATA_MAP_CODEC = Codec
+            .withAlternative(NeoForgeExtraCodecs.singularOrPluralCodec(EffectCure.CODEC, "cure").codec(), CURES_CODEC);
     public static final StreamCodec<ByteBuf, EffectCure> STREAM_CODEC = ByteBufCodecs.STRING_UTF8.map(EffectCure::get, EffectCure::name);
 
     /**
--- src/main/java/net/neoforged/neoforge/common/extensions/IMobEffectExtension.java
+++ src/main/java/net/neoforged/neoforge/common/extensions/IMobEffectExtension.java
@@ -5,26 +5,29 @@
 
 package net.neoforged.neoforge.common.extensions;
 
+import java.util.Optional;
 import java.util.Set;
+
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.neoforged.neoforge.common.EffectCure;
 import net.neoforged.neoforge.common.EffectCures;
+import net.neoforged.neoforge.registries.datamaps.builtin.NeoForgeDataMaps;
 
 public interface IMobEffectExtension {
     private MobEffect self() {
         return (MobEffect) this;
     }
 
-    /***
+    /**
      * Fill the given set with the {@link EffectCure}s this effect should be curable with by default
      */
     default void fillEffectCures(Set<EffectCure> cures, MobEffectInstance effectInstance) {
-        cures.addAll(EffectCures.DEFAULT_CURES);
-        if (self() == MobEffects.POISON.value()) {
-            cures.add(EffectCures.HONEY);
-        }
+        BuiltInRegistries.MOB_EFFECT.getResourceKey(self())
+                .flatMap(key -> Optional.ofNullable(BuiltInRegistries.MOB_EFFECT.getData(NeoForgeDataMaps.CURES, key)))
+                .ifPresent(cures::addAll);
     }
 
     /**
