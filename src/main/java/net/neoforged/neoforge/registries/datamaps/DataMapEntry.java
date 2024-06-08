062d2c48a9aecfcca6263172160745225d8cef6c
RaymondBlaze <raymond-blaze@outlook.com>
2024-06-08 11:33:48 +0800

    Deprecate DataMapEntry.Removal in favor of conditioned removal

--- src/main/java/net/neoforged/neoforge/registries/datamaps/DataMapEntry.java
+++ src/main/java/net/neoforged/neoforge/registries/datamaps/DataMapEntry.java
@@ -25,23 +25,4 @@ public record DataMapEntry<T>(T value, boolean replace) {
                         Codec.BOOL.optionalFieldOf("replace", false).forGetter(DataMapEntry::replace)).apply(i, DataMapEntry::new)),
                 type.codec()).xmap(e -> e.map(Function.identity(), DataMapEntry::new), entry -> entry.replace() ? Either.left(entry) : Either.right(entry.value()));
     }
-
-    public record Removal<T, R>(Either<TagKey<R>, ResourceKey<R>> key,
-            Optional<DataMapValueRemover<R, T>> remover) {
-        private Removal(Either<TagKey<R>, ResourceKey<R>> key) {
-            this(key, Optional.empty());
-        }
-
-        @SuppressWarnings("unchecked")
-        public static <T, R> Codec<Removal<T, R>> codec(Codec<Either<TagKey<R>, ResourceKey<R>>> tagOrValue, DataMapType<R, T> attachment) {
-            if (attachment instanceof AdvancedDataMapType<R, T, ?> advanced) {
-                return RecordCodecBuilder.create(in -> in.group(
-                        tagOrValue.fieldOf("key").forGetter(Removal::key),
-                        ((Codec<DataMapValueRemover<R, T>>) advanced.remover()).optionalFieldOf("remover").forGetter(Removal::remover)).apply(in, Removal::new));
-            }
-            return RecordCodecBuilder.create(inst -> inst
-                    .group(tagOrValue.fieldOf("key").forGetter(Removal::key))
-                    .apply(inst, Removal::new));
-        }
-    }
 }
