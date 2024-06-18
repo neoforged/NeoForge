1aa4f76abff98c86648dae91530f0eb6f5ffed07
Matyrobbrt <matyrobbrt@gmail.com>
2024-06-18 23:51:15 +0300

    Add a `can_villager_compost` field to the compostables datamap
    Setting the field to `true` allows villagers to compost the item if they're farmers.

--- src/main/java/net/neoforged/neoforge/common/data/internal/NeoForgeDataMapsProvider.java
+++ src/main/java/net/neoforged/neoforge/common/data/internal/NeoForgeDataMapsProvider.java
@@ -7,6 +7,7 @@ package net.neoforged.neoforge.common.data.internal;
 
 import it.unimi.dsi.fastutil.objects.Reference2IntMap;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
@@ -20,8 +21,10 @@ import net.minecraft.tags.ItemTags;
 import net.minecraft.util.random.Weight;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.behavior.GiveGiftToHero;
+import net.minecraft.world.entity.ai.behavior.WorkAtComposter;
 import net.minecraft.world.entity.animal.Parrot;
 import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.ComposterBlock;
 import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.world.level.gameevent.GameEvent;
@@ -46,7 +49,8 @@ public class NeoForgeDataMapsProvider extends DataMapProvider {
     @Override
     protected void gather() {
         final var compostables = builder(NeoForgeDataMaps.COMPOSTABLES);
-        ComposterBlock.COMPOSTABLES.forEach((item, chance) -> compostables.add(item.asItem().builtInRegistryHolder(), new Compostable(chance), false));
+        final List<Item> villagerCompostables = ObfuscationReflectionHelper.getPrivateValue(WorkAtComposter.class, null, "COMPOSTABLE_ITEMS");
+        ComposterBlock.COMPOSTABLES.forEach((item, chance) -> compostables.add(item.asItem().builtInRegistryHolder(), new Compostable(chance, villagerCompostables.contains(item.asItem())), false));
 
         final var fuels = builder(NeoForgeDataMaps.FURNACE_FUELS);
         AbstractFurnaceBlockEntity.buildFuels((value, time) -> value.ifLeft(item -> fuels.add(item.builtInRegistryHolder(), new FurnaceFuel(time), false))
--- src/main/java/net/neoforged/neoforge/registries/datamaps/builtin/Compostable.java
+++ src/main/java/net/neoforged/neoforge/registries/datamaps/builtin/Compostable.java
@@ -11,13 +11,15 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 /**
  * Data map value for {@linkplain NeoForgeDataMaps#COMPOSTABLES compostables}.
  *
- * @param chance the chance that a compost is successful
+ * @param chance             the chance that a compost is successful
+ * @param canVillagerCompost whether farmer villagers can compost the item
  */
-public record Compostable(float chance) {
+public record Compostable(float chance, boolean canVillagerCompost) {
     public static final Codec<Compostable> CHANCE_CODEC = Codec.floatRange(0f, 1f)
-            .xmap(Compostable::new, Compostable::chance);
+            .xmap(val -> new Compostable(val, false), Compostable::chance);
     public static final Codec<Compostable> CODEC = Codec.withAlternative(
             RecordCodecBuilder.create(in -> in.group(
-                    Codec.floatRange(0f, 1f).fieldOf("chance").forGetter(Compostable::chance)).apply(in, Compostable::new)),
+                    Codec.floatRange(0f, 1f).fieldOf("chance").forGetter(Compostable::chance),
+                    Codec.BOOL.optionalFieldOf("can_villager_compost", false).forGetter(Compostable::canVillagerCompost)).apply(in, Compostable::new)),
             CHANCE_CODEC);
 }
