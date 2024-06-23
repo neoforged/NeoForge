dc5d022023bc1e76cda77d06d1e066449f4af9a6
Matyrobbrt <matyrobbrt@gmail.com>
2024-06-23 23:07:23 +0300

    Review

--- src/main/java/net/neoforged/neoforge/registries/datamaps/builtin/Compostable.java
+++ src/main/java/net/neoforged/neoforge/registries/datamaps/builtin/Compostable.java
@@ -15,11 +15,20 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
  * @param canVillagerCompost whether farmer villagers can compost the item
  */
 public record Compostable(float chance, boolean canVillagerCompost) {
+
     public static final Codec<Compostable> CHANCE_CODEC = Codec.floatRange(0f, 1f)
-            .xmap(val -> new Compostable(val, false), Compostable::chance);
+            .xmap(Compostable::new, Compostable::chance);
     public static final Codec<Compostable> CODEC = Codec.withAlternative(
             RecordCodecBuilder.create(in -> in.group(
                     Codec.floatRange(0f, 1f).fieldOf("chance").forGetter(Compostable::chance),
                     Codec.BOOL.optionalFieldOf("can_villager_compost", false).forGetter(Compostable::canVillagerCompost)).apply(in, Compostable::new)),
             CHANCE_CODEC);
+    /**
+     * Constructs a {@link Compostable} that cannot be composted by farmer villagers.
+     * 
+     * @param chance the chance that a compost is successful
+     */
+    public Compostable(float chance) {
+        this(chance, false);
+    }
 }
