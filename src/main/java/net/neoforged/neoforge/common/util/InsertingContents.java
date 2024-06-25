3fcca473626e4f8558b4e453715109caeda8d94c
Basique <basiqueevangelist@yandex.ru>
2024-06-25 23:12:05 +0300

    should™ address reviews

--- src/main/java/net/neoforged/neoforge/common/util/InsertingContents.java
+++ src/main/java/net/neoforged/neoforge/common/util/InsertingContents.java
@@ -18,35 +18,36 @@ import net.minecraft.network.chat.contents.TranslatableContents;
 import net.minecraft.util.ExtraCodecs;
 import org.jetbrains.annotations.ApiStatus;
 
+@ApiStatus.Internal
 public record InsertingContents(int index) implements ComponentContents {
     public static final MapCodec<InsertingContents> CODEC = RecordCodecBuilder.mapCodec(i -> i.group(
             ExtraCodecs.NON_NEGATIVE_INT.fieldOf("index").forGetter(InsertingContents::index))
             .apply(i, InsertingContents::new));
 
-    public static final ComponentContents.Type<InsertingContents> TYPE = new ComponentContents.Type<>(CODEC, "inserting");
+    public static final ComponentContents.Type<InsertingContents> TYPE = new ComponentContents.Type<>(CODEC, "neoforge:inserting");
 
-    private static final ThreadLocal<Deque<TranslatableContents>> translationStack = ThreadLocal.withInitial(ArrayDeque::new);
+    private static final ThreadLocal<Deque<TranslatableContents>> TRANSLATION_STACK = ThreadLocal.withInitial(ArrayDeque::new);
 
     @ApiStatus.Internal
     public static boolean pushTranslation(TranslatableContents contents) {
-        for (TranslatableContents other : translationStack.get()) {
+        for (TranslatableContents other : TRANSLATION_STACK.get()) {
             if (contents == other) {
                 return false;
             }
         }
 
-        translationStack.get().push(contents);
+        TRANSLATION_STACK.get().push(contents);
         return true;
     }
 
     @ApiStatus.Internal
     public static void popTranslation() {
-        translationStack.get().pop();
+        TRANSLATION_STACK.get().pop();
     }
 
     @Override
     public <T> Optional<T> visit(FormattedText.ContentConsumer<T> visitor) {
-        var translation = translationStack.get().peek();
+        var translation = TRANSLATION_STACK.get().peek();
 
         if (translation == null || translation.getArgs().length <= index)
             return visitor.accept("%" + (index + 1) + "$s");
@@ -62,7 +63,7 @@ public record InsertingContents(int index) implements ComponentContents {
 
     @Override
     public <T> Optional<T> visit(FormattedText.StyledContentConsumer<T> visitor, Style style) {
-        var translation = translationStack.get().peek();
+        var translation = TRANSLATION_STACK.get().peek();
 
         if (translation == null || translation.getArgs().length <= index)
             return visitor.accept(style, "%" + (index + 1) + "$s");
