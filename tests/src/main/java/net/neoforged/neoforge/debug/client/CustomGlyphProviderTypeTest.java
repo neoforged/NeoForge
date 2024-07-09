fa20cba9ff3eda1bbc3314f7c6312593806c65b6
Josiah Glosson <soujournme@gmail.com>
2024-07-08 21:11:02 -0500

    Run applyAllFormatting

--- tests/src/main/java/net/neoforged/neoforge/debug/client/CustomGlyphProviderTypeTest.java
+++ tests/src/main/java/net/neoforged/neoforge/debug/client/CustomGlyphProviderTypeTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) NeoForged and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
 package net.neoforged.neoforge.debug.client;
 
 import com.mojang.datafixers.util.Either;
@@ -21,42 +26,37 @@ import net.neoforged.testframework.annotation.TestHolder;
 @ForEachTest(side = Dist.CLIENT, groups = { "client.fonts", "client" })
 public class CustomGlyphProviderTypeTest {
     public static final EnumProxy<GlyphProviderType> REFERENCE_2_PARAMS = new EnumProxy<>(
-        GlyphProviderType.class, "neotests:reference_2", Reference2.CODEC
-    );
+            GlyphProviderType.class, "neotests:reference_2", Reference2.CODEC);
 
     @TestHolder(description = "Tests if custom GlyphProviderTypes were used for loading resources", enabledByDefault = true)
     static void setupGlyphProviderTypeTest(DynamicTest test) {
-        test.framework().modEventBus().addListener((RegisterClientReloadListenersEvent event) ->
-            event.registerReloadListener(new SimplePreparableReloadListener<Void>() {
-                @Override
-                protected Void prepare(ResourceManager p_10796_, ProfilerFiller p_10797_) {
-                    return null;
-                }
+        test.framework().modEventBus().addListener((RegisterClientReloadListenersEvent event) -> event.registerReloadListener(new SimplePreparableReloadListener<Void>() {
+            @Override
+            protected Void prepare(ResourceManager p_10796_, ProfilerFiller p_10797_) {
+                return null;
+            }
 
-                @Override
-                protected void apply(Void p_10793_, ResourceManager p_10794_, ProfilerFiller p_10795_) {
-                    final Minecraft minecraft = Minecraft.getInstance();
-                    final MutableComponent component = Component.literal("iiiii");
-                    final int vanillaWidth = minecraft.font.width(component.withStyle(s -> s
-                        .withFont(ResourceLocation.withDefaultNamespace("uniform"))
-                    ));
-                    final int moddedWidth = minecraft.font.width(component.withStyle(s -> s
-                        .withFont(ResourceLocation.fromNamespaceAndPath("custom_glyph_provider_type_test", "vanilla"))
-                    ));
+            @Override
+            protected void apply(Void p_10793_, ResourceManager p_10794_, ProfilerFiller p_10795_) {
+                final Minecraft minecraft = Minecraft.getInstance();
+                final MutableComponent component = Component.literal("iiiii");
+                final int vanillaWidth = minecraft.font.width(component.withStyle(s -> s
+                        .withFont(ResourceLocation.withDefaultNamespace("uniform"))));
+                final int moddedWidth = minecraft.font.width(component.withStyle(s -> s
+                        .withFont(ResourceLocation.fromNamespaceAndPath("custom_glyph_provider_type_test", "vanilla"))));
 
-                    if (moddedWidth != vanillaWidth) {
-                        test.fail("Width of modded text is " + moddedWidth + ", but " + vanillaWidth + " was expected.");
-                        return;
-                    }
-                    test.pass();
+                if (moddedWidth != vanillaWidth) {
+                    test.fail("Width of modded text is " + moddedWidth + ", but " + vanillaWidth + " was expected.");
+                    return;
                 }
-            })
-        );
+                test.pass();
+            }
+        }));
     }
 
     public record Reference2(ResourceLocation what) implements GlyphProviderDefinition {
         public static final MapCodec<Reference2> CODEC = ResourceLocation.CODEC.fieldOf("what")
-            .xmap(Reference2::new, Reference2::what);
+                .xmap(Reference2::new, Reference2::what);
 
         @Override
         public GlyphProviderType type() {
