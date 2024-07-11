6ffe1c067e03f73c44ebdcced57a6df7dae225a8
Henry Loenwind <henry@loenwind.info>
2024-07-11 17:19:33 +0200

    Cleaned up ItemExpireEvent
    The old code had grown a bit of bitrot. Also cleaned up the event itself, getting rid of the superfluous cancelability. Otherwise the functionality is the same.

--- src/main/java/net/neoforged/neoforge/event/entity/item/ItemExpireEvent.java
+++ src/main/java/net/neoforged/neoforge/event/entity/item/ItemExpireEvent.java
@@ -6,32 +6,33 @@
 package net.neoforged.neoforge.event.entity.item;
 
 import net.minecraft.world.entity.item.ItemEntity;
-import net.neoforged.bus.api.ICancellableEvent;
 
 /**
  * Event that is fired when an EntityItem's age has reached its maximum
- * lifespan. Canceling this event will prevent the EntityItem from being
- * flagged as dead, thus staying it's removal from the world. If canceled
- * it will add more time to the entities life equal to extraLife.
+ * lifespan. Adding extra life time will prevent the EntityItem from being
+ * flagged as dead, thus staying it's removal from the world.
  */
-public class ItemExpireEvent extends ItemEvent implements ICancellableEvent {
-    private int extraLife;
+public class ItemExpireEvent extends ItemEvent {
+    private int extraLife = 0;
 
     /**
      * Creates a new event for an expiring EntityItem.
      * 
      * @param entityItem The EntityItem being deleted.
-     * @param extraLife  The amount of time to be added to this entities lifespan if the event is canceled.
      */
-    public ItemExpireEvent(ItemEntity entityItem, int extraLife) {
+    public ItemExpireEvent(ItemEntity entityItem) {
         super(entityItem);
-        this.setExtraLife(extraLife);
     }
 
     public int getExtraLife() {
         return extraLife;
     }
 
+    /**
+     * Sets the amount of extra life time (in ticks) to give this EntityItem.
+     * 
+     * @param extraLife The amount of time to be added to this entities lifespan.
+     */
     public void setExtraLife(int extraLife) {
         this.extraLife = extraLife;
     }
