2192af026994eaffff289a2d72dde954b19ec833
Henry Loenwind <henry@loenwind.info>
2024-07-13 22:03:27 +0200

    More javadoc, fixed javdoc, added add()

--- src/main/java/net/neoforged/neoforge/event/entity/item/ItemExpireEvent.java
+++ src/main/java/net/neoforged/neoforge/event/entity/item/ItemExpireEvent.java
@@ -8,32 +8,53 @@ package net.neoforged.neoforge.event.entity.item;
 import net.minecraft.world.entity.item.ItemEntity;
 
 /**
- * Event that is fired when an EntityItem's age has reached its maximum
- * lifespan. Adding extra life time will prevent the EntityItem from being
+ * Event that is fired when an {@link ItemEntity}'s age has reached its maximum
+ * lifespan. Adding extra life time will prevent the {@link ItemEntity} from being
  * flagged as dead, thus staying it's removal from the world.
+ * <p>
+ * This event will only be fired server-side.
  */
 public class ItemExpireEvent extends ItemEvent {
     private int extraLife = 0;
 
     /**
-     * Creates a new event for an expiring EntityItem.
+     * Creates a new event for an expiring {@link ItemEntity}.
      * 
-     * @param entityItem The EntityItem being deleted.
+     * @param itemEntity The {@link ItemEntity} being deleted.
      */
-    public ItemExpireEvent(ItemEntity entityItem) {
-        super(entityItem);
+    public ItemExpireEvent(ItemEntity itemEntity) {
+        super(itemEntity);
     }
 
+    /**
+     * Query the amount of extra time that will be added.
+     * <p>
+     * Note that this is the event result. If you need data from the entity, query it directly.
+     * {@link ItemEntity#lifespan} is the entities maximum lifespan and also its current age.
+     * 
+     * @return Extra time to be added in ticks.
+     */
     public int getExtraLife() {
         return extraLife;
     }
 
     /**
-     * Sets the amount of extra life time (in ticks) to give this EntityItem.
+     * Sets the amount of extra life time (in ticks) to give this {@link ItemEntity}.
+     * <p>
+     * <em>Consider using {@link #addExtraLife(int)} in case another mod also adds extra time.</em>
      * 
      * @param extraLife The amount of time to be added to this entities lifespan.
      */
     public void setExtraLife(int extraLife) {
         this.extraLife = extraLife;
+    }
+
+    /**
+     * Adds to the amount of extra life time (in ticks) to give this {@link ItemEntity}.
+     * 
+     * @param extraLife The amount of time to be added to this entities lifespan.
+     */
+    public void addExtraLife(int extraLife) {
+        this.extraLife += extraLife;
     }
 }
