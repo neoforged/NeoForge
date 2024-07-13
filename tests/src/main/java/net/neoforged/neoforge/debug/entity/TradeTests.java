612339c590a5173a8b2e7b5a3c31c9d4d534b17b
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-07-13 12:35:43 -0400

    Reflection now

--- tests/src/main/java/net/neoforged/neoforge/debug/entity/TradeTests.java
+++ tests/src/main/java/net/neoforged/neoforge/debug/entity/TradeTests.java
@@ -5,6 +5,7 @@
 
 package net.neoforged.neoforge.debug.entity;
 
+import java.lang.invoke.MethodHandle;
 import java.util.ArrayList;
 import net.minecraft.core.BlockPos;
 import net.minecraft.gametest.framework.GameTest;
@@ -18,11 +19,11 @@ import net.minecraft.world.item.Items;
 import net.neoforged.neoforge.common.BasicItemListing;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.village.VillagerTradesEvent;
-import net.neoforged.neoforge.mixins.VillagerAccessor;
 import net.neoforged.testframework.DynamicTest;
 import net.neoforged.testframework.annotation.ForEachTest;
 import net.neoforged.testframework.annotation.TestHolder;
 import net.neoforged.testframework.gametest.EmptyTemplate;
+import net.neoforged.testframework.impl.ReflectionUtils;
 
 @ForEachTest(groups = TradeTests.GROUP)
 public class TradeTests {
@@ -38,12 +39,17 @@ public class TradeTests {
                 .thenExecute(villager -> {
                     // Set villager to level 6 trades
                     villager.setVillagerData(new VillagerData(VillagerType.PLAINS, VillagerProfession.WEAPONSMITH, 0));
-                    ((VillagerAccessor) villager).neoforge$callIncreaseMerchantCareer();
-                    ((VillagerAccessor) villager).neoforge$callIncreaseMerchantCareer();
-                    ((VillagerAccessor) villager).neoforge$callIncreaseMerchantCareer();
-                    ((VillagerAccessor) villager).neoforge$callIncreaseMerchantCareer();
-                    ((VillagerAccessor) villager).neoforge$callIncreaseMerchantCareer();
-                    ((VillagerAccessor) villager).neoforge$callIncreaseMerchantCareer();
+                    try {
+                        MethodHandle methodHandle = ReflectionUtils.handle(villager.getClass().getDeclaredMethod("increaseMerchantCareer"));
+                        methodHandle.invoke(villager);
+                        methodHandle.invoke(villager);
+                        methodHandle.invoke(villager);
+                        methodHandle.invoke(villager);
+                        methodHandle.invoke(villager);
+                        methodHandle.invoke(villager);
+                    } catch (Throwable e) {
+                        helper.fail("Cannot find Villager#increaseMerchantCareer method.");
+                    }
                 })
                 .thenExecute(villager -> helper.assertTrue(villager.getOffers().size() == 9, "Weaponsmith did not get a new tier of trade"))
                 .thenExecute(villager -> helper.assertTrue(villager.getOffers().get(8).getResult().is(Items.NETHERITE_SWORD), "Netherite Sword was not in trade."))
