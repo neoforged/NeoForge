37e5dbbf8ab8d45396e31ea33c62967fc4127d61
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-07-14 09:50:35 -0400

    Fixed bad merge

--- src/main/java/net/neoforged/neoforge/common/extensions/IBlockExtension.java
+++ src/main/java/net/neoforged/neoforge/common/extensions/IBlockExtension.java
@@ -787,12 +787,12 @@ public interface IBlockExtension {
                     context.getLevel().getBlockState(context.getClickedPos().above()).isAir()) {
                         return block == Blocks.COARSE_DIRT ? Blocks.DIRT.defaultBlockState() : Blocks.FARMLAND.defaultBlockState();
                     }
-            }
         } else if (ItemAbilities.SHEARS_TRIM == itemAbility) {
             if (state.getBlock() instanceof GrowingPlantHeadBlock growingPlant && !growingPlant.isMaxAge(state)) {
                 if (!simulate)
                     context.getLevel().playSound(context.getPlayer(), context.getClickedPos(), SoundEvents.GROWING_PLANT_CROP, SoundSource.BLOCKS, 1.0F, 1.0F);
                 return growingPlant.getMaxAgeState(state);
+            }
         } else if (ItemAbilities.SHOVEL_DOUSE == itemAbility) {
             if (state.getBlock() instanceof CampfireBlock && state.getValue(CampfireBlock.LIT)) {
                 if (!simulate) {
