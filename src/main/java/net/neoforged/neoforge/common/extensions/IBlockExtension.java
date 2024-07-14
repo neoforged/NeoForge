3c03524ff86b785161ba6099c2741a2073101722
Henry Loenwind <henry@loenwind.info>
2024-07-14 15:39:39 +0200

    Add Item Abilities for lighting and dowsing (Ex. Candles, Campfire, Cake) (#1219)

--- src/main/java/net/neoforged/neoforge/common/extensions/IBlockExtension.java
+++ src/main/java/net/neoforged/neoforge/common/extensions/IBlockExtension.java
@@ -43,6 +43,9 @@ import net.minecraft.world.level.block.BeaconBeamBlock;
 import net.minecraft.world.level.block.BedBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.CampfireBlock;
+import net.minecraft.world.level.block.CandleBlock;
+import net.minecraft.world.level.block.CandleCakeBlock;
 import net.minecraft.world.level.block.FarmBlock;
 import net.minecraft.world.level.block.FenceGateBlock;
 import net.minecraft.world.level.block.FireBlock;
@@ -60,6 +63,7 @@ import net.minecraft.world.level.block.WeatheringCopper;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.level.material.FluidState;
@@ -780,6 +784,17 @@ public interface IBlockExtension {
                     context.getLevel().getBlockState(context.getClickedPos().above()).isAir()) {
                         return block == Blocks.COARSE_DIRT ? Blocks.DIRT.defaultBlockState() : Blocks.FARMLAND.defaultBlockState();
                     }
+        } else if (ItemAbilities.SHOVEL_DOUSE == itemAbility) {
+            if (state.getBlock() instanceof CampfireBlock && state.getValue(CampfireBlock.LIT)) {
+                if (!simulate) {
+                    CampfireBlock.dowse(context.getPlayer(), context.getLevel(), context.getClickedPos(), state);
+                }
+                return state.setValue(CampfireBlock.LIT, Boolean.valueOf(false));
+            }
+        } else if (ItemAbilities.FIRESTARTER_LIGHT == itemAbility) {
+            if (CampfireBlock.canLight(state) || CandleBlock.canLight(state) || CandleCakeBlock.canLight(state)) {
+                return state.setValue(BlockStateProperties.LIT, Boolean.valueOf(true));
+            }
         }
 
         return null;
