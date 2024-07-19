0bd06bad05e92f475f3f35f9632f22d74abdfa88
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-07-19 18:05:37 -0400

    adjust ranged tag javadoc

--- src/main/java/net/neoforged/neoforge/common/Tags.java
+++ src/main/java/net/neoforged/neoforge/common/Tags.java
@@ -689,7 +689,7 @@ public class Tags {
         public static final TagKey<Item> MELEE_WEAPON_TOOLS = tag("tools/melee_weapon");
         /**
          * A tag containing ranged-based weapons for recipes and loot tables.
-         * Tools are considered ranged if they have the ability to damage targets at a distance.
+         * Tools are considered ranged if they can damage entities beyond the weapon's and player's melee attack range.
          * Do not use this tag for determining a tool's behavior in-code.
          * Please use {@link ItemAbilities} instead for what action a tool can do.
          *
