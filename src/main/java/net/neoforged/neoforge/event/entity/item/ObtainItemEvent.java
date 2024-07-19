c50e45e01610906e45706ee0e627d7170da83466
lcy0x1 <chengyilin@umail.ucsb.edu>
2024-07-19 17:56:35 +0800

    patch advancement reward and commands

--- src/main/java/net/neoforged/neoforge/event/entity/item/ObtainItemEvent.java
+++ src/main/java/net/neoforged/neoforge/event/entity/item/ObtainItemEvent.java
@@ -5,24 +5,39 @@
 
 package net.neoforged.neoforge.event.entity.item;
 
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.Event;
+import net.neoforged.neoforge.event.EventHooks;
 import net.neoforged.neoforge.event.entity.player.ItemEntityPickupEvent;
 
 /**
- * Fired when player attempts to pick up an item. Note that other means of giving item to players / other entities are not accounted for.
- * Called after {@link ItemEntityPickupEvent.Pre} and before {@link ItemEntityPickupEvent.Post}.
- * Listeners of this event is allowed to reduce the amount of this stack, but is not allowed increase or change data component of this stack.
- * Mods that want to give items to player should call {@link net.neoforged.neoforge.event.EventHooks#onObtainItem(LivingEntity, ItemStack)}
- * to notify listeners of this event. <br><br>
+ * Fired when player gets an item from sources list below. Note that other means of giving item to players / other entities are not accounted for.<br>
+ * For item pickups, Called after {@link ItemEntityPickupEvent.Pre} and before {@link ItemEntityPickupEvent.Post}.<br>
+ * Listeners of this event is allowed to reduce the amount of this stack, but is not allowed increase or change data component of this stack.<br>
+ * Mods that want to give items to player should call {@link EventHooks#addToInventory(Player, ItemStack, Source)}<br>
+ * Mods that want entity to pick up items should call {@link EventHooks#onObtainItem(LivingEntity, ItemStack, Source)}<br>
+ * <br>
+ * Default Sources:
+ * <li>ItemEntitySource for item entity pickup</li>
+ * <li>ProjectileSource for arrow and trident</li>
+ * <li>COMMAND for loot and give commands</li>
+ * <li>REWARD for advancement rewards and modded</li>
+ * <li>DROPS for modded ways of giving drops to player without creating item entities</li>
+ * <br>
  * One example use case of this event is for backpack mods to redirect item pickup,
  * allowing them to also account for mod behaviors that directly add item to players,
  * such as wrench picking up machines, or ender pickaxe teleporting drops to player inventory.
  */
 public class ObtainItemEvent extends Event {
+    public static final Source REWARD = new SimpleSource("Reward");
+    public static final Source COMMAND = new SimpleSource("Command");
+    public static final Source DROPS = new SimpleSource("Drops");
+
     private final ItemStack stack;
     private final LivingEntity entity;
     private final Source source;
@@ -49,11 +64,19 @@ public class ObtainItemEvent extends Event {
 
     }
 
-    public record ItemEntitySource(ItemEntity entity) implements Source {
+    public interface EntitySource extends Source {
+        Entity entity();
+    }
+
+    public record SimpleSource(String name) implements Source {
 
     }
 
-    public record ProjectileSource(Projectile projectile) implements Source {
+    public record ItemEntitySource(ItemEntity entity) implements EntitySource {
+
+    }
+
+    public record ProjectileSource(Projectile entity) implements EntitySource {
 
     }
 }
