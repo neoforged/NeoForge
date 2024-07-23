d43b942389631b9fdca2d2c6b5fc8563320f31a7
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-07-23 14:19:31 -0700

    more formatting fixes

--- src/main/java/net/neoforged/neoforge/event/entity/player/ClientInformationUpdatedEvent.java
+++ src/main/java/net/neoforged/neoforge/event/entity/player/ClientInformationUpdatedEvent.java
@@ -16,39 +16,39 @@ import net.neoforged.neoforge.common.NeoForge;
  * This event is fired on the {@link NeoForge#EVENT_BUS}.
  **/
 public class ClientInformationUpdatedEvent extends PlayerEvent {
-	private final ClientInformation oldInformation;
-	private final ClientInformation updatedInformation;
+    private final ClientInformation oldInformation;
+    private final ClientInformation updatedInformation;
 
-	public ClientInformationUpdatedEvent(ServerPlayer player, ClientInformation oldInfo, ClientInformation newInfo) {
-		super(player);
-		this.oldInformation = oldInfo;
-		this.updatedInformation = newInfo;
-	}
+    public ClientInformationUpdatedEvent(ServerPlayer player, ClientInformation oldInfo, ClientInformation newInfo) {
+        super(player);
+        this.oldInformation = oldInfo;
+        this.updatedInformation = newInfo;
+    }
 
-	@Override
-	public ServerPlayer getEntity() {
-		return (ServerPlayer) super.getEntity();
-	}
+    @Override
+    public ServerPlayer getEntity() {
+        return (ServerPlayer) super.getEntity();
+    }
 
-	/**
-	 * Returns the new client info to be applied to the player.
-	 * Sometimes the client resends unchanged options, so if that matters
-	 * for your use case, check equality with {@link #getOldInformation()}.
-	 *
-	 * @return updated information
-	 */
-	public ClientInformation getUpdatedInformation() {
-		return this.updatedInformation;
-	}
+    /**
+     * Returns the new client info to be applied to the player.
+     * Sometimes the client resends unchanged options, so if that matters
+     * for your use case, check equality with {@link #getOldInformation()}.
+     *
+     * @return updated information
+     */
+    public ClientInformation getUpdatedInformation() {
+        return this.updatedInformation;
+    }
 
-	/**
-	 * Returns the existing client info from to the player.
-	 * <p>
-	 * May be blank or defaulted initial data on first event call for a player instance.
-	 *
-	 * @return updated information
-	 */
-	public ClientInformation getOldInformation() {
-		return this.oldInformation;
-	}
+    /**
+     * Returns the existing client info from to the player.
+     * <p>
+     * May be blank or defaulted initial data on first event call for a player instance.
+     *
+     * @return updated information
+     */
+    public ClientInformation getOldInformation() {
+        return this.oldInformation;
+    }
 }
