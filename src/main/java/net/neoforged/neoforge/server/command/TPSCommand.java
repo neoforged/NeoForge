f65ebd97c21e8823243b7897ff2e0ef180b59bd0
Apex <robsonlawson3@gmail.com>
2024-08-23 15:55:04 +0100

    Big refactoring to shorten overall output
    While keeping all of the same information and stylization

--- src/main/java/net/neoforged/neoforge/server/command/TPSCommand.java
+++ src/main/java/net/neoforged/neoforge/server/command/TPSCommand.java
@@ -11,13 +11,10 @@ import com.mojang.brigadier.builder.ArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import java.text.DecimalFormat;
-import java.util.Objects;
 import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.DimensionArgument;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.HoverEvent;
 import net.minecraft.network.chat.MutableComponent;
@@ -55,54 +52,41 @@ class TPSCommand {
     }
 
     private static Component createComponent(MinecraftServer server, @Nullable ServerLevel dimension) {
-        MutableComponent prefix;
         long[] times;
         TickRateManager tickRateManager;
 
         if (dimension == null) {
-            prefix = Component.translatable("commands.neoforge.tps.summary.overall");
             times = server.getTickTimesNanos();
             tickRateManager = server.tickRateManager();
         } else {
-            var dimKey = dimension.dimension();
-            var dimensionTimes = server.getTickTime(dimKey);
-
-            prefix = Component.translatable("commands.neoforge.tps.summary.dimension", createDimensionComponent(dimension));
+            var dimensionTimes = server.getTickTime(dimension.dimension());
             times = dimensionTimes == null ? UNLOADED : dimensionTimes;
             tickRateManager = dimension.tickRateManager();
         }
 
         var tickTime = Stats.meanOf(times) / TimeUtil.NANOSECONDS_PER_MILLISECOND;
         var tps = TimeUtil.MILLISECONDS_PER_SECOND / Math.max(tickTime, tickRateManager.millisecondsPerTick());
-        var timeComponent = Component.empty().append(createTickTimeComponent(tickTime)).append(". ").append(createTpsComponent(tickRateManager, tps));
-        return prefix.append(CommonComponents.SPACE).append(timeComponent);
-    }
+        var tickTimeComponent = Component.literal(TIME_FORMATTER.format(tickTime)).withColor(CommonColors.LIGHT_GRAY);
+        var tpsComponent = Component.literal(TIME_FORMATTER.format(tps)).withColor(calculateTPSColor(tickRateManager, tps));
 
-    private static Component createDimensionComponent(ServerLevel dimension) {
-        var regName = dimension.dimension().location();
-        var dimensionTypes = dimension.registryAccess().registryOrThrow(Registries.DIMENSION_TYPE);
-        var dimensionType = Objects.requireNonNull(dimensionTypes.getKey(dimension.dimensionType()));
+        MutableComponent component;
 
-        return Component.empty().append(dimension.getDescription()).withStyle(style -> style
-                .withColor(ChatFormatting.GREEN)
-                .withHoverEvent(new HoverEvent(
-                        HoverEvent.Action.SHOW_TEXT,
-                        Component.translatable("commands.neoforge.tps.summary.dimension.tooltip", regName.toString(), dimensionType.toString()))));
-    }
+        if (dimension == null)
+            component = Component.translatable("commands.neoforge.tps.overall", tpsComponent, tickTimeComponent);
+        else {
+            var dimensionType = dimension.dimensionTypeRegistration();
 
-    private static Component createTickTimeComponent(double tickTime) {
-        return Component.translatable("commands.neoforge.tps.mean_tick_time", Component.literal(TIME_FORMATTER.format(tickTime))
-                .withColor(CommonColors.GRAY))
-                .withStyle(style -> style.withHoverEvent(new HoverEvent(
-                        HoverEvent.Action.SHOW_TEXT,
-                        Component.translatable("commands.neoforge.tps.mean_tick_time.tooltip"))));
-    }
+            var dimensionName = Component.empty().append(dimension.getDescription()).withStyle(style -> style
+                    .withColor(ChatFormatting.GREEN)
+                    .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.translatable(
+                            "commands.neoforge.tps.dimension.tooltip",
+                            dimension.dimension().location().toString(),
+                            dimensionType.getRegisteredName()))));
 
-    private static Component createTpsComponent(TickRateManager tickRateManager, double tps) {
-        return Component.translatable("commands.neoforge.tps.mean_tps", Component.literal(TIME_FORMATTER.format(tps)).withColor(calculateTPSColor(tickRateManager, tps)))
-                .withStyle(style -> style.withHoverEvent(new HoverEvent(
-                        HoverEvent.Action.SHOW_TEXT,
-                        Component.translatable("commands.neoforge.tps.mean_tps.tooltip"))));
+            component = Component.translatable("commands.neoforge.tps.dimension", dimensionName, tpsComponent, tickTimeComponent);
+        }
+
+        return component.withStyle(style -> style.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.translatable("commands.neoforge.tps.tooltip", tickRateManager.tickrate()))));
     }
 
     private static int calculateTPSColor(TickRateManager tickRateManager, double tps) {
