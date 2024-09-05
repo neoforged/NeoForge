001aaea302365e2e41790dcb8f5460499c777af6
Apex <robsonlawson3@gmail.com>
2024-09-05 04:24:02 +0100

    Fix accidently typing DeferredDataComponentType to BlockEntity

--- src/main/java/net/neoforged/neoforge/registries/deferred/DeferredDataComponentType.java
+++ src/main/java/net/neoforged/neoforge/registries/deferred/DeferredDataComponentType.java
@@ -9,7 +9,6 @@ import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.entity.BlockEntity;
 
 /**
  * Special {@link DeferredHolder} for {@link DataComponentType DataComponentTypes}.
@@ -27,7 +26,7 @@ public class DeferredDataComponentType<TComponent> extends DeferredHolder<DataCo
      * @param <TComponent> The type of the target {@link DataComponentType}.
      * @param registryKey  The resource key of the target {@link DataComponentType}.
      */
-    public static <TComponent extends BlockEntity> DeferredDataComponentType<TComponent> createDataComponentType(ResourceKey<DataComponentType<?>> registryKey) {
+    public static <TComponent> DeferredDataComponentType<TComponent> createDataComponentType(ResourceKey<DataComponentType<?>> registryKey) {
         return new DeferredDataComponentType<>(registryKey);
     }
 
@@ -37,7 +36,7 @@ public class DeferredDataComponentType<TComponent> extends DeferredHolder<DataCo
      * @param <TComponent> The type of the target {@link DataComponentType}.
      * @param registryName The name of the target {@link DataComponentType}.
      */
-    public static <TComponent extends BlockEntity> DeferredDataComponentType<TComponent> createDataComponentType(ResourceLocation registryName) {
+    public static <TComponent> DeferredDataComponentType<TComponent> createDataComponentType(ResourceLocation registryName) {
         return createDataComponentType(ResourceKey.create(Registries.DATA_COMPONENT_TYPE, registryName));
     }
 }
