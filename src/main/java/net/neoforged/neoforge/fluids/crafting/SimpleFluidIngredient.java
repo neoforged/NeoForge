a351612a5335b161c919730bbfeb7860010600cf
max. <max@someone.ky>
2024-10-15 19:48:57 +0200

    Fix SimpleFluidIngredient's StreamCodec, cleanup

--- src/main/java/net/neoforged/neoforge/fluids/crafting/SimpleFluidIngredient.java
+++ src/main/java/net/neoforged/neoforge/fluids/crafting/SimpleFluidIngredient.java
@@ -6,15 +6,13 @@
 package net.neoforged.neoforge.fluids.crafting;
 
 import com.mojang.serialization.Codec;
-import com.mojang.serialization.DataResult;
-import com.mojang.serialization.DynamicOps;
-import com.mojang.serialization.MapCodec;
-import com.mojang.serialization.MapLike;
-import com.mojang.serialization.RecordBuilder;
 import java.util.stream.Stream;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.resources.HolderSetCodec;
 import net.minecraft.util.ExtraCodecs;
 import net.minecraft.world.level.material.Fluid;
@@ -30,29 +28,14 @@ import net.neoforged.neoforge.fluids.FluidStack;
  * though in JSON, it is still written <b>without</b> a type field, see {@link FluidIngredientCodecs#codec()}
  */
 public class SimpleFluidIngredient extends FluidIngredient {
-    public static final Codec<HolderSet<Fluid>> HOLDER_SET_NO_EMPTY_FLUID = HolderSetCodec.create(
+    private static final Codec<HolderSet<Fluid>> HOLDER_SET_NO_EMPTY_FLUID = HolderSetCodec.create(
             Registries.FLUID, FluidStack.FLUID_NON_EMPTY_CODEC, false);
 
-    public static final Codec<SimpleFluidIngredient> CODEC = ExtraCodecs.nonEmptyHolderSet(HOLDER_SET_NO_EMPTY_FLUID)
+    static final Codec<SimpleFluidIngredient> CODEC = ExtraCodecs.nonEmptyHolderSet(HOLDER_SET_NO_EMPTY_FLUID)
             .xmap(SimpleFluidIngredient::new, SimpleFluidIngredient::fluidSet);
 
-    // Note: This map codec explicitly always errors, since we want to force people to use HolderSet syntax!
-    public static final MapCodec<SimpleFluidIngredient> MAP_CODEC = new MapCodec<>() {
-        @Override
-        public <T> Stream<T> keys(DynamicOps<T> dynamicOps) {
-            return Stream.empty();
-        }
-
-        @Override
-        public <T> DataResult<SimpleFluidIngredient> decode(DynamicOps<T> ops, MapLike<T> mapLike) {
-            return DataResult.error(() -> "Simple fluid ingredients cannot be decoded using map syntax!");
-        }
-
-        @Override
-        public <T> RecordBuilder<T> encode(SimpleFluidIngredient ingredient, DynamicOps<T> ops, RecordBuilder<T> builder) {
-            return builder.withErrorsFrom(DataResult.error(() -> "Simple fluid ingredients cannot be encoded using map syntax! Please use vanilla syntax (namespaced:item or #tag) instead!"));
-        }
-    };
+    static final StreamCodec<RegistryFriendlyByteBuf, SimpleFluidIngredient> CONTENTS_STREAM_CODEC = ByteBufCodecs.holderSet(Registries.FLUID)
+            .map(SimpleFluidIngredient::new, SimpleFluidIngredient::fluidSet);
 
     private final HolderSet<Fluid> values;
 
