d15a61da7896ad4c9553d78871860cd16e6aa92f
dhyces <10985914+dhyces@users.noreply.github.com>
2024-11-18 23:48:00 -0500

    Add another fail condition

--- tests/src/main/java/net/neoforged/neoforge/debug/client/ClientEventTests.java
+++ tests/src/main/java/net/neoforged/neoforge/debug/client/ClientEventTests.java
@@ -147,7 +147,8 @@ public class ClientEventTests {
             listeners.forge().addListener((RenderPlayerEvent.Post event) -> {
                 int numRender = event.getRenderState().getRenderDataOrDefault(attachmentKey, -1);
                 if (numRender == -1) test.fail("Attachment render data not set");
-                float xRotation = event.getRenderState().getRenderDataOrDefault(key, 0f);
+                float xRotation = event.getRenderState().getRenderDataOrDefault(key, -1f);
+                if (numRender == -1) test.fail("Custom render data not set");
                 var poseStack = event.getPoseStack();
                 poseStack.pushPose();
                 for (int i = 0; i < numRender; i++) {
