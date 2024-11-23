64516c8ac549e61f3b7d5937e30349efe46e1af2
Apex <robsonlawson3@gmail.com>
2024-11-23 12:04:51 +0000

    Merge overloaded methods into vanilla code

--- src/main/java/net/neoforged/neoforge/common/extensions/IModelTemplateExtension.java
+++ src/main/java/net/neoforged/neoforge/common/extensions/IModelTemplateExtension.java
@@ -5,68 +5,11 @@
 
 package net.neoforged.neoforge.common.extensions;
 
-import com.google.common.base.Preconditions;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import java.util.Map;
-import java.util.function.BiConsumer;
-import java.util.function.Supplier;
-import net.minecraft.data.models.model.ModelLocationUtils;
 import net.minecraft.data.models.model.ModelTemplate;
-import net.minecraft.data.models.model.TextureMapping;
-import net.minecraft.data.models.model.TextureSlot;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.Block;
-import net.neoforged.neoforge.client.model.generators.ModelProvider;
-import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.common.data.vanilla.ModelTemplateWithCustomData;
-import org.jetbrains.annotations.Nullable;
 
 public interface IModelTemplateExtension {
-    default ResourceLocation create(Block block, TextureMapping textures, BiConsumer<ResourceLocation, Supplier<JsonElement>> modelOutput, @Nullable ExistingFileHelper fileHelper) {
-        return create(ModelLocationUtils.getModelLocation(block, self().suffix.orElse("")), textures, modelOutput, fileHelper);
-    }
-
-    default ResourceLocation createWithSuffix(Block block, String suffix, TextureMapping textures, BiConsumer<ResourceLocation, Supplier<JsonElement>> modelOutput, @Nullable ExistingFileHelper fileHelper) {
-        return create(ModelLocationUtils.getModelLocation(block, suffix + self().suffix.orElse("")), textures, modelOutput, fileHelper);
-    }
-
-    default ResourceLocation createWithOverride(Block block, String suffix, TextureMapping textures, BiConsumer<ResourceLocation, Supplier<JsonElement>> modelOutput, @Nullable ExistingFileHelper fileHelper) {
-        return create(ModelLocationUtils.getModelLocation(block, suffix), textures, modelOutput, fileHelper);
-    }
-
-    default ResourceLocation create(ResourceLocation modelPath, TextureMapping textures, BiConsumer<ResourceLocation, Supplier<JsonElement>> modelOutput, @Nullable ExistingFileHelper fileHelper) {
-        return self().create(modelPath, textures, modelOutput, (path, output) -> createBaseTemplate(path, output, fileHelper));
-    }
-
-    default JsonObject createBaseTemplate(ResourceLocation modelPath, Map<TextureSlot, ResourceLocation> textureMap, @Nullable ExistingFileHelper fileHelper) {
-        var modelJson = new JsonObject();
-
-        self().model.ifPresent(parentPath -> {
-            modelJson.addProperty("parent", parentPath.toString());
-
-            if (fileHelper != null) {
-                Preconditions.checkState(fileHelper.exists(parentPath, ModelProvider.MODEL), "Model at %s does not exist", parentPath);
-            }
-        });
-
-        if (!textureMap.isEmpty()) {
-            var texturesJson = new JsonObject();
-
-            textureMap.forEach((slot, texturePath) -> {
-                texturesJson.addProperty(slot.getId(), texturePath.toString());
-
-                if (fileHelper != null) {
-                    Preconditions.checkState(fileHelper.exists(texturePath, ModelProvider.TEXTURE), "Texture %s does not exist in any known resource pack", texturePath);
-                }
-            });
-
-            modelJson.add("textures", texturesJson);
-        }
-
-        return modelJson;
-    }
-
     default ModelTemplate withRenderType(ResourceLocation renderType) {
         return new ModelTemplateWithCustomData(self(), renderType);
     }
