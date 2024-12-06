7eb908d3f7232642bd3945ac9c6458fc3d080f30
Matyrobbrt <matyrobbrt@gmail.com>
2024-12-06 13:57:04 +0200

    Updates

--- buildSrc/src/main/java/net/neoforged/neodev/GenerateAccessTransformers.java
+++ buildSrc/src/main/java/net/neoforged/neodev/GenerateAccessTransformers.java
@@ -1,34 +1,36 @@
 package net.neoforged.neodev;
 
-import org.apache.commons.lang3.mutable.MutableInt;
+import net.neoforged.neodev.utils.FileUtils;
+import net.neoforged.neodev.utils.SerializablePredicate;
+import net.neoforged.neodev.utils.structure.ClassStructureVisitor;
+import net.neoforged.neodev.utils.structure.ClassInfo;
+import net.neoforged.neodev.utils.structure.FieldInfo;
+import net.neoforged.neodev.utils.structure.MethodInfo;
 import org.gradle.api.DefaultTask;
+import org.gradle.api.Named;
 import org.gradle.api.file.RegularFileProperty;
 import org.gradle.api.provider.ListProperty;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputFile;
 import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassVisitor;
-import org.objectweb.asm.FieldVisitor;
-import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.Type;
 
 import javax.annotation.Nullable;
 import java.io.IOException;
 import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.IntStream;
-import java.util.zip.ZipFile;
+import java.util.function.IntPredicate;
+import java.util.stream.Collectors;
 
 public abstract class GenerateAccessTransformers extends DefaultTask {
-    public static final Modifier PUBLIC = mod("public", false, Opcodes.ACC_PUBLIC);
-    public static final Modifier PROTECTED = mod("protected", false, Opcodes.ACC_PUBLIC, Opcodes.ACC_PROTECTED);
+    public static final Modifier PUBLIC = new Modifier("public", false, Opcodes.ACC_PUBLIC);
+    public static final Modifier PROTECTED = new Modifier("protected", false, Opcodes.ACC_PUBLIC, Opcodes.ACC_PROTECTED);
 
     @InputFile
     public abstract RegularFileProperty getInput();
@@ -42,58 +44,64 @@ public abstract class GenerateAccessTransformers extends DefaultTask {
     @TaskAction
     public void exec() throws IOException {
         Map<String, ClassInfo> targets = new HashMap<>();
-        var visitor = visitor(targets);
 
-        try (var zip = new ZipFile(getInput().getAsFile().get())) {
-            var entries = zip.entries();
-            while (entries.hasMoreElements()) {
-                var next = entries.nextElement();
-                if (next.isDirectory() || !next.getName().endsWith(".class")) continue;
-
-                var lastInner = next.getName().lastIndexOf("$");
-                if (lastInner >= 0) {
-                    // Skip anonymous classes
-                    if (Character.isDigit(next.getName().charAt(lastInner + 1))) continue;
-                }
-
-                try (var in = zip.getInputStream(next)) {
-                    var reader = new ClassReader(in);
-                    reader.accept(visitor, ClassReader.SKIP_CODE | ClassReader.SKIP_DEBUG | ClassReader.SKIP_FRAMES);
-                }
-            }
-        }
+        // First we collect all classes
+        new ClassStructureVisitor(targets).visit(getInput().getAsFile().get());
 
         var groupList = getGroups().get();
-        List<String>[] groups = IntStream.range(0, groupList.size())
-                .mapToObj(i -> new ArrayList<String>()).toArray(List[]::new);
 
+        List<String>[] groups = new List[groupList.size()];
+        for (int i = 0; i < groupList.size(); i++) {
+            groups[i] = new ArrayList<>();
+        }
+
+        // Now we check each class against each group and see if the group wants to handle it
         for (ClassInfo value : targets.values()) {
             for (int i = 0; i < groupList.size(); i++) {
                 var group = groupList.get(i);
                 if (group.classMatch.test(value)) {
+                    var lastInner = value.name().lastIndexOf("$");
+                    // Skip anonymous classes
+                    if (lastInner >= 0 && Character.isDigit(value.name().charAt(lastInner + 1))) {
+                        continue;
+                    }
+
+                    // fieldMatch is non-null only for field ATs
                     if (group.fieldMatch != null) {
-                        for (var field : value.fields) {
-                            if (group.fieldMatch.test(field) && !group.modifier.test(field.access)) {
-                                groups[i].add(group.modifier + " " + value.name.replace('/', '.') + " " + field.name);
+                        for (var field : value.fields()) {
+                            if (group.fieldMatch.test(field) && !group.modifier.test(field.access())) {
+                                groups[i].add(group.modifier.name + " " + value.name().replace('/', '.') + " " + field.name());
                             }
                         }
-                    } else if (group.methodMatch != null) {
-                        for (var method : value.methods) {
-                            if (group.methodMatch.test(method) && !group.modifier.test(method.access)) {
-                                groups[i].add(group.modifier + " " + value.name.replace('/', '.') + " " + method.name + method.descriptor);
+                    }
+                    // methodMatch is non-null only for group ATs
+                    else if (group.methodMatch != null) {
+                        for (var method : value.methods()) {
+                            if (group.methodMatch.test(method) && !group.modifier.test(method.access())) {
+                                groups[i].add(group.modifier.name + " " + value.name().replace('/', '.') + " " + method.name() + method.descriptor());
                             }
                         }
-                    } else if (!group.modifier.test(value.access.intValue())) {
-                        groups[i].add(group.modifier + " " + value.name.replace('/', '.'));
+                    }
+                    // If there's neither a field nor a method predicate, this is a class AT
+                    else if (!group.modifier.test(value.access().intValue())) {
+                        groups[i].add(group.modifier.name + " " + value.name().replace('/', '.'));
                     }
                 }
             }
         }
 
+        // Dump the ATs
         var text = new StringBuilder();
         for (int i = 0; i < groups.length; i++) {
-            text.append("#group ").append(groupList.get(i).name).append('\n')
-                    .append(String.join("\n", groups[i])).append("\n#endgroup\n");
+            // Check if the group found no targets. If it didn't, there's probably an error in the test and it should be reported
+            if (groups[i].isEmpty()) {
+                throw new IllegalStateException("Generated AT group '" + groupList.get(i).name + "' found no entries!");
+            }
+            text.append("# ").append(groupList.get(i).name).append('\n');
+            text.append(groups[i].stream().sorted().collect(Collectors.joining("\n")));
+            text.append('\n');
+
+            if (i < groups.length - 1) text.append('\n');
         }
 
         var outFile = getAccessTransformer().getAsFile().get().toPath();
@@ -101,7 +109,7 @@ public abstract class GenerateAccessTransformers extends DefaultTask {
             Files.createDirectories(outFile.getParent());
         }
 
-        Files.writeString(outFile, text);
+        FileUtils.writeStringSafe(outFile, text.toString(), StandardCharsets.UTF_8);
     }
 
     public void classGroup(String name, Modifier modifier, SerializablePredicate<ClassInfo> match) {
@@ -116,130 +124,47 @@ public abstract class GenerateAccessTransformers extends DefaultTask {
         getGroups().add(new AtGroup(name, modifier, targetTest, null, fieldTest));
     }
 
-    public SerializablePredicate<ClassInfo> matchClass(String name) {
-        return target -> target.name.equals(name);
+    public <T extends Named> SerializablePredicate<T> named(String name) {
+        return target -> target.getName().equals(name);
     }
 
-    public SerializablePredicate<ClassInfo> matchClassesWithSuperclass(String superClass) {
+    public SerializablePredicate<ClassInfo> classesWithSuperclass(String superClass) {
         return target -> target.hasSuperclass(superClass);
     }
 
-    public SerializablePredicate<ClassInfo> matchInnerClassesOf(String parent) {
+    public SerializablePredicate<ClassInfo> innerClassesOf(String parent) {
         var parentFullName = parent + "$";
-        return target -> target.name.startsWith(parentFullName);
+        return target -> target.name().startsWith(parentFullName);
     }
 
-    public SerializablePredicate<MethodInfo> matchMethodsWithName(String name) {
-        return methodInfo -> methodInfo.name.equals(name);
-    }
-
-    public SerializablePredicate<MethodInfo> matchMethodsReturning(String type) {
+    public SerializablePredicate<MethodInfo> methodsReturning(String type) {
         var endMatch = ")L" + type + ";";
-        return methodInfo -> methodInfo.descriptor.endsWith(endMatch);
+        return methodInfo -> methodInfo.descriptor().endsWith(endMatch);
     }
 
-    public SerializablePredicate<FieldInfo> matchFieldsOfType(SerializablePredicate<ClassInfo> type) {
-        return value -> type.test(value.type);
+    public SerializablePredicate<FieldInfo> fieldsOfType(SerializablePredicate<ClassInfo> type) {
+        return value -> type.test(value.type());
     }
 
     public <T> SerializablePredicate<T> matchAny() {
         return value -> true;
     }
 
-    private ClassVisitor visitor(Map<String, ClassInfo> targets) {
-        return new ClassVisitor(Opcodes.ASM9) {
-            ClassInfo current;
-
-            @Override
-            public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
-                current = GenerateAccessTransformers.getClass(targets, name);
-                current.access.setValue(access);
-                if (superName != null) {
-                    current.parents.add(GenerateAccessTransformers.getClass(targets, superName));
-                }
-                for (String iface : interfaces) {
-                    current.parents.add(GenerateAccessTransformers.getClass(targets, iface));
-                }
-            }
-
-            @Override
-            public void visitInnerClass(String name, String outerName, String innerName, int access) {
-                if (name.equals(current.name)) {
-                    current.access.setValue(access);
-                }
-            }
-
-            @Override
-            public FieldVisitor visitField(int access, String name, String descriptor, String signature, Object value) {
-                current.fields.add(new FieldInfo(name, GenerateAccessTransformers.getClass(targets, Type.getType(descriptor).getInternalName()), access));
-                return null;
-            }
-
-            @Override
-            public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
-                current.addMethod(name, descriptor, access);
-                return null;
-            }
-        };
-    }
-
-    private static ClassInfo getClass(Map<String, ClassInfo> targets, String name) {
-        var existing = targets.get(name);
-        if (existing != null) return existing;
-        existing = new ClassInfo(name, new MutableInt(0), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
-        targets.put(name, existing);
-        return existing;
-    }
-
-    public record ClassInfo(String name, MutableInt access, List<ClassInfo> parents, List<MethodInfo> methods, List<FieldInfo> fields) {
-        public void addMethod(String name, String desc, int access) {
-            this.methods.add(new MethodInfo(name, desc, access));
-        }
-
-        public boolean hasSuperclass(String name) {
-            for (ClassInfo parent : parents) {
-                if (parent.hasSuperclass(name)) {
-                    return true;
-                }
-            }
-            return this.name.equals(name);
-        }
-    }
-    public record MethodInfo(String name, String descriptor, int access) {}
-    public record FieldInfo(String name, ClassInfo type, int access) {}
-
     public record AtGroup(String name, Modifier modifier, SerializablePredicate<ClassInfo> classMatch,
                           @Nullable SerializablePredicate<MethodInfo> methodMatch, @Nullable SerializablePredicate<FieldInfo> fieldMatch) implements Serializable {
     }
 
-    @FunctionalInterface
-    public interface SerializablePredicate<T> extends Serializable {
-        boolean test(T value);
-    }
+    public record Modifier(String name, boolean isFinal, int... validOpcodes) implements Serializable, IntPredicate {
+        @Override
+        public boolean test(int value) {
+            if (isFinal && (value & Opcodes.ACC_FINAL) == 0) return false;
 
-    @FunctionalInterface
-    public interface Modifier extends Serializable {
-        boolean test(int value);
-    }
-
-    private static Modifier mod(String name, boolean isFinal, int... validOpcodes) {
-        return new Modifier() {
-            @Override
-            public boolean test(int value) {
-                if (isFinal && (value & Opcodes.ACC_FINAL) == 0) return false;
-
-                for (int validOpcode : validOpcodes) {
-                    if ((value & validOpcode) != 0) {
-                        return true;
-                    }
+            for (int validOpcode : validOpcodes) {
+                if ((value & validOpcode) != 0) {
+                    return true;
                 }
-                return false;
             }
-
-            @Override
-            public String toString() {
-                return name;
-            }
-        };
+            return false;
+        }
     }
 }
