1cb8c4ce52d7afdcebbebc8d56148221fd6683e3
Apex <robsonlawson3@gmail.com>
2024-12-16 08:37:05 +0000

    Reword javadoc

--- src/main/java/net/neoforged/neoforge/client/model/generators/template/RotationBuilder.java
+++ src/main/java/net/neoforged/neoforge/client/model/generators/template/RotationBuilder.java
@@ -52,7 +52,7 @@ public final class RotationBuilder {
     }
 
     /**
-     * Sets whether or not to scale the faces across the whole model when rotation has been applied.
+     * Sets whether or not the quad should be scaled after rotation to maintain its relative size.
      */
     public RotationBuilder rescale(boolean rescale) {
         this.rescale = rescale;
