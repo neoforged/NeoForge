2767d2d697f25fb56b52a79123eae34d52766b6e
XFactHD <11262040+XFactHD@users.noreply.github.com>
2025-01-23 18:27:20 +0000

    Backport #1674 to 1.21.1

--- src/main/java/net/neoforged/neoforge/client/ClientHooks.java
+++ src/main/java/net/neoforged/neoforge/client/ClientHooks.java
@@ -1122,6 +1122,19 @@ public class ClientHooks {
         return RECIPE_BOOK_TYPES;
     }
 
+    private static final RandomSource OUTLINE_PASS_RANDOM = RandomSource.create();
+
+    public static boolean isInTranslucentBlockOutlinePass(Level level, BlockPos pos, BlockState state) {
+        BakedModel model = Minecraft.getInstance().getBlockRenderer().getBlockModel(state);
+        OUTLINE_PASS_RANDOM.setSeed(42);
+        ChunkRenderTypeSet renderTypes = model.getRenderTypes(state, OUTLINE_PASS_RANDOM, level.getModelData(pos));
+        return renderTypes.contains(RenderType.TRANSLUCENT) || renderTypes.contains(RenderType.TRIPWIRE);
+    }
+
+    public static void reloadRenderer() {
+        Minecraft.getInstance().levelRenderer.allChanged();
+    }
+
     public static Map<ResourceLocation, ResourceLocation> gatherMaterialAtlases(Map<ResourceLocation, ResourceLocation> vanillaAtlases) {
         vanillaAtlases = new HashMap<>(vanillaAtlases);
         ModLoader.postEvent(new RegisterMaterialAtlasesEvent(vanillaAtlases));
