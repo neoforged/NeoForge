e679d21d3a82289f29d5f9c9b60d0b879f7c5118
IThundxr <me@ithundxr.dev>
2025-02-23 10:12:31 -0500

    format

--- src/main/java/net/neoforged/neoforge/registries/RegistryBuilder.java
+++ src/main/java/net/neoforged/neoforge/registries/RegistryBuilder.java
@@ -46,7 +46,7 @@ public class RegistryBuilder<T> {
 
     /**
      * @deprecated Vanilla has deprecated intrusive holders and is in the process of moving away from them,
-     * you should not use this unless you have no better option.
+     *             you should not use this unless you have no better option.
      */
     @Deprecated
     public RegistryBuilder<T> withIntrusiveHolders() {
