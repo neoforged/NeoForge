b9b79622f4ec58ab5816fb2ca69925dae8ba2630
Sara Freimer <sara@freimer.com>
2025-03-08 15:05:19 -0600

    Fix import order

--- src/main/java/net/neoforged/neoforge/registries/IRegistryExtension.java
+++ src/main/java/net/neoforged/neoforge/registries/IRegistryExtension.java
@@ -6,7 +6,6 @@
 package net.neoforged.neoforge.registries;
 
 import java.util.Map;
-import org.jetbrains.annotations.Nullable;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
@@ -15,6 +14,7 @@ import net.neoforged.neoforge.registries.callback.BakeCallback;
 import net.neoforged.neoforge.registries.callback.ClearCallback;
 import net.neoforged.neoforge.registries.callback.RegistryCallback;
 import net.neoforged.neoforge.registries.datamaps.DataMapType;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * An extension for {@link Registry}, adding some additional functionality to vanilla registries, such as
