d7b4f81be7ed603c91dab2df73a42ed8ad0ab53e
ChampionAsh5357 <ash@ashwork.net>
2025-03-19 15:17:17 -0400

    chore(keybinds): Update comment

--- src/main/java/net/neoforged/neoforge/client/settings/KeyMappingLookup.java
+++ src/main/java/net/neoforged/neoforge/client/settings/KeyMappingLookup.java
@@ -85,7 +85,7 @@ public class KeyMappingLookup {
                     .toList());
         }
 
-        // If there were no matches, check the key without any modifiers
+        // If there were no matches or a key is being released, check the key without any modifiers
         if (releasing || matchingBindings.isEmpty()) {
             matchingBindings.addAll(findKeybinds(keyCode, KeyModifier.NONE));
         }
