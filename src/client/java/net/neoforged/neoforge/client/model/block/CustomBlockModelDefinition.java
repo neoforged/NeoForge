b83ffa9214b62ee953ce88993856f2cc03ab2ad8
XFactHD <xfacthd@gmx.de>
2025-03-25 15:37:29 +0100

    Address review comments

--- src/client/java/net/neoforged/neoforge/client/model/block/CustomBlockModelDefinition.java
+++ src/client/java/net/neoforged/neoforge/client/model/block/CustomBlockModelDefinition.java
@@ -8,6 +8,7 @@ package net.neoforged.neoforge.client.model.block;
 import com.mojang.serialization.MapCodec;
 import java.util.Map;
 import java.util.function.Supplier;
+import net.minecraft.client.renderer.block.model.BlockModelDefinition;
 import net.minecraft.client.renderer.block.model.BlockStateModel;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -23,6 +24,8 @@ public interface CustomBlockModelDefinition {
      * @param states         The {@link StateDefinition} of the block this definition is being instantiated for
      * @param sourceSupplier A {@link Supplier} providing the source file and source pack name for debugging
      * @return a map of {@link BlockState}s to {@link BlockStateModel.UnbakedRoot}s for all states of the provided state definition
+     *
+     * @see BlockModelDefinition#instantiateVanilla(StateDefinition, Supplier)
      */
     Map<BlockState, BlockStateModel.UnbakedRoot> instantiate(StateDefinition<Block, BlockState> states, Supplier<String> sourceSupplier);
 
