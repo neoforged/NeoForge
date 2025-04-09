73205492a30e24aa3a2d0a81fcb8d61b6ceb3b36
Sebastian Hartte <shartte@users.noreply.github.com>
2025-04-09 15:32:28 +0200

    Try adding a builder

--- src/client/java/net/neoforged/neoforge/client/model/generators/blockstate/UnbakedMutator.java
+++ src/client/java/net/neoforged/neoforge/client/model/generators/blockstate/UnbakedMutator.java
@@ -8,6 +8,11 @@ package net.neoforged.neoforge.client.model.generators.blockstate;
 import net.minecraft.client.renderer.block.model.BlockStateModel;
 import net.minecraft.client.renderer.block.model.SingleVariant;
 import net.minecraft.client.renderer.block.model.Variant;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.IdentityHashMap;
+import java.util.Map;
+import java.util.function.UnaryOperator;
 
 public interface UnbakedMutator {
     default Variant apply(Variant variant) {
@@ -15,4 +20,51 @@ public interface UnbakedMutator {
     }
 
     <T extends BlockStateModel.Unbaked> T apply(T unbaked);
+
+    static Builder builder() {
+        return new Builder();
+    }
+
+    class Builder {
+        private final Map<Class<?>, Handler<?>> handlers = new IdentityHashMap<>();
+
+        private Builder() {
+        }
+
+        public <T extends BlockStateModel.Unbaked> Builder add(Class<T> supportedClass, UnaryOperator<T> operator) {
+            if (handlers.containsKey(supportedClass)) {
+                throw new IllegalStateException("There is already a mutator registered for " + supportedClass);
+            }
+            handlers.put(supportedClass, new Handler<>(supportedClass, operator));
+            return this;
+        }
+
+        public UnbakedMutator build() {
+            return new UnbakedMutator() {
+                @Override
+                public <T extends BlockStateModel.Unbaked> T apply(T unbaked) {
+                    var handler = getHandler(unbaked);
+                    if (handler == null) {
+                        throw new UnsupportedOperationException(
+                                "This unbaked transform cannot be applied to unbaked model " + unbaked.getClass()
+                                        + ", it only supports: " + handlers.keySet());
+                    }
+
+                    return handler.apply(unbaked);
+                }
+
+                @Nullable
+                @SuppressWarnings("unchecked")
+                private <T extends BlockStateModel.Unbaked> Handler<T> getHandler(T unbaked) {
+                    return (Handler<T>) handlers.get(unbaked.getClass());
+                }
+            };
+        }
+
+        private record Handler<T>(Class<T> supportedClass, UnaryOperator<T> operator) {
+            public T apply(BlockStateModel.Unbaked unbaked) {
+                return supportedClass.cast(unbaked);
+            }
+        }
+    }
 }
