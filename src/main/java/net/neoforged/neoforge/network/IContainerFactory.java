d23775aeaf480e3cc82f6e2485932e6b97456af1
Sebastian Hartte <shartte@users.noreply.github.com>
2025-04-18 11:25:06 +0200

    Fix backport issues.

--- src/main/java/net/neoforged/neoforge/network/IContainerFactory.java
+++ src/main/java/net/neoforged/neoforge/network/IContainerFactory.java
@@ -11,6 +11,7 @@ import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuType;
+import net.neoforged.neoforge.client.extensions.IMenuProviderExtension;
 
 /**
  * This extension of {@link MenuType.MenuSupplier} allows a mod to handle the extra data it sent to the client
@@ -23,7 +24,7 @@ public interface IContainerFactory<T extends AbstractContainerMenu> extends Menu
      * @param windowId The {@link AbstractContainerMenu#containerId} of the menu on the server-side.
      * @param inv      Player inventory of the player for whom the menu is being created.
      * @param data     Additional data written by the server when the menu was opened.
-     *                 It contains any data written by {@link net.neoforged.neoforge.common.extensions.IMenuProviderExtension#writeClientSideData(AbstractContainerMenu, RegistryFriendlyByteBuf)},
+     *                 It contains any data written by {@link IMenuProviderExtension#writeClientSideData(AbstractContainerMenu, RegistryFriendlyByteBuf)},
      *                 followed by optional contextual data written by the {@code extraDataWriter} argument to
      *                 {@link net.neoforged.neoforge.common.extensions.IPlayerExtension#openMenu(MenuProvider, Consumer)}.
      * @return The menu instance to use on the client-side.
