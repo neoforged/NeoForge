de94f2572a8683f0004867afb0f7f6017f046deb
sciwhiz12 <arnoldnunag12@gmail.com>
2025-04-19 18:30:35 +0800

    Change test to be parameterized

--- tests/src/junit/java/net/neoforged/neoforge/unittest/ExtraCodecsTests.java
+++ tests/src/junit/java/net/neoforged/neoforge/unittest/ExtraCodecsTests.java
@@ -5,45 +5,50 @@
 
 package net.neoforged.neoforge.unittest;
 
-import com.google.gson.JsonElement;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
+import com.mojang.serialization.DynamicOps;
 import com.mojang.serialization.JsonOps;
 import java.util.Map;
+import java.util.stream.Stream;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.NbtOps;
-import net.minecraft.nbt.Tag;
 import net.neoforged.neoforge.common.util.NeoForgeExtraCodecs;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class ExtraCodecsTests {
-    @Test
-    public void test_unboundedMapAsList() {
-        Map<BlockPos, String> values = Map.of(
-                new BlockPos(8, 0, 0), "testA",
-                new BlockPos(0, 8, 0), "testB",
-                new BlockPos(0, 0, 8), "testC");
-        Codec<Map<BlockPos, String>> codec = NeoForgeExtraCodecs.unboundedMapAsList("position", BlockPos.CODEC, "value", Codec.STRING);
+    private final Map<BlockPos, String> values = Map.of(
+            new BlockPos(8, 0, 0), "testA",
+            new BlockPos(0, 8, 0), "testB",
+            new BlockPos(0, 0, 8), "testC");
 
-        DataResult<JsonElement> resultJson = codec.encodeStart(JsonOps.INSTANCE, values);
-        assertTrue(resultJson.isSuccess(), "Encode to JSON should succeed");
+    private final Codec<Map<BlockPos, String>> codec = NeoForgeExtraCodecs.unboundedMapAsList("position", BlockPos.CODEC, "value", Codec.STRING);
 
-        DataResult<Tag> resultTag = codec.encodeStart(NbtOps.INSTANCE, values);
-        assertTrue(resultTag.isSuccess(), "Encode to NBT should succeed");
+    @DisplayName("test round-trip for unboundedMapAsList codec")
+    @ParameterizedTest(name = "using ops for {1}")
+    @MethodSource("provideDynamicOps")
+    public <T> void roundTripTest_unboundedMapAsList(DynamicOps<T> ops, String name) {
+        DataResult<T> resultJson = codec.encodeStart(ops, values);
+        assertTrue(resultJson.isSuccess(), "Encode to " + name + " should succeed");
 
-        DataResult<Map<BlockPos, String>> resultJsonBack = codec.parse(JsonOps.INSTANCE, resultJson.getOrThrow());
-        assertTrue(resultJsonBack.isSuccess(), "Decode from JSON should succeed");
-
-        DataResult<Map<BlockPos, String>> resultTagBack = codec.parse(NbtOps.INSTANCE, resultTag.getOrThrow());
-        assertTrue(resultTagBack.isSuccess(), "Decode from NBT should succeed");
+        DataResult<Map<BlockPos, String>> resultJsonBack = codec.parse(ops, resultJson.getOrThrow());
+        assertTrue(resultJsonBack.isSuccess(), "Decode from " + name + " should succeed");
 
         Map<BlockPos, String> jsonDecoded = resultJsonBack.getOrThrow();
-        assertEquals(values, jsonDecoded, "JSON round-tripped map should be equal to original map");
+        assertEquals(values, jsonDecoded, name + "JSON round-tripped map should be equal to original map");
+    }
 
-        Map<BlockPos, String> tagDecoded = resultTagBack.getOrThrow();
-        assertEquals(values, tagDecoded, "NBT round-tripped map should be equal to original map");
+    private static Stream<Arguments> provideDynamicOps() {
+        return Stream.of(
+                arguments(NbtOps.INSTANCE, "NBT"),
+                arguments(JsonOps.INSTANCE, "JSON")
+        );
     }
 }
