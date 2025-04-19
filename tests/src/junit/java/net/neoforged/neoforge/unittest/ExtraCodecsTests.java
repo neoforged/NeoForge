df4c152ce4f26889dc67d19149bb4a96f46a2160
sciwhiz12 <arnoldnunag12@gmail.com>
2025-04-19 18:14:35 +0800

    Add round-trip to test

--- tests/src/junit/java/net/neoforged/neoforge/unittest/ExtraCodecsTests.java
+++ tests/src/junit/java/net/neoforged/neoforge/unittest/ExtraCodecsTests.java
@@ -5,8 +5,6 @@
 
 package net.neoforged.neoforge.unittest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import com.google.gson.JsonElement;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
@@ -17,6 +15,9 @@ import net.minecraft.nbt.NbtOps;
 import net.minecraft.nbt.Tag;
 import net.neoforged.neoforge.common.util.NeoForgeExtraCodecs;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ExtraCodecsTests {
     @Test
@@ -33,5 +34,17 @@ public class ExtraCodecsTests {
 
         DataResult<Tag> resultTag = codec.encodeStart(NbtOps.INSTANCE, values);
         assertTrue(resultTag.isSuccess(), "Encode to NBT should succeed");
+
+        DataResult<Map<BlockPos, String>> resultJsonBack = codec.parse(JsonOps.INSTANCE, resultJson.getOrThrow());
+        assertTrue(resultJsonBack.isSuccess(), "Decode from JSON should succeed");
+
+        DataResult<Map<BlockPos, String>> resultTagBack = codec.parse(NbtOps.INSTANCE, resultTag.getOrThrow());
+        assertTrue(resultTagBack.isSuccess(), "Decode from NBT should succeed");
+
+        Map<BlockPos, String> jsonDecoded = resultJsonBack.getOrThrow();
+        assertEquals(values, jsonDecoded, "JSON round-tripped map should be equal to original map");
+
+        Map<BlockPos, String> tagDecoded = resultTagBack.getOrThrow();
+        assertEquals(values, tagDecoded, "NBT round-tripped map should be equal to original map");
     }
 }
