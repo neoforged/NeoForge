95791fb4b6ac9d3b1725120ff4d29c13f693c6ef
Matyrobbrt <65940752+Matyrobbrt@users.noreply.github.com>
2025-04-19 10:25:42 +0000

    Backport #2158 to 1.20.4

--- src/main/java/net/neoforged/neoforge/common/data/GlobalLootModifierProvider.java
+++ src/main/java/net/neoforged/neoforge/common/data/GlobalLootModifierProvider.java
@@ -15,7 +15,7 @@ import cpw.mods.modlauncher.api.LamdbaExceptionUtils;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
