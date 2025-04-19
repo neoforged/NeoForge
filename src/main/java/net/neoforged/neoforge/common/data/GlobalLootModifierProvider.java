64545b5161399149d350f92f5a31675ddbcb0c8a
Matyrobbrt <65940752+Matyrobbrt@users.noreply.github.com>
2025-04-19 10:25:48 +0000

    Backport #2158 to 1.20.6

--- src/main/java/net/neoforged/neoforge/common/data/GlobalLootModifierProvider.java
+++ src/main/java/net/neoforged/neoforge/common/data/GlobalLootModifierProvider.java
@@ -12,7 +12,7 @@ import com.google.gson.JsonObject;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -38,7 +38,7 @@ public abstract class GlobalLootModifierProvider implements DataProvider {
     private final PackOutput output;
     private final CompletableFuture<HolderLookup.Provider> registries;
     private final String modid;
-    private final Map<String, WithConditions<IGlobalLootModifier>> toSerialize = new HashMap<>();
+    private final Map<String, WithConditions<IGlobalLootModifier>> toSerialize = new LinkedHashMap<>();
     private boolean replace = false;
 
     public GlobalLootModifierProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries, String modid) {
