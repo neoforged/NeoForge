52fa00e0fa23b75558b7b268e69a57b8fdaeff16
Thiakil <thiakil@users.noreply.github.com>
2025-04-25 18:04:18 +0800

    add ability to get attachment data without an optional or creation

--- src/main/java/net/neoforged/neoforge/attachment/IAttachmentHolder.java
+++ src/main/java/net/neoforged/neoforge/attachment/IAttachmentHolder.java
@@ -52,7 +52,15 @@ public interface IAttachmentHolder {
      *
      * <p>If there is no data attachment of the given type, an empty optional is returned.
      */
-    <T> Optional<T> getExistingData(AttachmentType<T> type);
+    default <T> Optional<T> getExistingData(AttachmentType<T> type) {
+        return Optional.ofNullable(getExistingDataNullable(type));
+    }
+
+    /**
+     * @return an existing data attachment value of the given type, or null if there is no data attachment of the given type
+     */
+    @Nullable
+    <T> T getExistingDataNullable(AttachmentType<T> type);
 
     /**
      * {@return an optional possibly containing a data attachment value of the given type}
@@ -61,6 +69,14 @@ public interface IAttachmentHolder {
      */
     default <T> Optional<T> getExistingData(Supplier<AttachmentType<T>> type) {
         return getExistingData(type.get());
+    }
+
+    /**
+     * @return an existing data attachment value of the given type, or null if there is no data attachment of the given type
+     */
+    @Nullable
+    default <T> T getExistingDataNullable(Supplier<AttachmentType<T>> type) {
+        return getExistingDataNullable(type.get());
     }
 
     /**
