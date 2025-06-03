fd6bd3cb8ea07938976e08fb8318dbd0f0bc1dda
XFactHD <xfacthd@gmx.de>
2025-06-04 00:25:02 +0200

    Fix root transforms with arbitrary rotations creating mangled quads

---
+++ src/client/java/net/neoforged/neoforge/client/extensions/ModelStateExtension.java
@@ -0,0 +1,18 @@
+package net.neoforged.neoforge.client.extensions;
+
+import net.minecraft.client.resources.model.BlockModelRotation;
+import net.minecraft.client.resources.model.ModelState;
+
+public interface ModelStateExtension {
+    private ModelState self() {
+        return (ModelState) this;
+    }
+
+    /**
+     * {@return whether this model state may apply a rotation that is not a multiple of 90 degrees}
+     */
+    default boolean mayApplyArbitraryRotation() {
+        ModelState self = self();
+        return !(self instanceof BlockModelRotation || self instanceof BlockModelRotation.WithUvLock);
+    }
+}
