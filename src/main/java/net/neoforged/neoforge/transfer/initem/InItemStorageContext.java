64c6405df3e605303b9141587ae708638c304731
Technici4n <13494793+Technici4n@users.noreply.github.com>
2025-06-05 01:01:04 +0200

    Move more user-facing methods to the top of InItemStorageContext

--- src/main/java/net/neoforged/neoforge/transfer/initem/InItemStorageContext.java
+++ src/main/java/net/neoforged/neoforge/transfer/initem/InItemStorageContext.java
@@ -25,81 +25,6 @@ import org.jetbrains.annotations.Nullable;
  */
 public interface InItemStorageContext {
     /**
-     * @return The current variant of the item that contains the storage that should be accessed. This may
-     * be blank or become blank if {@link #extract} is used.
-     */
-    ItemVariant getCurrent();
-
-    /**
-     * @return The current amount of {@linkplain #getCurrent() the item} containing the storage. Always returns 0 if
-     * {@linkplain #getCurrent() the current item} is blank.
-     */
-    long getCurrentAmount();
-
-    /**
-     * @return True if any attempts to modify the items in this context will not work. Storage implementations can
-     * use this to return appropriate information from {@link Storage#supportsInsertion()}
-     * or {@link Storage#supportsExtraction()}.
-     */
-    default boolean supportsModification() {
-        return true;
-    }
-
-    /**
-     * Transactionally insert an item into the "slot" representing the location of the item containing the storage.
-     * <p>
-     * If the inserted item is not stackable with the current item, it may be inserted in a place that is inaccessible
-     * for {@link #extract}, such as the player inventory.
-     *
-     * @see Storage#insert
-     */
-    long insert(ItemVariant itemVariant, long maxAmount, TransactionContext transaction);
-
-    /**
-     * Transactionally extract some of the current item.
-     *
-     * @see Storage#extract
-     */
-    long extract(ItemVariant itemVariant, long maxAmount, TransactionContext transaction);
-
-    /**
-     * Try to exchange as many items as possible of {@linkplain #getCurrent() the current item} with another.
-     * <p>
-     * That is, {@link #extract} the given amount of the current item, and transactionally {@link #insert} the same amount of the given variant instead.
-     * <p>
-     * This method is particularly useful if the contained storage is being mutated and the resulting changes to the item
-     * should be persisted.
-     *
-     * @param newVariant  The variant of the items after the conversion. May not be blank.
-     * @param maxAmount   The maximum amount of items to convert. May not be negative.
-     * @param transaction The transaction this operation is part of.
-     * @return A non-negative integer not greater than maxAmount: the amount that was transformed.
-     */
-    default long exchange(ItemVariant newVariant, long maxAmount, TransactionContext transaction) {
-        StoragePreconditions.notBlankNotNegative(newVariant, maxAmount);
-
-        try (var nested = Transaction.open(transaction)) {
-            long extracted = extract(getCurrent(), maxAmount, nested);
-
-            if (insert(newVariant, extracted, nested) == extracted) {
-                nested.commit();
-                return extracted;
-            }
-        }
-
-        return 0;
-    }
-
-    /**
-     * Retrieve a capability for the contents of this context.
-     */
-    @ApiStatus.NonExtendable
-    @Nullable
-    default <T> T getCapability(ItemCapability<T, InItemStorageContext> capability) {
-        return getCurrent().toStack().getCapability(capability, this);
-    }
-
-    /**
      * Returns a context for interaction with a player's hand. This is recommended for item use interactions.
      *
      * <p>In creative mode, {@link #forCreativeInteraction} is used with the hand stack.
@@ -174,5 +99,80 @@ public interface InItemStorageContext {
     @ApiStatus.NonExtendable
     default InItemStorageContext oneByOne() {
         return new OneByOneStorageContext(this);
+    }
+
+    /**
+     * Retrieve a capability for the contents of this context.
+     */
+    @ApiStatus.NonExtendable
+    @Nullable
+    default <T> T getCapability(ItemCapability<T, InItemStorageContext> capability) {
+        return getCurrent().toStack().getCapability(capability, this);
+    }
+
+    /**
+     * @return The current variant of the item that contains the storage that should be accessed. This may
+     * be blank or become blank if {@link #extract} is used.
+     */
+    ItemVariant getCurrent();
+
+    /**
+     * @return The current amount of {@linkplain #getCurrent() the item} containing the storage. Always returns 0 if
+     * {@linkplain #getCurrent() the current item} is blank.
+     */
+    long getCurrentAmount();
+
+    /**
+     * @return True if any attempts to modify the items in this context will not work. Storage implementations can
+     * use this to return appropriate information from {@link Storage#supportsInsertion()}
+     * or {@link Storage#supportsExtraction()}.
+     */
+    default boolean supportsModification() {
+        return true;
+    }
+
+    /**
+     * Transactionally insert an item into the "slot" representing the location of the item containing the storage.
+     * <p>
+     * If the inserted item is not stackable with the current item, it may be inserted in a place that is inaccessible
+     * for {@link #extract}, such as the player inventory.
+     *
+     * @see Storage#insert
+     */
+    long insert(ItemVariant itemVariant, long maxAmount, TransactionContext transaction);
+
+    /**
+     * Transactionally extract some of the current item.
+     *
+     * @see Storage#extract
+     */
+    long extract(ItemVariant itemVariant, long maxAmount, TransactionContext transaction);
+
+    /**
+     * Try to exchange as many items as possible of {@linkplain #getCurrent() the current item} with another.
+     * <p>
+     * That is, {@link #extract} the given amount of the current item, and transactionally {@link #insert} the same amount of the given variant instead.
+     * <p>
+     * This method is particularly useful if the contained storage is being mutated and the resulting changes to the item
+     * should be persisted.
+     *
+     * @param newVariant  The variant of the items after the conversion. May not be blank.
+     * @param maxAmount   The maximum amount of items to convert. May not be negative.
+     * @param transaction The transaction this operation is part of.
+     * @return A non-negative integer not greater than maxAmount: the amount that was transformed.
+     */
+    default long exchange(ItemVariant newVariant, long maxAmount, TransactionContext transaction) {
+        StoragePreconditions.notBlankNotNegative(newVariant, maxAmount);
+
+        try (var nested = Transaction.open(transaction)) {
+            long extracted = extract(getCurrent(), maxAmount, nested);
+
+            if (insert(newVariant, extracted, nested) == extracted) {
+                nested.commit();
+                return extracted;
+            }
+        }
+
+        return 0;
     }
 }
