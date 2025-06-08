6dd41653b8b6b9591e01c0b02020f690b5252663
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2025-06-08 18:42:32 -0400

    adjusted javadoc wording

--- src/main/java/net/neoforged/neoforge/registries/IRegistryExtension.java
+++ src/main/java/net/neoforged/neoforge/registries/IRegistryExtension.java
@@ -67,7 +67,7 @@ public interface IRegistryExtension<T> {
     /**
      * Adds an alias that maps from <code>alias</code> to <code>registeredName</code>.
      * <p>
-     * Any registry lookups for <code>alias</code> will be redirected to <code>registeredName</code>,
+     * Any registry lookups for <code>alias</code>'s ResourceLocation will be redirected to <code>registeredName</code>'s ResourceLocation,
      * unless an object with <code>alias</code> is present in the registry.
      *
      * @param alias the original name of the registry object
