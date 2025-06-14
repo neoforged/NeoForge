e6eca04287c43dbe9887c5ad67070078893d4699
AnrDaemon <anrdaemon@yandex.ru>
2025-06-14 20:34:37 +0300

    Reduce process number on server start

--- server_files/run.sh
+++ server_files/run.sh
@@ -3,4 +3,4 @@
 # Add custom JVM arguments to the user_jvm_args.txt
 # Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
 #  pass them to this script directly
-java @user_jvm_args.txt @libraries/@MAVEN_PATH@/unix_args.txt "$@"
+exec java @user_jvm_args.txt @libraries/@MAVEN_PATH@/unix_args.txt "$@"
