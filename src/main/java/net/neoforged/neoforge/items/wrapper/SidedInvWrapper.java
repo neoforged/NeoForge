95f96ba441161ec45cc5eb62d607aec9105a32b9
Soaryn <1859805+Soaryn@users.noreply.github.com>
2025-06-15 18:25:27 -0400

    Fixes messed up SidedInvWrapper due to reversion. This is likely to not be the only class.

--- src/main/java/net/neoforged/neoforge/items/wrapper/SidedInvWrapper.java
+++ src/main/java/net/neoforged/neoforge/items/wrapper/SidedInvWrapper.java
@@ -53,6 +53,9 @@ public class SidedInvWrapper implements IItemHandlerModifiable {
     }
 
     public static int getSlot(WorldlyContainer inv, int slot, @Nullable Direction side) {
+        if (side == null) {
+            return slot;
+        }
         int[] slots = inv.getSlotsForFace(side);
         if (slot < slots.length)
             return slots[slot];
@@ -80,6 +83,9 @@ public class SidedInvWrapper implements IItemHandlerModifiable {
 
     @Override
     public int getSlots() {
+        if (side == null) {
+            return inv.getContainerSize();
+        }
         return inv.getSlotsForFace(side).length;
     }
 
@@ -187,7 +193,7 @@ public class SidedInvWrapper implements IItemHandlerModifiable {
         if (stackInSlot.isEmpty())
             return ItemStack.EMPTY;
 
-        if (!inv.canTakeItemThroughFace(slot1, stackInSlot, side))
+        if (side != null && !inv.canTakeItemThroughFace(slot1, stackInSlot, side))
             return ItemStack.EMPTY;
 
         if (simulate) {
