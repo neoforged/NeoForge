ab37b7180dd880d4d8bdb52b355f380f78309271
Soaryn <1859805+Soaryn@users.noreply.github.com>
2025-06-18 11:53:48 -0400

    Removes primer notes from IEnergyHandler

--- src/main/java/net/neoforged/neoforge/transfer/handlers/energy/IEnergyHandler.java
+++ src/main/java/net/neoforged/neoforge/transfer/handlers/energy/IEnergyHandler.java
@@ -5,14 +5,13 @@
 
 package net.neoforged.neoforge.transfer.handlers.energy;
 
+import net.neoforged.neoforge.capabilities.Capabilities;
 import net.neoforged.neoforge.transfer.handlers.ITransactionHandler;
 import net.neoforged.neoforge.transfer.transaction.TransactionContext;
 
 /**
- * Formerly `IEnergyStorage`
- * <p>
  * A capability interface providing the methods such as insert/extract a buffered energy amount for a handler.<br>
- * To use Neo's energy capability see the {@link net.neoforged.neoforge.capabilities.Capabilities.EnergyHandler#BLOCK EnergyCapability} in {@link net.neoforged.neoforge.capabilities.Capabilities Capabilities}.
+ * To use Neo's energy capability see the {@link Capabilities.EnergyHandler#BLOCK EnergyCapability} in {@link Capabilities Capabilities}.
  * <br>
  * To make your own energy system using this interface, you can register a new capability using something like the following.
  *
@@ -25,8 +24,6 @@ import net.neoforged.neoforge.transfer.transaction.TransactionContext;
  */
 public interface IEnergyHandler extends ITransactionHandler {
     /**
-     * <b>PRIMER: New</b> - Required for the indexed methods below to be inquired correctly.
-     *
      * @return The number of indices this handler manages. You shouldn't return 0 where avoidable, but this is allowed in cases there are no buffers such as the {@link net.neoforged.neoforge.transfer.handlers.templates.energy.EmptyEnergyHandler Empty handler}.
      *         <p>
      *         <strong>Must be non-negative</strong>
@@ -34,8 +31,6 @@ public interface IEnergyHandler extends ITransactionHandler {
     int size();
 
     /**
-     * <b>PRIMER: Formerly</b> `getEnergyStored`. Now needs an index.
-     * <p>
      * To know the total amount of energy stored in an {@link IEnergyHandler} consider using {@link net.neoforged.neoforge.transfer.EnergyHandlerUtil#getAmount(IEnergyHandler) EnergyHandlerUtil.getAmount(IEnergyHandler)}
      * <p>
      * It should be expected that {@code getAmount(index) <= getCapacity(index)}.
@@ -59,8 +54,6 @@ public interface IEnergyHandler extends ITransactionHandler {
     }
 
     /**
-     * <b>PRIMER: Formerly</b> `getMaxEnergyStored`. Now has an index similar to IResourceHandler.
-     * <p>
      * Gets the capacity that index can hold.
      * To know the total capacity of energy in an {@link IEnergyHandler} consider using {@link net.neoforged.neoforge.transfer.EnergyHandlerUtil#getCapacity(IEnergyHandler) EnergyHandlerUtil.getCapacity(IEnergyHandler)}
      *
@@ -83,12 +76,7 @@ public interface IEnergyHandler extends ITransactionHandler {
     }
 
     /**
-     * <b>PRIMER: Formerly</b> `canReceive` This however deviates a bit from its use as it is now at ANY point can it be inserted to rather than in that point in time.
-     * The update path for scenarios you'd call canReceive to determine if you'd call receiveEnergy are is now just to call allowsInsertion once on capability invalidation
-     * then assuming it was true insert(simulate) anytime you are wanting to see if you can insert energy. This will be true for the indexed and the allowsExtraction methods.
-     * Changing this requires you to invalidate your capability, so change sparingly.
-     * <p>
-     * <strong>Note:</strong> It is advised to override this and return true or false if you already know the handler will have a possible true result
+     * <strong>Note:</strong> It is advised to override this and return true or false if you already know the handler will have a possible true result.
      *
      * @return True if the handler can be inserted into at this time, false otherwise. If using indices, this should return true if any index allows insertion
      * @see #supportsInsertion(int)
@@ -102,11 +90,6 @@ public interface IEnergyHandler extends ITransactionHandler {
     }
 
     /**
-     * <b>PRIMER: New</b>
-     * <p>
-     * An estimation of or hint if {@link IEnergyHandler#insert} would ever succeed. This should not be used to determine if something is full, nor should it return as such.
-     * A typical use case is identifying which implementations of {@link IEnergyHandler} in a group would be able to be insertable. Changing this requires you to invalidate your capability, so change sparingly.
-     * <p>
      * <b>IMPORTANT:</b> This doesn't add any control, this is merely a guide for things like pipes to know ahead of time if it can be ever inserted into when the capability invalidates for example.
      * You shouldn't call this in your own {@link IEnergyHandler#insert} method, but you still need to handle the result if insert wouldn't fill there.
      *
@@ -117,11 +100,6 @@ public interface IEnergyHandler extends ITransactionHandler {
     boolean supportsInsertion(int index);
 
     /**
-     * <b>PRIMER: Formerly</b> `canExtract`
-     * <p>
-     * An estimation of or hint if {@link IEnergyHandler#extract} would result in any energy. This should not be used to determine if something is empty, nor should it return as such.
-     * A typical use case is identifying which implementations of {@link IEnergyHandler} in a group would be able to be extractable. Changing this requires you to invalidate your capability, so change sparingly.
-     * <p>
      * <strong>Note:</strong> It is advised to override this and return {@code true} if you already know the handler will have a possible true result or {@code false} if none of them will ever be true;
      * rather than having it require a full iteration to look up the results.
      *
@@ -137,11 +115,6 @@ public interface IEnergyHandler extends ITransactionHandler {
     }
 
     /**
-     * <b>PRIMER: New</b>
-     * <p>
-     * An estimation of or hint if {@link IEnergyHandler#extract} would result in any energy. This should <b>not</b> be used to determine if something is empty, nor should it return as such.
-     * A typical use case is identifying which implementations of {@link IEnergyHandler} in a group would be able to be extractable regardless of amount stored. Changing this requires you to invalidate your capability, so change sparingly.
-     * <p>
      * <b>IMPORTANT:</b> This doesn't add any control, this is merely a guide for things like pipes to know ahead of time if it can be ever extracted from when the capability invalidates for example.
      * You shouldn't call this in your own {@link IEnergyHandler#extract} method.
      *
@@ -152,8 +125,6 @@ public interface IEnergyHandler extends ITransactionHandler {
     boolean supportsExtraction(int index);
 
     /**
-     * <b>PRIMER: New</b>
-     * <p>
      * Inserts a given amount of energy into the handler at the target index. If the intent is to just arbitrarily send power to the handler, consider using {@link IEnergyHandler#insert(int, TransactionContext)} instead.
      *
      * @param index       The index to insert into. <strong>Must be non-negative</strong>
@@ -165,8 +136,6 @@ public interface IEnergyHandler extends ITransactionHandler {
     int insert(int index, int amount, TransactionContext transaction);
 
     /**
-     * <b>PRIMER: Formerly</b> `receiveEnergy(int toReceive, bool simulate)`
-     * <p>
      * Inserts a given amount into the handler. Distribution is up to the handler.
      * <p>
      * When implementing, it is advised to not make this call {@link IEnergyHandler#insert(int, int, TransactionContext)} for each index directly,
@@ -180,8 +149,6 @@ public interface IEnergyHandler extends ITransactionHandler {
     int insert(int amount, TransactionContext transaction);
 
     /**
-     * <b>PRIMER: New</b>
-     * <p>
      * Extracts a given amount of energy from the handler at the given index. If the intent is to arbitrarily extract power from the handler, consider using {@link IEnergyHandler#extract(int, TransactionContext)} instead.
      *
      * @param index       The index to extract from. <strong>Must be non-negative</strong>
@@ -193,8 +160,6 @@ public interface IEnergyHandler extends ITransactionHandler {
     int extract(int index, int amount, TransactionContext transaction);
 
     /**
-     * <b>PRIMER: Formerly</b> `extractEnergy(int toReceive, bool simulate)`
-     * <p>
      * Extracts a given amount from the handler. Distribution is up to the handler.
      * <p>
      * When implementing this method, it is advised to not make this call {@link IEnergyHandler#extract(int, int, TransactionContext)} for each index directly,
