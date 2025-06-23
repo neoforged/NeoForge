d4ce1ce2e91696584e7780656bc4e6ff704befc2
Sebastian Hartte <shartte@users.noreply.github.com>
2025-06-23 02:49:12 +0200

    More docs

--- src/client/java/net/neoforged/neoforge/client/gui/PictureInPictureRendererRegistration.java
+++ src/client/java/net/neoforged/neoforge/client/gui/PictureInPictureRendererRegistration.java
@@ -10,5 +10,9 @@ import net.minecraft.client.gui.render.pip.PictureInPictureRenderer;
 import net.minecraft.client.gui.render.state.pip.PictureInPictureRenderState;
 import net.minecraft.client.renderer.MultiBufferSource;
 
+/**
+ * Encapsulates a factory to create {@link PictureInPictureRenderer} and links it to the type of {@link PictureInPictureRenderState}
+ * that the created renderers support.
+ */
 public record PictureInPictureRendererRegistration<T extends PictureInPictureRenderState>(Class<T> stateClass,
         Function<MultiBufferSource.BufferSource, PictureInPictureRenderer<T>> factory) {}
