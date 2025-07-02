6b4f58d5cee964ed5b1cf66bfe73f5100fc8d73e
Luke Bemish <lukebemish@lukebemish.dev>
2025-07-02 18:08:11 -0500

    Fix junit entrypoint

--- src/main/java/net/neoforged/neoforge/junit/JUnitMain.java
+++ src/main/java/net/neoforged/neoforge/junit/JUnitMain.java
@@ -5,8 +5,12 @@
 
 package net.neoforged.neoforge.junit;
 
+import cpw.mods.modlauncher.Launcher;
+import java.lang.invoke.MethodHandles;
+import java.util.Set;
 import net.minecraft.SharedConstants;
 import net.minecraft.server.Bootstrap;
+import net.neoforged.fml.loading.moddiscovery.locators.NeoForgeDevDistCleaner;
 
 public class JUnitMain {
     public static void main(String[] args) {
@@ -15,5 +19,16 @@ public class JUnitMain {
 
         // Load mods
         net.neoforged.neoforge.server.loading.ServerModLoader.load();
+
+        // We launch as a server, but we want client classes available.
+        // So we explicitly clear the list of masked classes.
+        try {
+            var distCleaner = (NeoForgeDevDistCleaner) Launcher.INSTANCE.environment().findLaunchPlugin("neoforgedevdistcleaner").orElseThrow();
+            var maskedClassesGetter = MethodHandles.privateLookupIn(NeoForgeDevDistCleaner.class, MethodHandles.lookup()).findGetter(NeoForgeDevDistCleaner.class, "maskedClasses", Set.class);
+            var maskedClasses = (Set<String>) maskedClassesGetter.invoke(distCleaner);
+            maskedClasses.clear();
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
     }
 }
