2ad30bf45450cd12437ff60df6b31ad85badafcb
Soaryn <1859805+Soaryn@users.noreply.github.com>
2025-07-06 10:29:17 -0400

    Removes injected interface in container as that shouldn't be part of this slice.
    Adds missing files from slice

---
+++ src/main/java/net/neoforged/neoforge/transfer/ResourceHandlerUtil.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) NeoForged and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
+package net.neoforged.neoforge.transfer;
+
+import net.minecraft.ReportedException;
+import net.neoforged.neoforge.transfer.resources.IResource;
+
+public class ResourceHandlerUtil {
+    /**
+     * A utility method to check both resource and amount to validate if the resource would be empty.
+     * <p>
+     * Typically used in handler insert or extract implementations to determine if the operation is valid before proceeding.
+     *
+     * @throws ReportedException When the amount is negative
+     * @see ResourceContainerContentsHandler#insert(int, IResource, int, TransactionContext)
+     */
+    public static <T extends IResource> boolean isEmpty(T resource, int amount) {
+        return TransferPreconditions.checkNonNegative(amount) == 0 || resource.isEmpty();
+    }
+}
