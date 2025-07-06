5b0223da2910c6d0b9866516fbc57eb80d83d994
Soaryn <1859805+Soaryn@users.noreply.github.com>
2025-07-06 10:34:22 -0400

    Adds missing deprecation handling file from slice

---
+++ src/main/java/net/neoforged/neoforge/transfer/ResourceHandlerDeprecationHandling.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) NeoForged and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
+package net.neoforged.neoforge.transfer;
+
+@Deprecated(since = ResourceHandlerDeprecationHandling.MC_1_21_6)
+public class ResourceHandlerDeprecationHandling {
+    public static final String MC_1_21_6 = "1.21.6";
+}
