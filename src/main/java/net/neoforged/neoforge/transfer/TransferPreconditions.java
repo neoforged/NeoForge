0e2d163be0fa43acfbdaceea39351b34ec5ce879
Soaryn <1859805+Soaryn@users.noreply.github.com>
2025-07-06 23:25:00 -0400

    Removes ReportedException from non-negative check. Targets `Objects.checkIndex` as the baseline
    Changes Internal -> Deprecated

--- src/main/java/net/neoforged/neoforge/transfer/TransferPreconditions.java
+++ src/main/java/net/neoforged/neoforge/transfer/TransferPreconditions.java
@@ -5,9 +5,6 @@
 
 package net.neoforged.neoforge.transfer;
 
-import net.minecraft.CrashReport;
-import net.minecraft.ReportedException;
-
 public class TransferPreconditions {
     private TransferPreconditions() {}
 
@@ -15,14 +12,10 @@ public class TransferPreconditions {
      * Ensures the value passed in is non-negative, throws otherwise.
      *
      * @return The value passed.
-     * @throws ReportedException when value is negative.
+     * @throws IllegalArgumentException when value is negative.
      */
     public static int checkNonNegative(int value) {
         if (value >= 0) return value;
-
-        CrashReport report = CrashReport.forThrowable(new IllegalArgumentException("Value must be non-negative"), "Value was negative");
-        report.addCategory("Non-negative")
-                .setDetail("Value", value);
-        throw new ReportedException(report);
+        throw new IllegalArgumentException("Non-negative check failed: " + value);
     }
 }
--- src/main/java/net/neoforged/neoforge/transfer/resources/UnsafeResourceUtils.java
+++ src/main/java/net/neoforged/neoforge/transfer/resources/UnsafeResourceUtils.java
@@ -7,20 +7,20 @@ package net.neoforged.neoforge.transfer.resources;
 
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.fluids.FluidStack;
-import org.jetbrains.annotations.ApiStatus;
 
 /**
  * <strong>Avoid use of this utility.</strong> This is only intended to be used internally by Neo or in read only
  * scenarios to avoid allocation where you can ensure no data is written back to the stack.
  */
-@ApiStatus.Internal
+//Deprecated annotations are java's standard as well for "unsafe" uses
+@Deprecated
 public final class UnsafeResourceUtils {
     /**
      * <strong>Avoid use of this method.</strong>
      * <p>
      * You shouldn't be calling this yourself. This is intended for <b>readonly</b> use in the ResourceHandler wrapper implementations.
      */
-    @ApiStatus.Internal
+    @Deprecated
     public static ItemStack innerStackOf(ItemResource resource) {
         return resource.innerStack;
     }
@@ -30,7 +30,7 @@ public final class UnsafeResourceUtils {
      * <p>
      * You shouldn't be calling this yourself. This is intended for <b>readonly</b> use in the ResourceHandler wrapper implementations.
      */
-    @ApiStatus.Internal
+    @Deprecated
     public static FluidStack innerStackOf(FluidResource resource) {
         return resource.innerStack;
     }
