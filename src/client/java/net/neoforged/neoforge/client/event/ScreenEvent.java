028d53e3fa72d1cad342988b4dc2ed829b403d31
James Mitchell <tehgeek@gmail.com>
2025-07-08 01:06:11 -1000

    Update src/client/java/net/neoforged/neoforge/client/event/ScreenEvent.java
    Co-authored-by: Dennis C <xfacthd@gmx.de>

--- src/client/java/net/neoforged/neoforge/client/event/ScreenEvent.java
+++ src/client/java/net/neoforged/neoforge/client/event/ScreenEvent.java
@@ -217,7 +217,7 @@ public abstract class ScreenEvent extends Event {
          *
          * <p>This event is not {@linkplain ICancellableEvent cancellable}, and does not {@linkplain HasResult have a result}.</p>
          *
-         * <p>This event is fired on the {@linkplain NeoForge#EVENT_BUS main Forge event bus},
+         * <p>This event is fired on the {@linkplain NeoForge#EVENT_BUS game event bus},
          * only on the {@linkplain LogicalSide#CLIENT logical client}.</p>
          */
         public static class Background extends ScreenEvent.Render {
