37a4e372135876f703604f1cbb87f62200be4223
Soaryn <1859805+Soaryn@users.noreply.github.com>
2025-07-09 11:25:03 -0400

    Documents IRegisteredResource more.

--- src/main/java/net/neoforged/neoforge/transfer/resources/IRegisteredResource.java
+++ src/main/java/net/neoforged/neoforge/transfer/resources/IRegisteredResource.java
@@ -10,17 +10,16 @@ import net.minecraft.core.Holder;
 import net.minecraft.core.HolderSet;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
+import org.jetbrains.annotations.ApiStatus;
 
 /**
- * A helper version of {@link IResource} intended for resources registered to some registry bound by some backing instance.
- * These resources are expected to be bound by some holder and registry, such as {@link Item Item} is for {@link ItemResource}
+ * A helper version of {@link IResource} intended for resources registered to some registry bound by some holder, such as {@link Item Item} is for {@link ItemResource}
  *
  * @param <T> The type of the backing instance.
  * @see ItemResource
  * @see FluidResource
  */
 public interface IRegisteredResource<T, R extends IResource<R>> extends IResource<R> {
-    //TODO provide documentation on all methods
     /**
      * @return The backing instance of the resource.
      * @see ItemResource#getInstanceValue() returns an Item
@@ -29,30 +28,54 @@ public interface IRegisteredResource<T, R extends IResource<R>> extends IResourc
     T getInstanceValue();
 
     /**
-     * A helper override that allows the {@code is} methods in {@link IRegisteredResource} to not need to manually be overridden.
+     * {@return The registered holder of the backing resource}
      * 
-     * @return The registered holder of the backing resource.
      * @see ItemResource#getHolder()
      * @see FluidResource#getHolder()
      */
     Holder<T> getHolder();
 
+    /**
+     * @param tag Tag to check
+     * @return {@code true} if the holder from {@link #getHolder()} has the specified tag applied
+     */
+    @ApiStatus.NonExtendable
     default boolean is(TagKey<T> tag) {
         return getHolder().is(tag);
     }
 
+    /**
+     * @param instance the instance to compare
+     * @return {@code true} if an exact equality comparison ('==') between the instance provided and the value from {@link #getInstanceValue()} is {@code true}
+     */
+    @ApiStatus.NonExtendable
     default boolean is(T instance) {
         return instance == getInstanceValue();
     }
 
+    /**
+     * @param predicate The predicate to perform the test.
+     * @return {@code true} if the predicate's test returns {@code true} for the holder from {@link #getHolder()}.
+     */
+    @ApiStatus.NonExtendable
     default boolean is(Predicate<Holder<T>> predicate) {
         return predicate.test(getHolder());
     }
 
+    /**
+     * @param holder the holder to check
+     * @return {@code true} if the holder's value is exactly equal ('==') to the instance value from {@link #getInstanceValue()}
+     */
+    @ApiStatus.NonExtendable
     default boolean is(Holder<T> holder) {
         return is(holder.value());
     }
 
+    /**
+     * @param holders Set of holders to check
+     * @return {@code true} if the holder set contains the holder provided from {@link #getHolder()}
+     */
+    @ApiStatus.NonExtendable
     default boolean is(HolderSet<T> holders) {
         return holders.contains(getHolder());
     }
