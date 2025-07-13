605b3053445b3015dddf4075db4d0fe8c40a9c43
RogueLogix <rogue@logix.dev>
2025-07-13 12:05:40 -0700

    Use config value to enable B3D validation layer.

--- src/client/java/net/neoforged/neoforge/client/ClientHooks.java
+++ src/client/java/net/neoforged/neoforge/client/ClientHooks.java
@@ -140,7 +140,6 @@ import net.neoforged.bus.api.Event;
 import net.neoforged.fml.ModLoader;
 import net.neoforged.fml.earlydisplay.DisplayWindow;
 import net.neoforged.fml.loading.EarlyLoadingScreenController;
-import net.neoforged.fml.loading.FMLLoader;
 import net.neoforged.neoforge.client.blaze3d.validation.ValidationGpuDevice;
 import net.neoforged.neoforge.client.config.NeoForgeClientConfig;
 import net.neoforged.neoforge.client.entity.animation.json.AnimationTypeManager;
@@ -1137,9 +1136,9 @@ public class ClientHooks {
 
     public static GpuDevice createGpuDevice(long window, int debugLevel, boolean syncDebug, BiFunction<ResourceLocation, ShaderType, String> defaultShaderSource, boolean enableDebugLabels) {
         final var glDevice = new GlDevice(window, debugLevel, syncDebug, defaultShaderSource, enableDebugLabels);
-        if (FMLLoader.isProduction() || Boolean.getBoolean("neoforge.disableB3DValidation")) {
-            return glDevice;
+        if (NeoForgeClientConfig.INSTANCE.enableB3DValidationLayer.getAsBoolean()) {
+            return new ValidationGpuDevice(glDevice, true);
         }
-        return new ValidationGpuDevice(glDevice, true);
+        return glDevice;
     }
 }
