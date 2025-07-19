89d83277e87ef70ddbc614424c475fd602b45240
Kinsteen <me@kinsteen.fr>
2025-07-19 12:37:13 +0200

    doc: add some documentation to LivingFreezeEvent

--- src/main/java/net/neoforged/neoforge/event/entity/living/LivingFreezeEvent.java
+++ src/main/java/net/neoforged/neoforge/event/entity/living/LivingFreezeEvent.java
@@ -5,9 +5,21 @@
 
 package net.neoforged.neoforge.event.entity.living;
 
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.LivingEntity;
 import net.neoforged.bus.api.ICancellableEvent;
+import net.neoforged.neoforge.common.CommonHooks;
+import net.neoforged.neoforge.common.NeoForge;
 
+/**
+ * LivingFreezeEvent is fired whenever a living entity ticks.<br>
+ * <br>
+ * This event is fired via {@link CommonHooks#onLivingFreeze(LivingEntity, ServerLevel)}.<br>
+ * <br>
+ * This event is {@link ICancellableEvent}.<br>
+ * <br>
+ * This event is fired on {@link NeoForge#EVENT_BUS}, on the logical server only.
+ */
 public class LivingFreezeEvent extends LivingEvent {
     private boolean isFreezing;
     private int ticksRequiredToFreeze;
@@ -24,14 +36,29 @@ public class LivingFreezeEvent extends LivingEvent {
         this.damageTickRate = 40;
     }
 
+    /**
+     * If the entity is freezing, its freezing counter will be increased. If it's over the
+     * {@link #getTicksRequiredToFreeze()} threshold, the entity will take damage.<br>
+     * If the entity is not freezing, its freezing counter will be decreased.
+     *
+     * @return True if the entity is freezing
+     */
     public boolean isFreezing() {
         return isFreezing;
     }
 
+    /**
+     * Sets if the entity is freezing or not.
+     *
+     * @param freezing The new value.
+     */
     public void setFreezing(boolean freezing) {
         isFreezing = freezing;
     }
 
+    /**
+     * @return The number of ticks needed to fully freeze (start applying damage) while freezing.
+     */
     public int getTicksRequiredToFreeze() {
         return ticksRequiredToFreeze;
     }
