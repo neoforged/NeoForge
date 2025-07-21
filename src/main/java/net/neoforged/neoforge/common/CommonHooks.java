8e80dca1e01efb6b04c4ffb6912725549575ef8d
TheSilkMiner <thesilkminer@outlook.com>
2025-07-21 14:42:18 +0200

    Add unit tests to verify EDA behavior
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- src/main/java/net/neoforged/neoforge/common/CommonHooks.java
+++ src/main/java/net/neoforged/neoforge/common/CommonHooks.java
@@ -28,7 +28,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.EnumMap;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
