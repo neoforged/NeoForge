31da29063b1d3670de1d06c5575802c62bf4ff03
Technici4n <13494793+Technici4n@users.noreply.github.com>
2025-08-14 22:49:56 +0200

    Remove ISingleResourceHandler

--- src/main/java/net/neoforged/neoforge/transfer/handlers/templates/resources/VoidResourceHandler.java
+++ src/main/java/net/neoforged/neoforge/transfer/handlers/templates/resources/VoidResourceHandler.java
@@ -6,20 +6,21 @@
 package net.neoforged.neoforge.transfer.handlers.templates.resources;
 
 import java.util.Objects;
-import net.neoforged.neoforge.transfer.handlers.resources.ISingleResourceHandler;
+
+import net.neoforged.neoforge.transfer.handlers.resources.IResourceHandler;
 import net.neoforged.neoforge.transfer.resources.FluidResource;
 import net.neoforged.neoforge.transfer.resources.IResource;
 import net.neoforged.neoforge.transfer.resources.ItemResource;
 import net.neoforged.neoforge.transfer.transaction.TransactionContext;
 
 /**
- * An {@link ISingleResourceHandler} that automatically destroys any resources that are inserted into it.
+ * A resource handler that automatically destroys any resources that are inserted into it.
  * You should use the static instances provided such as {@link #ITEM} or {@link #FLUID} instead of creating one yourself.
  * In the case of your own resource type, or one not specifically builtin, it is advised to create your own cached instance for reuse.
  *
  * @param <T> The type of resource that this storage can accept.
  */
-public final class VoidResourceHandler<T extends IResource> implements ISingleResourceHandler<T> {
+public final class VoidResourceHandler<T extends IResource> implements IResourceHandler<T> {
     public static final VoidResourceHandler<ItemResource> ITEM = new VoidResourceHandler<>(ItemResource.EMPTY);
     public static final VoidResourceHandler<FluidResource> FLUID = new VoidResourceHandler<>(FluidResource.EMPTY);
 
@@ -30,8 +31,15 @@ public final class VoidResourceHandler<T extends IResource> implements ISingleRe
     }
 
     @Override
-    public int insert(T resource, int amount, TransactionContext context) {
-        return amount; // ignore the inputs, and inform the return is always accepted
+    public int size() {
+        return 1;
+    }
+
+    @Override
+    public int insert(int index, T resource, int amount, TransactionContext context) {
+        Objects.checkIndex(index, size());
+        // Always accept the full amount
+        return amount;
     }
 
     @Override
@@ -40,14 +48,15 @@ public final class VoidResourceHandler<T extends IResource> implements ISingleRe
     }
 
     @Override
-    public int extract(T resource, int amount, TransactionContext context) {
-        return 0; // Nothing is ever allowed to be extracted
+    public int extract(int index, T resource, int amount, TransactionContext context) {
+        Objects.checkIndex(index, size());
+        return 0;
     }
 
     @Override
     public T getResource(int index) {
         Objects.checkIndex(index, size());
-        return emptyResource; // The resource type's "None"
+        return emptyResource;
     }
 
     @Override
@@ -59,7 +68,7 @@ public final class VoidResourceHandler<T extends IResource> implements ISingleRe
     @Override
     public int getCapacity(int index, T resource) {
         Objects.checkIndex(index, size());
-        return Integer.MAX_VALUE; // Maximum capacity
+        return Integer.MAX_VALUE;
     }
 
     @Override
@@ -71,6 +80,6 @@ public final class VoidResourceHandler<T extends IResource> implements ISingleRe
     @Override
     public boolean isValid(int index, T resource) {
         Objects.checkIndex(index, size());
-        return true; // What ever resource is queried is always allowed
+        return true;
     }
 }
