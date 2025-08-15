9f86baf193b5516a2bfb52f9f74403909c5fd980
Technici4n <13494793+Technici4n@users.noreply.github.com>
2025-08-15 10:43:41 +0200

    Javadoc pass on IResourceHandler

--- src/main/java/net/neoforged/neoforge/transfer/handlers/resources/IResourceHandler.java
+++ src/main/java/net/neoforged/neoforge/transfer/handlers/resources/IResourceHandler.java
@@ -8,41 +8,56 @@ package net.neoforged.neoforge.transfer.handlers.resources;
 import net.neoforged.neoforge.transfer.ResourceHandlerUtil;
 import net.neoforged.neoforge.transfer.resources.IResource;
 import net.neoforged.neoforge.transfer.transaction.SnapshotJournal;
+import net.neoforged.neoforge.transfer.transaction.Transaction;
 import net.neoforged.neoforge.transfer.transaction.TransactionContext;
 
 /**
- * A generic handler for handling a {@link IResource resource} of type {@link T} whether it be inserting, extracting, querying some size value, etc.
- * Trying to interact with a handler with an index larger than the size is expected to often times throw an {@link IndexOutOfBoundsException} or similar error. Ensure you are within the size constraints of the handler before calling a method.
+ * A generic handler for the transfer and storage of {@link IResource resources} whether it be inserting, extracting, querying some value, etc.
+ *
+ * <h2>Indices</h2>
+ * <p>A resource handler is organized into indices, which are addressed using an int between {@code 0} and {@code size() - 1}.
+ * <p>Out-of-bounds access using methods that accept an {@code int slot} will usually throw an exception,
+ * so only indices between 0 (included) and the size (excluded) should be used.
+ * If a storage has a dynamic size, it should be lenient to accommodate for callers
+ * holding onto a previously returned size.
  *
  * @param <T> The type of resource this handler manages.
  */
 public interface IResourceHandler<T extends IResource> {
     /**
-     * An index is synonymous with "slot", "tank", "buffer", etc.
+     * {@return the <i>current</i> number of indices in this resource handler}
      *
-     * @return The number of indices this handler manages. <strong>Must be non-negative</strong>
+     * <p>An index is synonymous with "slot", "tank", "buffer", etc.
+     *
+     * <p>Note that the size of a resource handler can change.
      */
     int size();
 
     /**
-     * @param index The index to get the resource from. <strong>Must be non-negative</strong>
-     * @return The resource at the given index.
-     * @throws IndexOutOfBoundsException when passing an invalid index. Negative indices are always invalid.
+     * {@return the resource at the given index}
+     *
+     * @param index The index to get the resource from.
      */
     T getResource(int index);
 
     /**
-     * @param index The index to get the amount from. <strong>Must be non-negative</strong>
-     * @return The amount of the resource at the given index. <strong>Must be non-negative</strong> and should never surpass capacity
-     * @throws IndexOutOfBoundsException when passing an invalid index. Negative indices are always invalid.
+     * {@return the amount of the resource at the given index}.
+     *
+     * <p>The returned amount should be <strong>non-negative</strong>, and should never surpass the {@link #getCapacity capacity} of the same index.
+     *
+     * @param index The index to get the amount from.
      * @see #getAmountAsLong(int)
      */
     int getAmount(int index);
 
     /**
-     * @param index The index to get the amount from. <strong>Must be non-negative</strong>
-     * @return The amount as a long of the resource at the given index. <strong>Must be non-negative</strong> and must never surpass capacity
-     * @throws IndexOutOfBoundsException when passing an invalid index. Negative indices are always invalid.
+     * {@return the amount of the resource at the given index, as a long}.
+     *
+     * <p>This method is optional, and allows for larger amounts than {@link #getAmount(int) the int-returning overload} can handle.
+     *
+     * <p>The returned amount should be <strong>non-negative</strong>, and should never surpass the {@link #getCapacityAsLong capacity} of the same index.
+     *
+     * @param index The index to get the amount from.
      * @see #getAmount(int)
      */
     default long getAmountAsLong(int index) {
@@ -50,35 +65,34 @@ public interface IResourceHandler<T extends IResource> {
     }
 
     /**
-     * Gets the maximum capacity that the given index can handle of the given resource.
-     * If an empty resource (an {@link IResource} that returns {@code true} on {@link IResource#isEmpty()}) is provided,
-     * then the theoretical maximum should be returned, regardless of the return of {@link #getResource}.
+     * {@return the capacity of the handler at the given index and for the given resource,
+     * irrespective of the current amount or resource currently at that index}
      * <p>
      * While passing in resources that would return {@code false} on {@link #isValid(int, IResource)}, it should be expected to always return 0.
      * <p>
-     * If the resource returned from {@link #getResource(int)} with the same index does not match and is not empty, it is expected the capacity would return 0.
+     * This function serves as metadata only, and its result might be approximate.
+     * The only way to know if a handler will accept a resource, is to try to {@link #insert insert} it.
      *
-     * @param index    The index to get the limit from. <strong>Must be non-negative</strong>
-     * @param resource The resource to get the limit for. If empty, this should return the theoretical limit of that index
-     * @return The limit of the resource at the given index. <strong>Must be non-negative</strong>
-     * @throws IndexOutOfBoundsException when passing an invalid index. Negative indices are always invalid.
+     * @param index    The index to get the limit for.
+     * @param resource The resource to get the limit for. May be empty to return a generic limit.
      * @see #getCapacityAsLong(int, IResource)
      */
+    // TODO: remark that the amount should be larger than the capacity?
     int getCapacity(int index, T resource);
 
     /**
-     * Gets the maximum capacity that the given index can handle as a long of the given resource.
-     * If an empty resource (an {@link IResource} that returns {@code true} on {@link IResource#isEmpty()}) is provided,
-     * then the theoretical maximum should be returned, regardless of the return of {@link #getResource}.
+     * {@return the capacity of the handler at the given index and for the given resource,
+     * irrespective of the current amount or resource currently at that index, as a long}
+     * <p>
+     * This method is optional, and allows for larger amounts than {@link #getCapacity(int, IResource) the int-returning overload} can handle.
      * <p>
      * While passing in resources that would return {@code false} on {@link #isValid(int, IResource)}, it should be expected to always return 0.
      * <p>
-     * If the resource returned from {@link #getResource(int)} with the same index does not match, it is expected the capacity would return 0.
+     * This function serves as metadata only, and its result might be approximate.
+     * The only way to know if a handler will accept a resource, is to try to {@link #insert insert} it.
      *
-     * @param index    The index to get the limit from. <strong>Must be non-negative</strong>
-     * @param resource The resource to get the limit for. If empty, this should return the theoretical limit of that index
-     * @return The limit of the resource at the given index. <strong>Must be non-negative</strong>
-     * @throws IndexOutOfBoundsException when passing an invalid index. Negative indices are always invalid.
+     * @param index    The index to get the limit for.
+     * @param resource The resource to get the limit for. May be empty to return a generic limit.
      * @see #getCapacity(int, IResource)
      */
     default long getCapacityAsLong(int index, T resource) {
@@ -86,48 +100,48 @@ public interface IResourceHandler<T extends IResource> {
     }
 
     /**
-     * Checks if the given resource is allowed to be inserted into the handler at the given index.
-     * This is typically called in the {@link #insert(int, IResource, int, TransactionContext Context)}
-     * implementations or general resource querying. However, this is separate from if the resource could
-     * currently fit in the handler. This is expected to be true, even if the handler would be full.
-     * Empty resources should always return {@code true}.
+     * {@return whether the given resource is generally allowed to be contained in the handler at the given index,
+     * irrespective of the current amount or resource currently at that index}
+     * <p>
+     * This function serves as metadata only, and its result might be approximate.
+     * The only way to know if a handler will accept a resource, is to try to {@link #insert insert} it.
      *
-     * @param index    The index to check. <strong>Must be non-negative</strong>
-     * @param resource The resource to check.
-     * @return True if the resource can be inserted, false otherwise.
-     * @throws IndexOutOfBoundsException when passing an invalid index.
-     *                                   Negative indices are always invalid.
+     * @param index    The index to check.
+     * @param resource The resource to check. Must not be empty, or an exception will typically be thrown.
      */
     boolean isValid(int index, T resource);
 
     /**
-     * Inserts a given amount of the resource into the handler at the given index.
+     * Tries to insert up to some amount of a resource into the handler at the given index.
      *
-     * @param index       The index to insert the resource into. <strong>Must be non-negative</strong>
-     * @param resource    The resource to insert.
-     * @param amount      The amount of the resource to insert. <strong>Must be non-negative</strong>
-     * @param transaction The {@link TransactionContext} transaction to be inserting with.
-     *                    It is expected that the handler properly supports rollbacks/reversions with a {@link SnapshotJournal}
-     * @return The amount of the resource that was inserted. <strong>Must be non-negative.</strong>
-     * @throws IndexOutOfBoundsException when passing an invalid index. Negative indices are always invalid.
-     * @throws IllegalArgumentException  if the amount is negative.
+     * <p>Changes to the handler are made in the context of a {@link Transaction},
+     * and are expected to properly support rollbacks/reversions, see also {@link SnapshotJournal}.
+     *
+     * @param index       The index to insert the resource into.
+     * @param resource    The resource to insert. <strong>Must be non-empty.</strong>
+     * @param amount      The maximum amount of the resource to insert. <strong>Must be non-negative.</strong>
+     * @param transaction The transaction that this operation is part of.
+     * @return A non-negative integer not greater than {@code amount}: the amount that was inserted.
      * @see #insert(IResource, int, TransactionContext) Inserting into any index in the handler
      */
     int insert(int index, T resource, int amount, TransactionContext transaction);
 
     /**
-     * Inserts a given amount of the resource into the handler. Distribution of the resource is up to the handler.
-     * <p>
-     * Implementation advice, there are some performance gains that can be achieved by handling the indexable and non-indexed versions of this call more carefully.
-     * See {@link StackListHandler#insert(IResource, int, TransactionContext) StackListHandler.insertBehaviour} for an example.
+     * Tries to insert up to some amount of a resource into the handler.
      *
-     * @param resource    The resource to insert. <strong>Must be non-negative</strong>
-     * @param amount      The amount of the resource to insert. <strong>Must be non-negative</strong>
-     * @param transaction The {@link TransactionContext } transaction to be inserting with.
-     *                    It is expected that the handler properly supports rollbacks/reversions with a {@link SnapshotJournal}
-     * @return The amount (Must be non-negative) of the resource that was inserted.
-     * @throws IllegalArgumentException if the amount is negative.
-     * @see #insert(int, IResource, int, TransactionContext) Inserting by index
+     * <p>This function is preferred to the {@link #insert(int, IResource, int, TransactionContext) index-specific overload}
+     * since it lets the handler decide how to distribute the resource.
+     * It can also be a lot more efficient than calling the index-specific overload in a loop,
+     * as resource handlers with a special internal structure can optimize it.
+     *
+     * <p>Changes to the handler are made in the context of a {@link Transaction},
+     * and are expected to properly support rollbacks/reversions, see also {@link SnapshotJournal}.
+     *
+     * @param resource    The resource to insert. <strong>Must be non-empty.</strong>
+     * @param amount      The maximum amount of the resource to insert. <strong>Must be non-negative.</strong>
+     * @param transaction The transaction that this operation is part of.
+     * @return A non-negative integer not greater than {@code amount}: the amount that was inserted.
+     * @see #insert(int, IResource, int, TransactionContext) Inserting into a specific index of the handler
      */
     default int insert(T resource, int amount, TransactionContext transaction) {
         if (ResourceHandlerUtil.isEmpty(resource, amount)) return 0;
@@ -141,33 +155,36 @@ public interface IResourceHandler<T extends IResource> {
     }
 
     /**
-     * Extracts a given amount of the resource from the handler at the given index.
+     * Tries to extract up to some amount of a resource from the handler at the given index.
      *
-     * @param index       The index to extract the resource from. <strong>Must be non-negative</strong>
-     * @param resource    The resource to extract.
-     * @param amount      The amount of the resource to extract. <strong>Must be non-negative</strong>
-     * @param transaction The {@link TransactionContext} transaction to be extracting with.
-     *                    It is expected that the handler properly supports rollbacks/reversions with a {@link SnapshotJournal}
-     * @return The amount (Must be non-negative) of the resource that was extracted.
-     * @throws IndexOutOfBoundsException when passing an invalid index. Negative indices are always invalid.
-     * @throws IllegalArgumentException  if the amount is negative.
+     * <p>Changes to the handler are made in the context of a {@link Transaction},
+     * and are expected to properly support rollbacks/reversions, see also {@link SnapshotJournal}.
+     *
+     * @param index       The index to extract the resource from.
+     * @param resource    The resource to extract. <strong>Must be non-empty.</strong>
+     * @param amount      The maximum amount of the resource to extract. <strong>Must be non-negative.</strong>
+     * @param transaction The transaction that this operation is part of.
+     * @return A non-negative integer not greater than {@code amount}: the amount that was extracted.
      * @see #extract(IResource, int, TransactionContext) Extracting from any index in the handler
      */
     int extract(int index, T resource, int amount, TransactionContext transaction);
 
     /**
-     * Extracts a given amount of the resource from the handler. Distribution of the resource is up to the handler.
-     * <p>
-     * Implementation advice, there are some performance gains that can be achieved by handling the indexable and non-indexed versions of this call more carefully.
-     * See {@link StackListHandler#extract(IResource, int, TransactionContext) StackListHandler.extractBehaviour} for an example.
+     * Tries to extract up to some amount of a resource from the handler.
      *
-     * @param resource    The resource to extract.
-     * @param amount      The amount of the resource to extract. <strong>Must be non-negative</strong>
-     * @param transaction The {@link TransactionContext } transaction to be extracting with.
-     *                    It is expected that the handler properly supports rollbacks/reversions with a {@link SnapshotJournal}
-     * @return The amount (Must be non-negative) of the resource that was extracted.
-     * @throws IllegalArgumentException if the amount is negative.
-     * @see #extract(int, IResource, int, TransactionContext) Extracting by index
+     * <p>This function is preferred to the {@link #extract(int, IResource, int, TransactionContext) index-specific overload}
+     * since it lets the handler decide how to select the resource.
+     * It can also be a lot more efficient than calling the index-specific overload in a loop,
+     * as resource handlers with a special internal structure can optimize it.
+     *
+     * <p>Changes to the handler are made in the context of a {@link Transaction},
+     * and are expected to properly support rollbacks/reversions, see also {@link SnapshotJournal}.
+     *
+     * @param resource    The resource to extract. <strong>Must be non-empty.</strong>
+     * @param amount      The maximum amount of the resource to extract. <strong>Must be non-negative.</strong>
+     * @param transaction The transaction that this operation is part of.
+     * @return A non-negative integer not greater than {@code amount}: the amount that was extracted.
+     * @see #extract(int, IResource, int, TransactionContext) Extracting from a specific index of the handler
      */
     default int extract(T resource, int amount, TransactionContext transaction) {
         if (ResourceHandlerUtil.isEmpty(resource, amount)) return 0;
@@ -181,17 +198,10 @@ public interface IResourceHandler<T extends IResource> {
     }
 
     /**
-     * <p>
-     * Example:
-     *
-     * <pre>{@code
-     * public static final BlockCapability<IResourceHandler<FluidResource>, @Nullable Direction> BLOCK = BlockCapability.createSided(create("fluid_handler"), IResourceHandler.asClass());
-     * }</pre>
-     *
-     * @return a class type ready to be used by something like the capability token registry.
+     * Creates a class with the right generic type, such that it can be used to register a capability.
      */
+    @SuppressWarnings("unchecked")
     static <T extends IResource> Class<IResourceHandler<T>> asClass() {
-        //noinspection unchecked
         return (Class<IResourceHandler<T>>) (Object) IResourceHandler.class;
     }
 }
