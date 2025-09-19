25d3c37e9f3d3583c89f80832dad90dd3f1e7380
Minecraftschurli <minecraftschurli@gmail.com>
2025-09-19 14:00:58 +0200

    Implement some query methods for neoforge

---
+++ src/main/java/net/neoforged/neoforge/server/jsonrpc/ModRecord.java
@@ -0,0 +1,14 @@
+package net.neoforged.neoforge.server.jsonrpc;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
+record ModRecord(String modId, String version, String displayName, String description) {
+    public static final Codec<ModRecord> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+                    Codec.STRING.fieldOf("modId").forGetter(ModRecord::modId),
+                    Codec.STRING.fieldOf("version").forGetter(ModRecord::version),
+                    Codec.STRING.fieldOf("displayName").forGetter(ModRecord::displayName),
+                    Codec.STRING.fieldOf("description").forGetter(ModRecord::description)
+            ).apply(instance, ModRecord::new)
+    );
+}
---
+++ src/main/java/net/neoforged/neoforge/server/jsonrpc/NeoForgeSchemas.java
@@ -0,0 +1,22 @@
+package net.neoforged.neoforge.server.jsonrpc;
+
+import net.minecraft.server.jsonrpc.api.ReferenceUtil;
+import net.minecraft.server.jsonrpc.api.Schema;
+import net.minecraft.server.jsonrpc.api.SchemaComponent;
+
+public final class NeoForgeSchemas {
+    private NeoForgeSchemas() {}
+
+    public static final SchemaComponent MOD_SCHEMA = registerSchema("mod", Schema.record()
+            .withField("modId", Schema.STRING_SCHEMA.flatten())
+            .withField("version", Schema.STRING_SCHEMA.flatten())
+            .withField("displayName", Schema.STRING_SCHEMA.flatten())
+            .withField("description", Schema.STRING_SCHEMA.flatten())
+    );
+
+    private static SchemaComponent registerSchema(String name, Schema schema) {
+        SchemaComponent schemacomponent = new SchemaComponent("neoforge:" + name, ReferenceUtil.createLocalReference(name), schema);
+        Schema.getSchemaRegistry().add(schemacomponent);
+        return schemacomponent;
+    }
+}
