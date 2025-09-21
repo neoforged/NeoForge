1fbdd7c25638b6805a8f6614e2ddb76567d98059
Technici4n <13494793+Technici4n@users.noreply.github.com>
2025-09-21 22:07:37 +0200

    Align ItemAccessFluidHandler with item equivalent (no supplier and field name)

--- src/main/java/net/neoforged/neoforge/transfer/handlers/templates/fluids/ItemAccessFluidHandler.java
+++ src/main/java/net/neoforged/neoforge/transfer/handlers/templates/fluids/ItemAccessFluidHandler.java
@@ -5,7 +5,6 @@
 
 package net.neoforged.neoforge.transfer.handlers.templates.fluids;
 
-import java.util.function.Supplier;
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.world.item.Item;
 import net.neoforged.neoforge.fluids.SimpleFluidContent;
@@ -23,21 +22,21 @@ import net.neoforged.neoforge.transfer.resources.ItemResource;
  */
 public class ItemAccessFluidHandler extends ItemAccessResourceHandler<FluidResource> {
     protected final Item validItem;
-    protected final Supplier<DataComponentType<SimpleFluidContent>> componentType;
+    protected final DataComponentType<SimpleFluidContent> component;
     protected int capacity;
 
-    protected ItemAccessFluidHandler(ItemAccess itemAccess, Supplier<DataComponentType<SimpleFluidContent>> componentType, int capacity) {
+    protected ItemAccessFluidHandler(ItemAccess itemAccess, DataComponentType<SimpleFluidContent> component, int capacity) {
         super(itemAccess, 1);
         // Store the current item, such that if the item changes later we don't return any stored content from it.
         this.validItem = itemAccess.getResource().getItem();
-        this.componentType = componentType;
+        this.component = component;
         this.capacity = capacity;
     }
 
     @Override
     protected FluidResource getResourceFrom(ItemResource accessResource, int index) {
         if (accessResource.is(validItem)) {
-            return FluidResource.of(accessResource.getOrDefault(componentType, SimpleFluidContent.EMPTY).copy());
+            return FluidResource.of(accessResource.getOrDefault(component, SimpleFluidContent.EMPTY).copy());
         } else {
             return FluidResource.EMPTY;
         }
@@ -46,7 +45,7 @@ public class ItemAccessFluidHandler extends ItemAccessResourceHandler<FluidResou
     @Override
     protected int getAmountFrom(ItemResource accessResource, int index) {
         if (accessResource.is(validItem)) {
-            return accessResource.getOrDefault(componentType, SimpleFluidContent.EMPTY).getAmount();
+            return accessResource.getOrDefault(component, SimpleFluidContent.EMPTY).getAmount();
         } else {
             return 0;
         }
@@ -54,7 +53,7 @@ public class ItemAccessFluidHandler extends ItemAccessResourceHandler<FluidResou
 
     @Override
     protected ItemResource update(ItemResource accessResource, int index, FluidResource newResource, int newAmount) {
-        return accessResource.with(componentType, SimpleFluidContent.copyOf(newResource.toStack(newAmount)));
+        return accessResource.with(component, SimpleFluidContent.copyOf(newResource.toStack(newAmount)));
     }
 
     @Override
