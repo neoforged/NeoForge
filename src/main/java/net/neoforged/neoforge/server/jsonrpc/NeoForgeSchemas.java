d1cc288ec4ddeda45f37c2041fe522278573ce56
Minecraftschurli <minecraftschurli@gmail.com>
2025-09-23 19:07:16 +0200

    Simplify api and add a threadsafe register method to Schema

--- src/main/java/net/neoforged/neoforge/server/jsonrpc/NeoForgeSchemas.java
+++ src/main/java/net/neoforged/neoforge/server/jsonrpc/NeoForgeSchemas.java
@@ -5,22 +5,17 @@
 
 package net.neoforged.neoforge.server.jsonrpc;
 
-import net.minecraft.server.jsonrpc.api.ReferenceUtil;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.jsonrpc.api.Schema;
 import net.minecraft.server.jsonrpc.api.SchemaComponent;
+import net.neoforged.neoforge.internal.versions.neoforge.NeoForgeVersion;
 
 public final class NeoForgeSchemas {
     private NeoForgeSchemas() {}
 
-    public static final SchemaComponent MOD_SCHEMA = registerSchema("mod", Schema.record()
-            .withField("modId", Schema.STRING_SCHEMA.flatten())
-            .withField("version", Schema.STRING_SCHEMA.flatten())
-            .withField("displayName", Schema.STRING_SCHEMA.flatten())
-            .withField("description", Schema.STRING_SCHEMA.flatten()));
-
-    private static SchemaComponent registerSchema(String name, Schema schema) {
-        SchemaComponent schemacomponent = new SchemaComponent("neoforge:" + name, ReferenceUtil.createLocalReference(name), schema);
-        Schema.getSchemaRegistry().add(schemacomponent);
-        return schemacomponent;
-    }
+    public static final SchemaComponent MOD_SCHEMA = Schema.registerSchema(ResourceLocation.fromNamespaceAndPath(NeoForgeVersion.MOD_ID, "mod"), Schema.record()
+            .withField("modId", Schema.STRING_SCHEMA)
+            .withField("version", Schema.STRING_SCHEMA)
+            .withField("displayName", Schema.STRING_SCHEMA)
+            .withField("description", Schema.STRING_SCHEMA));
 }
