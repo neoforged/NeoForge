185cd8132d27575fdc84482d8662b1b90f18da5d
Minecraftschurli <minecraftschurli@gmail.com>
2025-09-30 21:34:14 +0200

    Add docs for RegisterSchemaEvent

--- src/main/java/net/neoforged/neoforge/event/server/RegisterSchemaEvent.java
+++ src/main/java/net/neoforged/neoforge/event/server/RegisterSchemaEvent.java
@@ -7,9 +7,15 @@ package net.neoforged.neoforge.event.server;
 
 import java.util.Map;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.dedicated.DedicatedServer;
+import net.minecraft.server.jsonrpc.ManagementServer;
 import net.minecraft.server.jsonrpc.api.SchemaComponent;
 import net.neoforged.bus.api.Event;
 
+/**
+ * Event to register {@link SchemaComponent SchemaComponents} used in incoming and outgoing JSON-RPC methods.<br>
+ * Fired before the {@link ManagementServer} is instantiated in {@link DedicatedServer#initServer()}.
+ */
 public class RegisterSchemaEvent extends Event {
     private final Map<ResourceLocation, SchemaComponent> components;
 
