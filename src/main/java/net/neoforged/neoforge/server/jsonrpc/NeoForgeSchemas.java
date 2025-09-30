3e1fb3260b666ce92f6e171a3b8988979503d1a8
Minecraftschurli <minecraftschurli@gmail.com>
2025-09-30 21:25:44 +0200

    Move schema registration to an event

--- src/main/java/net/neoforged/neoforge/server/jsonrpc/NeoForgeSchemas.java
+++ src/main/java/net/neoforged/neoforge/server/jsonrpc/NeoForgeSchemas.java
@@ -8,14 +8,19 @@ package net.neoforged.neoforge.server.jsonrpc;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.jsonrpc.api.Schema;
 import net.minecraft.server.jsonrpc.api.SchemaComponent;
+import net.neoforged.neoforge.event.server.RegisterSchemaEvent;
 import net.neoforged.neoforge.internal.versions.neoforge.NeoForgeVersion;
 
 public final class NeoForgeSchemas {
     private NeoForgeSchemas() {}
 
-    public static final SchemaComponent MOD_SCHEMA = Schema.registerSchema(ResourceLocation.fromNamespaceAndPath(NeoForgeVersion.MOD_ID, "mod"), Schema.record()
+    public static final SchemaComponent MOD_SCHEMA = new SchemaComponent(ResourceLocation.fromNamespaceAndPath(NeoForgeVersion.MOD_ID, "mod"), Schema.record()
             .withField("modId", Schema.STRING_SCHEMA)
             .withField("version", Schema.STRING_SCHEMA)
             .withField("displayName", Schema.STRING_SCHEMA)
             .withField("description", Schema.STRING_SCHEMA));
+
+    public static void registerSchemas(RegisterSchemaEvent event) {
+        event.register(MOD_SCHEMA);
+    }
 }
