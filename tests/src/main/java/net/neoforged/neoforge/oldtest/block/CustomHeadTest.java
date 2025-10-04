b501265c54442ca54f806578ad1c167996cf3703
SpinoScythe <84230130+Spinoscythe@users.noreply.github.com>
2025-10-04 22:33:03 +0100

    format

--- tests/src/main/java/net/neoforged/neoforge/oldtest/block/CustomHeadTest.java
+++ tests/src/main/java/net/neoforged/neoforge/oldtest/block/CustomHeadTest.java
@@ -31,7 +31,6 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.client.event.RegisterSpecialBlockModelRendererEvent;
 import net.neoforged.neoforge.common.util.Lazy;
