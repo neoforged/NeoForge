5615b01a6b3c7134f27b33aa090f0fda5ba9db9f
Sebastian Hartte <shartte@users.noreply.github.com>
2025-10-11 21:43:20 +0200

    Remove ordering from the event.

--- src/client/java/net/neoforged/neoforge/client/event/RegisterParticleGroupsEvent.java
+++ src/client/java/net/neoforged/neoforge/client/event/RegisterParticleGroupsEvent.java
@@ -19,7 +19,7 @@ import org.jetbrains.annotations.ApiStatus;
 
 /**
  * Fired for registering additional {@linkplain ParticleGroup particle group factories}.
- * <p>This is used when creating particle groups for particles in {@link ParticleEngine#createParticleGroup(ParticleRenderType)}.
+ * <p>This is used when creating particle groups for particles in {@link ParticleEngine}.
  *
  * <p>This event is fired on the mod-specific event bus,
  * only on the {@linkplain LogicalSide#CLIENT logical client}.</p>
@@ -37,84 +37,17 @@ public class RegisterParticleGroupsEvent extends Event implements IModBusEvent {
     }
 
     /**
-     * Registers a factory function for the given {@linkplain Particle#getGroup() particle group}, to be rendered
-     * before all currently registered types.
+     * Registers a factory function for the given {@linkplain Particle#getGroup() particle group}.
      *
-     * @param particleRenderType An identifier uniquely identifying the particle group.
-     * @param factory            A factory function used to create a {@link ParticleGroup} for the particle group.
-     *
-     * @throws IllegalArgumentException when {@code particleRenderType} has already been registered.
+     * @param group   The {@linkplain Particle#getGroup() particle group} to register for.
+     * @param factory A factory function used to create the {@link ParticleGroup}.
+     * @throws IllegalArgumentException when a factory has already been registered for {@code group}.
      */
-    public void registerBeforeAll(ParticleRenderType particleRenderType, Function<ParticleEngine, ParticleGroup<?>> factory) {
-        if (particleGroupFactories.putIfAbsent(particleRenderType, factory) != null) {
-            throw new IllegalArgumentException("Factory already registered for provided particle render type: " + particleRenderType.name());
+    public void register(ParticleRenderType group, Function<ParticleEngine, ParticleGroup<?>> factory) {
+        if (particleGroupFactories.putIfAbsent(group, factory) != null) {
+            throw new IllegalArgumentException("Factory already registered for provided particle render type: " + group.name());
         }
 
-        particleRenderOrder.addFirst(particleRenderType);
-    }
-
-    /**
-     * Registers a factory function for the given {@linkplain Particle#getGroup() particle group}, to be rendered
-     * after all currently registered types.
-     *
-     * @param particleRenderType An identifier uniquely identifying the particle group.
-     * @param factory            A factory function used to create a {@link ParticleGroup} for the particle group.
-     *
-     * @throws IllegalArgumentException when {@code particleRenderType} has already been registered.
-     */
-    public void registerAfterAll(ParticleRenderType particleRenderType, Function<ParticleEngine, ParticleGroup<?>> factory) {
-        if (particleGroupFactories.putIfAbsent(particleRenderType, factory) != null) {
-            throw new IllegalArgumentException("Factory already registered for provided particle render type: " + particleRenderType.name());
-        }
-
-        particleRenderOrder.add(particleRenderType);
-    }
-
-    /**
-     * Registers a factory function for the given {@linkplain Particle#getGroup() particle group}, to be rendered
-     * before the given type.
-     *
-     * @param before             An identifier uniquely identifying that particle group to register before
-     * @param particleRenderType An identifier uniquely identifying the particle group.
-     * @param factory            A factory function used to create a {@link ParticleGroup} for the particle group.
-     *
-     * @throws IllegalArgumentException when {@code before} has not been registered.
-     * @throws IllegalArgumentException when {@code particleRenderType} has already been registered.
-     */
-    public void registerBefore(ParticleRenderType before, ParticleRenderType particleRenderType, Function<ParticleEngine, ParticleGroup<?>> factory) {
-        var idx = particleRenderOrder.indexOf(before);
-        if (idx < 0) {
-            throw new IllegalArgumentException("Unknown particle render type: " + before.name());
-        }
-
-        if (particleGroupFactories.putIfAbsent(particleRenderType, factory) != null) {
-            throw new IllegalArgumentException("Factory already registered for provided particle render type: " + particleRenderType.name());
-        }
-
-        particleRenderOrder.add(idx, particleRenderType);
-    }
-
-    /**
-     * Registers a factory function for the given {@linkplain Particle#getGroup() particle group}, to be rendered
-     * after the given type.
-     *
-     * @param after              An identifier uniquely identifying that particle group to register after
-     * @param particleRenderType An identifier uniquely identifying the particle group.
-     * @param factory            A factory function used to create a {@link ParticleGroup} for the particle group.
-     *
-     * @throws IllegalArgumentException when {@code after} has not been registered.
-     * @throws IllegalArgumentException when {@code particleRenderType} has already been registered.
-     */
-    public void registerAfter(ParticleRenderType after, ParticleRenderType particleRenderType, Function<ParticleEngine, ParticleGroup<?>> factory) {
-        var idx = particleRenderOrder.indexOf(after);
-        if (idx < 0) {
-            throw new IllegalArgumentException("Unknown particle render type: " + after.name());
-        }
-
-        if (particleGroupFactories.putIfAbsent(particleRenderType, factory) != null) {
-            throw new IllegalArgumentException("Factory already registered for provided particle render type: " + particleRenderType.name());
-        }
-
-        particleRenderOrder.add(idx + 1, particleRenderType);
+        particleRenderOrder.add(group);
     }
 }
