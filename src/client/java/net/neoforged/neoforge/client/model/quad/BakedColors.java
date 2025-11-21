678a9d0142c52a3a27182d807e0171ad02cd214f
XFactHD <xfacthd@gmx.de>
2025-11-21 18:23:37 +0100

    Apply requested changes

--- src/client/java/net/neoforged/neoforge/client/model/quad/BakedColors.java
+++ src/client/java/net/neoforged/neoforge/client/model/quad/BakedColors.java
@@ -1,58 +1,43 @@
 package net.neoforged.neoforge.client.model.quad;
 
+/**
+ * Holds a static color modulator, either for the entire quad or for each vertex, in the ARGB format.
+ * <p>
+ * The stored color value is converted from ARGB to ABGR during buffering.
+ */
 public sealed interface BakedColors {
-    BakedColors DEFAULT = new Slim(0xFFFFFFFF);
+    BakedColors DEFAULT = new PerQuad(0xFFFFFFFF);
 
-    int color0();
+    int color(int vertex);
 
-    int color1();
-
-    int color2();
-
-    int color3();
-
-    default int color(int vert) {
-        return switch (vert) {
-            case 0 -> color0();
-            case 1 -> color1();
-            case 2 -> color2();
-            case 3 -> color3();
-            default -> throw new IndexOutOfBoundsException(vert);
-        };
-    }
-
-    record Slim(int color) implements BakedColors {
+    record PerQuad(int color) implements BakedColors {
         @Override
-        public int color0() {
-            return color;
-        }
-
-        @Override
-        public int color1() {
-            return color;
-        }
-
-        @Override
-        public int color2() {
-            return color;
-        }
-
-        @Override
-        public int color3() {
+        public int color(int vertex) {
             return color;
         }
     }
 
-    record Full(int color0, int color1, int color2, int color3) implements BakedColors { }
+    record PerVertex(int color0, int color1, int color2, int color3) implements BakedColors {
+        @Override
+        public int color(int vertex) {
+            return switch (vertex) {
+                case 0 -> color0;
+                case 1 -> color1;
+                case 2 -> color2;
+                case 3 -> color3;
+                default -> throw new IndexOutOfBoundsException(vertex);
+            };
+        }
+    }
 
     static BakedColors of(int color0, int color1, int color2, int color3) {
         if (color0 == color1 && color0 == color2 && color0 == color3) {
             return of(color0);
         }
-        return new Full(color0, color1, color2, color3);
+        return new PerVertex(color0, color1, color2, color3);
     }
 
     static BakedColors of(int color) {
-        return color == 0xFFFFFFFF ? DEFAULT : new Slim(color);
+        return color == 0xFFFFFFFF ? DEFAULT : new PerQuad(color);
     }
 }
--- src/client/java/net/neoforged/neoforge/client/model/quad/BakedNormals.java
+++ src/client/java/net/neoforged/neoforge/client/model/quad/BakedNormals.java
@@ -1,58 +1,44 @@
 package net.neoforged.neoforge.client.model.quad;
 
+/**
+ * Holds pre-computed normals, either for the entire quad or for each vertex.
+ * <p>
+ * The normal values are quantized to an integer between -127 and 127, truncated to a byte
+ * and packed into an int, leaving the MSB unused.
+ */
 public sealed interface BakedNormals {
-    BakedNormals UNSPECIFIED = new Slim(0);
+    BakedNormals UNSPECIFIED = new PerQuad(0);
 
-    int normals0();
+    int normals(int vertex);
 
-    int normals1();
-
-    int normals2();
-
-    int normals3();
-
-    default int normals(int vert) {
-        return switch (vert) {
-            case 0 -> normals0();
-            case 1 -> normals1();
-            case 2 -> normals2();
-            case 3 -> normals3();
-            default -> throw new IndexOutOfBoundsException(vert);
-        };
-    }
-
-    record Slim(int normals) implements BakedNormals {
+    record PerQuad(int normals) implements BakedNormals {
         @Override
-        public int normals0() {
-            return normals;
-        }
-
-        @Override
-        public int normals1() {
-            return normals;
-        }
-
-        @Override
-        public int normals2() {
-            return normals;
-        }
-
-        @Override
-        public int normals3() {
+        public int normals(int vertex) {
             return normals;
         }
     }
 
-    record Full(int normals0, int normals1, int normals2, int normals3) implements BakedNormals { }
+    record PerVertex(int normals0, int normals1, int normals2, int normals3) implements BakedNormals {
+        @Override
+        public int normals(int vertex) {
+            return switch (vertex) {
+                case 0 -> normals0;
+                case 1 -> normals1;
+                case 2 -> normals2;
+                case 3 -> normals3;
+                default -> throw new IndexOutOfBoundsException(vertex);
+            };
+        }
+    }
 
     static BakedNormals of(int normals0, int normals1, int normals2, int normals3) {
         if (normals0 == normals1 && normals0 == normals2 && normals0 == normals3) {
             return of(normals0);
         }
-        return new Full(normals0, normals1, normals2, normals3);
+        return new PerVertex(normals0, normals1, normals2, normals3);
     }
 
     static BakedNormals of(int normals) {
-        return normals == 0 ? UNSPECIFIED : new Slim(normals);
+        return normals == 0 ? UNSPECIFIED : new PerQuad(normals);
     }
 }
