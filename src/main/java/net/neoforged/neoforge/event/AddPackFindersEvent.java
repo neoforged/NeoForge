6b2d57344afec17caae665a0d271ca208b841380
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2025-12-11 20:32:43 -0500

    Formatter check goes brrr

--- src/main/java/net/neoforged/neoforge/event/AddPackFindersEvent.java
+++ src/main/java/net/neoforged/neoforge/event/AddPackFindersEvent.java
@@ -12,7 +12,6 @@ import net.minecraft.resources.Identifier;
 import net.minecraft.server.packs.PackLocationInfo;
 import net.minecraft.server.packs.PackSelectionConfig;
 import net.minecraft.server.packs.PackType;
-import net.minecraft.server.packs.repository.BuiltInPackSource;
 import net.minecraft.server.packs.repository.KnownPack;
 import net.minecraft.server.packs.repository.Pack;
 import net.minecraft.server.packs.repository.PackRepository;
