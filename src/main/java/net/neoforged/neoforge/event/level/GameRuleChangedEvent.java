bb10a9e0c4c8862cd2462ab02960a56c762b5264
Apex <robsonlawson3@gmail.com>
2025-12-16 01:00:49 +0000

    Move event to be called from `MinecraftServer#onGameRuleChanged`

--- src/main/java/net/neoforged/neoforge/event/level/GameRuleChangedEvent.java
+++ src/main/java/net/neoforged/neoforge/event/level/GameRuleChangedEvent.java
@@ -6,7 +6,6 @@
 package net.neoforged.neoforge.event.level;
 
 import java.util.function.Consumer;
-import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.level.gamerules.GameRule;
 import net.minecraft.world.level.gamerules.GameRules;
@@ -14,41 +13,30 @@ import net.neoforged.bus.api.Event;
 import net.neoforged.fml.LogicalSide;
 import net.neoforged.neoforge.common.NeoForge;
 import org.jetbrains.annotations.ApiStatus;
-import org.jspecify.annotations.Nullable;
 
 /**
  * Event which is fired when ever a {@link GameRule} is updated.
  * <p>
- * This event is fired on the {@link NeoForge#EVENT_BUS main NeoForge event bus}, only on both the logical {@linkplain LogicalSide#CLIENT client} and {@linkplain LogicalSide#SERVER}.
+ * This event is fired on the {@link NeoForge#EVENT_BUS main NeoForge event bus}, only on the {@linkplain LogicalSide#SERVER logical server}.
  */
 public final class GameRuleChangedEvent extends Event {
-    @Nullable
     private final MinecraftServer server;
-    private final GameRules gameRules;
     private final GameRule<?> gameRule;
     private final Object newValue;
 
     @ApiStatus.Internal
-    public <T> GameRuleChangedEvent(@Nullable MinecraftServer server, GameRules gameRules, GameRule<T> gameRule, T newValue) {
+    public <T> GameRuleChangedEvent(MinecraftServer server, GameRule<T> gameRule, T newValue) {
         this.server = server;
-        this.gameRules = gameRules;
         this.gameRule = gameRule;
         this.newValue = newValue;
     }
 
-    /**
-     * {@return The active server instance holding the game rules.}
-     *
-     * @apiNote Note that not all game rule state changes pass along a server instance, For example the {@linkplain net.neoforged.neoforge.server.command.TimeSpeedCommand#setSpeed(CommandSourceStack, float) TimeSpeedCommand}
-     *          updates {@link GameRules#ADVANCE_TIME} while passing a {@code null} server instance.
-     */
-    @Nullable
     public MinecraftServer getServer() {
         return server;
     }
 
     public GameRules getGameRules() {
-        return gameRules;
+        return server.getWorldData().getGameRules();
     }
 
     public GameRule<?> getGameRule() {
