fccf25218ba5c2ed331d594bcd22337633f4a0d0
XFactHD <xfacthd@gmx.de>
2025-12-18 19:09:32 +0100

    Remove unused import

--- src/client/java/net/neoforged/neoforge/client/ClientHooks.java
+++ src/client/java/net/neoforged/neoforge/client/ClientHooks.java
@@ -127,7 +127,6 @@ import net.minecraft.server.packs.resources.ReloadInstance;
 import net.minecraft.server.packs.resources.ReloadableResourceManager;
 import net.minecraft.sounds.Music;
 import net.minecraft.util.ARGB;
-import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.InteractionHand;
