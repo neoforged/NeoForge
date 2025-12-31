6b2ba315899985118778681c4792e7bff35ab163
Sebastian Hartte <shartte@users.noreply.github.com>
2025-12-31 17:44:23 +0100

    Add hacky debug output to try and figure out why this test fails.

--- tests/src/junit/java/net/neoforged/neoforge/unittest/ComponentHashingTest.java
+++ tests/src/junit/java/net/neoforged/neoforge/unittest/ComponentHashingTest.java
@@ -5,16 +5,24 @@
 
 package net.neoforged.neoforge.unittest;
 
+import it.unimi.dsi.fastutil.objects.Reference2ObjectMap;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.core.component.PatchedDataComponentMap;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.neoforged.fml.util.ObfuscationReflectionHelper;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ComponentHashingTest {
+    private static final Logger LOG = LoggerFactory.getLogger(ComponentHashingTest.class);
+
     @Test
     void testHashCodeCollisions() {
         int MAX_DAMAGE = 1000;
@@ -35,6 +43,34 @@ public class ComponentHashingTest {
 
         // Collisions should be rare, say less than 1%
         double collisionRate = 1 - (double) hashCodeToStacks.size() / MAX_DAMAGE / MAX_REPAIR_COST;
+        LOG.info("HashCode Dump (Unique HashCodes: {}):", hashCodeToStacks.size());
+        LOG.info("========================================================");
+        for (var entry : hashCodeToStacks.entrySet()) {
+            LOG.info("{} -> {}", entry.getKey(), entry.getValue());
+        }
+        LOG.info("========================================================");
+        LOG.info("Breakdown for biggest bucket");
+        LOG.info("========================================================");
+        var biggestBucket = hashCodeToStacks.values().stream().max(Comparator.comparingInt(List::size)).get();
+        for (var stack : biggestBucket) {
+            LOG.info("HashCode Breakdown for {} ({})", stack.getItem(), stack.getComponentsPatch());
+            LOG.info("  Overall -> {}", stack.hashCode());
+            LOG.info("  Item -> {}", stack.getItem().hashCode());
+            LOG.info("  DataComponentMap -> {}", stack.getComponents().hashCode());
+            if (stack.getComponents() instanceof PatchedDataComponentMap patchedMap) {
+                var prototypeHash = ObfuscationReflectionHelper.getPrivateValue(PatchedDataComponentMap.class, patchedMap, "prototype").hashCode();
+                var patch = (Reference2ObjectMap<?, ?>) ObfuscationReflectionHelper.getPrivateValue(PatchedDataComponentMap.class, patchedMap, "patch");
+
+                LOG.info("  prototype -> {}", prototypeHash);
+                LOG.info("  patch -> {}", patch.hashCode());
+                for (var patchEntry : patch.entrySet()) {
+                    LOG.info("    [{}] -> {} (value: {})", patchEntry.getKey(), patchEntry.getValue().hashCode(), patchEntry.getValue());
+                }
+            } else {
+                LOG.info("  unpatched map -> {}", stack.getComponents().hashCode());
+            }
+        }
+        LOG.info("========================================================");
         if (collisionRate > 0.01) {
             throw new AssertionError("Too many hash code collisions detected: " + collisionRate);
         }
