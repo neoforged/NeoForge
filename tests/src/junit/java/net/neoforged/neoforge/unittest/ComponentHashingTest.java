6fc835ac4b7a0a8f85c321b8fad75c4954e45943
Sebastian Hartte <shartte@users.noreply.github.com>
2025-12-31 23:13:16 +0100

    Fix higher than expected collision count for DataComponentMap hashcodes

--- tests/src/junit/java/net/neoforged/neoforge/unittest/ComponentHashingTest.java
+++ tests/src/junit/java/net/neoforged/neoforge/unittest/ComponentHashingTest.java
@@ -5,29 +5,24 @@
 
 package net.neoforged.neoforge.unittest;
 
-import it.unimi.dsi.fastutil.objects.Reference2ObjectMap;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HexFormat;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
-import java.util.stream.Collectors;
 import net.minecraft.core.component.DataComponents;
-import net.minecraft.core.component.PatchedDataComponentMap;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.neoforged.fml.util.ObfuscationReflectionHelper;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.opentest4j.TestAbortedException;
 
 public class ComponentHashingTest {
-    private static final Logger LOG = LoggerFactory.getLogger(ComponentHashingTest.class);
-
     @Test
     void testHashCodeCollisions() {
+        // If the hashcode of DAMAGE and REPAIR_COST is identical, this test will produce a lot more collisions
+        if (System.identityHashCode(DataComponents.DAMAGE) == System.identityHashCode(DataComponents.REPAIR_COST)) {
+            throw new TestAbortedException("Cannot run hash collision test when hashcode of the two data components used by this test is the same");
+        }
+
         int MAX_DAMAGE = 1000;
         int MAX_REPAIR_COST = 100;
 
@@ -46,37 +41,6 @@ public class ComponentHashingTest {
 
         // Collisions should be rare, say less than 1%
         double collisionRate = 1 - (double) hashCodeToStacks.size() / MAX_DAMAGE / MAX_REPAIR_COST;
-        LOG.info("HashCode Dump (Unique HashCodes: {}):", hashCodeToStacks.size());
-        LOG.info("========================================================");
-        for (var entry : hashCodeToStacks.entrySet()) {
-            var stackList = entry.getValue().stream().map(is -> String.format(Locale.ROOT, "[%d, %d]", is.get(DataComponents.DAMAGE), is.get(DataComponents.REPAIR_COST))).collect(Collectors.joining(", "));
-            LOG.info("{} -> {}", entry.getKey(), stackList);
-        }
-        LOG.info("========================================================");
-        LOG.info("Breakdown for biggest bucket");
-        LOG.info("========================================================");
-        var biggestBucket = hashCodeToStacks.values().stream().max(Comparator.comparingInt(List::size)).get();
-        for (var stack : biggestBucket) {
-            LOG.info("HashCode Breakdown for {} ({})", stack.getItem(), stack.getComponentsPatch());
-            LOG.info("  Overall -> {}", ItemStack.hashItemAndComponents(stack));
-            LOG.info("  Item -> {}", stack.getItem().hashCode());
-            LOG.info("  DataComponentMap -> {}", stack.getComponents().hashCode());
-            if (stack.getComponents() instanceof PatchedDataComponentMap patchedMap) {
-                var prototypeHash = ObfuscationReflectionHelper.getPrivateValue(PatchedDataComponentMap.class, patchedMap, "prototype").hashCode();
-                var patch = (Reference2ObjectMap<?, ?>) ObfuscationReflectionHelper.getPrivateValue(PatchedDataComponentMap.class, patchedMap, "patch");
-
-                LOG.info("    prototype -> {}", prototypeHash);
-                int n = patch.size();
-                var iterator = it.unimi.dsi.fastutil.objects.Reference2ObjectMaps.fastIterator(patch);
-                while (n-- != 0) {
-                    var entry = iterator.next();
-                    LOG.info("      [{} -> {}] -> [ihc=0x{}, value:{}]", entry.getKey(), entry.getValue(), HexFormat.of().toHexDigits(System.identityHashCode(entry.getKey())), entry.getValue().hashCode());
-                }
-            } else {
-                LOG.info("    unpatched map -> {}", stack.getComponents().hashCode());
-            }
-        }
-        LOG.info("========================================================");
         if (collisionRate > 0.01) {
             throw new AssertionError("Too many hash code collisions detected: " + collisionRate);
         }
