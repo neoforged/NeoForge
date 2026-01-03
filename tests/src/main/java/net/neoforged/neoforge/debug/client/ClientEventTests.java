neoforged/NeoForge

commit 2fe541ad0a7bc1c1851f1812ec6df017546e6a15

Port to 1.21.9-pre1
Co-authored-by: XFactHD <xfacthd@gmx.de>
Co-authored-by: coehlrich <coehlrich@users.noreply.github.com>

patches/net/minecraft/client/renderer/entity/EntityRenderer.java.patch
@@ -1,42 +1,11 @@
 --- a/net/minecraft/client/renderer/entity/EntityRenderer.java
 +++ b/net/minecraft/client/renderer/entity/EntityRenderer.java
-@@ -114,6 +_,8 @@
-         }
+@@ -132,6 +_,8 @@
  
-         if (p_364816_.nameTag != null) {
-+            var event = new net.neoforged.neoforge.client.event.RenderNameTagEvent.DoRender(p_364816_, p_364816_.nameTag, this, p_114488_, p_114489_, p_114490_, p_364816_.partialTick);
+     protected void submitNameTag(S p_436016_, PoseStack p_433473_, SubmitNodeCollector p_434265_, CameraRenderState p_451278_) {
+         if (p_436016_.nameTag != null) {
++            var event = new net.neoforged.neoforge.client.event.RenderNameTagEvent.DoRender(p_436016_, p_436016_.nameTag, this, p_433473_, p_434265_, p_436016_.partialTick);
 +            if (!net.neoforged.neoforge.common.NeoForge.EVENT_BUS.post(event).isCanceled())
-             this.renderNameTag(p_364816_, p_364816_.nameTag, p_114488_, p_114489_, p_114490_);
-         }
-     }
-@@ -228,6 +_,7 @@
-     public final S createRenderState(T p_361382_, float p_360885_) {
-         S s = this.reusedState;
-         this.extractRenderState(p_361382_, s, p_360885_);
-+        net.neoforged.neoforge.client.renderstate.RenderStateExtensions.onUpdateEntityRenderState(this, p_361382_, s);
-         return s;
-     }
- 
-@@ -254,9 +_,11 @@
-         }
- 
-         p_361028_.distanceToCameraSq = this.entityRenderDispatcher.distanceToSqr(p_362104_);
--        boolean flag1 = p_361028_.distanceToCameraSq < 4096.0 && this.shouldShowName(p_362104_, p_361028_.distanceToCameraSq);
-+        var event = new net.neoforged.neoforge.client.event.RenderNameTagEvent.CanRender(p_362104_, p_361028_, this.getNameTag(p_362104_), this, p_362204_);
-+        net.neoforged.neoforge.common.NeoForge.EVENT_BUS.post(event);
-+        boolean flag1 = event.canRender().isTrue() || (event.canRender().isDefault() && p_361028_.distanceToCameraSq < 4096.0 && this.shouldShowName(p_362104_, p_361028_.distanceToCameraSq));
-         if (flag1) {
--            p_361028_.nameTag = this.getNameTag(p_362104_);
-+            p_361028_.nameTag = event.getContent();
-             p_361028_.nameTagAttachment = p_362104_.getAttachments().getNullable(EntityAttachment.NAME_TAG, 0, p_362104_.getYRot(p_362204_));
-         } else {
-             p_361028_.nameTag = null;
-@@ -334,6 +_,8 @@
-             p_361028_.hitboxesRenderState = null;
-             p_361028_.serverHitboxesRenderState = null;
-         }
-+
-+        p_361028_.partialTick = p_362204_;
-     }
- 
-     private void extractHitboxes(T p_412316_, S p_412142_, float p_412603_) {
+             p_434265_.submitNameTag(
+                 p_433473_,
+                 p_436016_.nameTagAttachment,

tests/src/main/java/net/neoforged/neoforge/debug/client/ClientEventTests.java
@@ -10,13 +10,15 @@ import com.mojang.blaze3d.vertex.BufferBuilder;
 import com.mojang.math.Axis;
 import java.util.Map;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.AbstractHoglinRenderer;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.MobRenderer;
-import net.minecraft.client.renderer.entity.player.PlayerRenderer;
+import net.minecraft.client.renderer.entity.PigRenderer;
 import net.minecraft.client.renderer.entity.state.HoglinRenderState;
 import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
+import net.minecraft.client.renderer.item.ItemStackRenderState;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.SectionPos;
@@ -134,7 +136,11 @@ public class ClientEventTests {
             listeners.forge().addListener((final RenderPlayerEvent.Post event) -> {
                 event.getPoseStack().pushPose();
                 event.getPoseStack().translate(0, 2, 0);
-                Minecraft.getInstance().getItemRenderer().renderStatic(itemStack, ItemDisplayContext.GROUND, event.getPackedLight(), OverlayTexture.NO_OVERLAY, event.getPoseStack(), event.getMultiBufferSource(), Minecraft.getInstance().level, 0);
+
+                ItemStackRenderState renderState = new ItemStackRenderState();
+                Minecraft.getInstance().getItemModelResolver().updateForTopItem(renderState, itemStack, ItemDisplayContext.GROUND, null, null, 0);
+                renderState.submit(event.getPoseStack(), event.getSubmitNodeCollector(), LightTexture.FULL_BRIGHT, OverlayTexture.NO_OVERLAY, 0);
+
                 event.getPoseStack().popPose();
             });
             test.requestConfirmation(Minecraft.getInstance().player, Component.literal("Is an iron block rendered above you in third-person?"));
@@ -147,7 +153,7 @@ public class ClientEventTests {
         var numRenderAttachmentKey = new ContextKey<Integer>(ResourceLocation.fromNamespaceAndPath(test.createModId(), "times_to_render"));
         var testAttachment = test.registrationHelper().attachments().registerSimpleAttachment("test", () -> 3);
         test.framework().modEventBus().addListener((RegisterRenderStateModifiersEvent event) -> {
-            event.registerEntityModifier(PlayerRenderer.class, (entity, renderState) -> {
+            event.registerEntityModifier(PigRenderer.class, (entity, renderState) -> {
                 renderState.setRenderData(rotationKey, 45f);
             });
             event.registerEntityModifier(new TypeToken<LivingEntityRenderer<? extends LivingEntity, LivingEntityRenderState, ?>>() {}, (entity, renderState) -> {
@@ -171,7 +177,7 @@ public class ClientEventTests {
                     return;
                 }
                 float xRotation = event.getRenderState().getRenderDataOrDefault(rotationKey, 0f);
-                if (event.getRenderer() instanceof PlayerRenderer && numRender == 0) {
+                if (event.getRenderer() instanceof PigRenderer && numRender == 0) {
                     test.fail("Custom render data not set for player");
                     return;
                 }
@@ -183,14 +189,12 @@ public class ClientEventTests {
                     poseStack.translate(0, 1, 0);
                     poseStack.pushPose();
                     poseStack.mulPose(Axis.XP.rotation(xRotation));
-                    Minecraft.getInstance().getBlockRenderer().renderSingleBlock(
-                            Blocks.CALCITE.defaultBlockState(),
+                    event.getSubmitNodeCollector().submitBlock(
                             poseStack,
-                            event.getMultiBufferSource(),
-                            event.getPackedLight(),
+                            Blocks.CALCITE.defaultBlockState(),
+                            event.getRenderState().lightCoords,
                             OverlayTexture.NO_OVERLAY,
-                            EmptyBlockAndTintGetter.INSTANCE,
-                            BlockPos.ZERO);
+                            -1);
                     poseStack.popPose();
                 }
                 poseStack.popPose();
@@ -208,7 +212,7 @@ public class ClientEventTests {
                         var randomSource = new SingleThreadedRandomSource(0);
                         var state = Blocks.GOLD_BLOCK.defaultBlockState();
                         var stack = event.getPoseStack();
-                        var camera = event.getCamera().getPosition();
+                        var camera = event.getCamera().pos;
                         event.getRenderableSections().forEach(section -> {
                             if (section.isEmpty()) {
                                 return;

commit cfe951bac6fc5ad81528d4e8a55005a9115f08f6

Fix RenderNameTagEvent.CanRender not being posted to event bus
The event was broken in 2fe541ad0a7bc1c1851f1812ec6df017546e6a15 where
the EVENT_BUS.post() call was accidentally removed. This restores the
event posting so listeners can receive the event again.

Added a test to verify the event is fired correctly.

patches/net/minecraft/client/renderer/entity/EntityRenderer.java.patch
@@ -25,12 +25,13 @@
          p_361028_.ageInTicks = p_362104_.tickCount + p_362204_;
          p_361028_.boundingBoxWidth = p_362104_.getBbWidth();
          p_361028_.boundingBoxHeight = p_362104_.getBbHeight();
-@@ -184,9 +_,10 @@
+@@ -184,9 +_,11 @@
  
          if (this.entityRenderDispatcher.camera != null) {
              p_361028_.distanceToCameraSq = this.entityRenderDispatcher.distanceToSqr(p_362104_);
 -            boolean flag1 = p_361028_.distanceToCameraSq < 4096.0 && this.shouldShowName(p_362104_, p_361028_.distanceToCameraSq);
 +            var event = new net.neoforged.neoforge.client.event.RenderNameTagEvent.CanRender(p_362104_, p_361028_, this.getNameTag(p_362104_), this, p_362204_);
++            net.neoforged.neoforge.common.NeoForge.EVENT_BUS.post(event);
 +            boolean flag1 = event.canRender().isTrue() || (event.canRender().isDefault() && p_361028_.distanceToCameraSq < 4096.0 && this.shouldShowName(p_362104_, p_361028_.distanceToCameraSq));
              if (flag1) {
 -                p_361028_.nameTag = this.getNameTag(p_362104_);

tests/src/main/java/net/neoforged/neoforge/debug/client/ClientEventTests.java
@@ -44,6 +44,7 @@ import net.neoforged.neoforge.client.event.ClientPlayerChangeGameTypeEvent;
 import net.neoforged.neoforge.client.event.RegisterRenderBuffersEvent;
 import net.neoforged.neoforge.client.event.RenderLevelStageEvent;
 import net.neoforged.neoforge.client.event.RenderLivingEvent;
+import net.neoforged.neoforge.client.event.RenderNameTagEvent;
 import net.neoforged.neoforge.client.event.RenderPlayerEvent;
 import net.neoforged.neoforge.client.renderstate.RegisterRenderStateModifiersEvent;
 import net.neoforged.neoforge.common.NeoForge;
@@ -240,6 +241,15 @@ public class ClientEventTests {
                         });
                     });
                 }
+            });
+        });
+    }
+
+    @TestHolder(description = { "Test RenderNameTagEvent.CanRender is called" })
+    static void nameTagCanRender(final DynamicTest test) {
+        test.whenEnabled(listeners -> {
+            listeners.forge().addListener((final RenderNameTagEvent.CanRender canRenderEvent) -> {
+                test.pass();
             });
         });
     }