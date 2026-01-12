1652321295d26527cf65ebb6744a092b934fc53d
Apex <robsonlawson3@gmail.com>
2026-01-12 19:29:29 +0000

    Remove old '#wool' reference
    Was left over from initial testing when the wool tag was replaced in shears tool rules

--- src/main/java/net/neoforged/neoforge/common/data/internal/NeoForgeBlockTagsProvider.java
+++ src/main/java/net/neoforged/neoforge/common/data/internal/NeoForgeBlockTagsProvider.java
@@ -310,7 +310,7 @@ public final class NeoForgeBlockTagsProvider extends BlockTagsProvider {
         tag(Tags.Blocks.SANDS).addOptionalTag(forge("sand"));
         tag(Tags.Blocks.SANDS_COLORLESS).addOptionalTag(forge("sand/colorless"));
         tag(Tags.Blocks.SANDS_RED).addOptionalTag(forge("sand/red"));
-        tag(Tags.Blocks.SHEARS_EFFICIENT).addTag(BlockTags.WOOL);
+        tag(Tags.Blocks.SHEARS_EFFICIENT);
     }
 
     private TagAppender<Block, Block> tagWithOptionalLegacy(TagKey<Block> tag) {
