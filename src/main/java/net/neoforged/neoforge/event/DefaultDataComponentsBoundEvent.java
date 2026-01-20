5ebd270d1f227b47441ab269b8c29afd135157ed
Sebastian Hartte <shartte@users.noreply.github.com>
2026-01-20 01:03:34 +0100

    Rework component bound events

--- src/main/java/net/neoforged/neoforge/event/DefaultDataComponentsBoundEvent.java
+++ src/main/java/net/neoforged/neoforge/event/DefaultDataComponentsBoundEvent.java
@@ -15,19 +15,19 @@ import org.jetbrains.annotations.ApiStatus;
  * <p>This event signals when {@link Holder#areComponentsBound()} becomes true.
  */
 public class DefaultDataComponentsBoundEvent extends Event {
-    private final TagsUpdatedEvent.UpdateCause updateCause;
+    private final UpdateCause updateCause;
     private final boolean integratedServer;
 
     @ApiStatus.Internal
     public DefaultDataComponentsBoundEvent(boolean fromClientPacket, boolean isIntegratedServerConnection) {
-        this.updateCause = fromClientPacket ? TagsUpdatedEvent.UpdateCause.CLIENT_PACKET_RECEIVED : TagsUpdatedEvent.UpdateCause.SERVER_DATA_LOAD;
+        this.updateCause = fromClientPacket ? UpdateCause.CLIENT_PACKET_RECEIVED : UpdateCause.SERVER_DATA_LOAD;
         this.integratedServer = isIntegratedServerConnection;
     }
 
     /**
-     * @return the cause for this tag update
+     * @return the cause for binding the default data components
      */
-    public TagsUpdatedEvent.UpdateCause getUpdateCause() {
+    public UpdateCause getUpdateCause() {
         return updateCause;
     }
 
@@ -36,20 +36,20 @@ public class DefaultDataComponentsBoundEvent extends Event {
      * result of this event. Effectively this means that in single player only the server-side updates this data.
      */
     public boolean shouldUpdateStaticData() {
-        return updateCause == TagsUpdatedEvent.UpdateCause.SERVER_DATA_LOAD || !integratedServer;
+        return updateCause == UpdateCause.SERVER_DATA_LOAD || !integratedServer;
     }
 
     /**
-     * Represents the cause for a tag update.
+     * Represents the cause for binding the default data components.
      */
     public enum UpdateCause {
         /**
-         * The tag update is caused by the server loading datapack data. Note that in single player this still happens
+         * The default data components were bound due to the server loading datapack data. Note that in single player this still happens
          * on the client thread.
          */
         SERVER_DATA_LOAD,
         /**
-         * The tag update is caused by the client receiving the tag data from the server.
+         * The default data components were bound due to the client receiving the datapack data from the server.
          */
         CLIENT_PACKET_RECEIVED
     }
