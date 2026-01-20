b18db64390474578af90de7f44345582aeb62f74
Swedz <me@swedz.net>
2026-01-20 06:01:27 -0500

    Fix offhand slot not being handled

--- src/main/java/net/neoforged/neoforge/common/CommonHooks.java
+++ src/main/java/net/neoforged/neoforge/common/CommonHooks.java
@@ -108,6 +108,7 @@ import net.minecraft.world.entity.ai.attributes.DefaultAttributes;
 import net.minecraft.world.entity.ai.village.poi.PoiManager;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.monster.EnderMan;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.AnvilMenu;
@@ -666,7 +667,8 @@ public class CommonHooks {
                 }
                 // inform the client that the item was not consumed
                 if (player instanceof ServerPlayer serverPlayer) {
-                    serverPlayer.connection.send(new ClientboundSetPlayerInventoryPacket(serverPlayer.getInventory().getSelectedSlot(), itemstack));
+                    int slot = context.getHand() == InteractionHand.MAIN_HAND ? serverPlayer.getInventory().getSelectedSlot() : Inventory.SLOT_OFFHAND;
+                    serverPlayer.connection.send(serverPlayer.getInventory().createInventoryUpdatePacket(slot));
                 }
             } else {
                 // Change the stack to its new content
