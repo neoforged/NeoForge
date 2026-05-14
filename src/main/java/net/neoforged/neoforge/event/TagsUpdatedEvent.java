2873fa32bf840e548cf8ef8052617f9db923b792
XFactHD <11262040+XFactHD@users.noreply.github.com>
2026-05-14 06:08:48 +0200

    Apply suggested changes

--- src/main/java/net/neoforged/neoforge/event/TagsUpdatedEvent.java
+++ src/main/java/net/neoforged/neoforge/event/TagsUpdatedEvent.java
@@ -9,19 +9,20 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.server.ReloadableServerResources;
 import net.neoforged.bus.api.Event;
+import org.jetbrains.annotations.ApiStatus;
 
-/**
- * Fired when tags are updated on either server or client. This event can be used to refresh data that depends on tags.
- */
+/// Fired when tags are updated on either server or client. This event can be used to refresh data that depends on tags.
+///
+/// Listeners intending to use this event to update static data guarded by [#shouldUpdateStaticData()] should listen to
+/// the root event. Listeners interested the side-specific notification or the side-specific context should listen to
+/// the [TagsUpdatedEvent.ServerDataLoad] and [TagsUpdatedEvent.ClientPacketReceived] subclasses instead.
 public sealed class TagsUpdatedEvent extends Event {
     private final RegistryAccess registries;
     private final UpdateCause updateCause;
-    private final boolean integratedServer;
 
-    protected TagsUpdatedEvent(RegistryAccess registries, boolean fromClientPacket, boolean isIntegratedServerConnection) {
+    protected TagsUpdatedEvent(RegistryAccess registries, boolean fromClientPacket) {
         this.registries = registries;
         this.updateCause = fromClientPacket ? UpdateCause.CLIENT_PACKET_RECEIVED : UpdateCause.SERVER_DATA_LOAD;
-        this.integratedServer = isIntegratedServerConnection;
     }
 
     /// {@return the registries that have had their tags rebound}
@@ -32,32 +33,32 @@ public sealed class TagsUpdatedEvent extends Event {
     /// @return The dynamic registries that have had their tags rebound.
     ///
     /// @deprecated Use [#getRegistries()] instead
-    @Deprecated
+    @Deprecated(forRemoval = true, since = "26.1.2")
     public HolderLookup.Provider getLookupProvider() {
         return registries;
     }
 
-    /**
-     * @return the cause for this tag update
-     */
+    /// {@return the cause for this tag update}
+    ///
+    /// @deprecated Subscribe to subclasses instead
+    @Deprecated(forRemoval = true, since = "26.1.2")
     public UpdateCause getUpdateCause() {
         return updateCause;
     }
 
-    /**
-     * Whether static data (which in single player is shared between server and client thread) should be updated as a
-     * result of this event. Effectively this means that in single player only the server-side updates this data.
-     */
+    /// Whether static data (which in single player is shared between server and client thread) should be updated as a
+    /// result of this event. Effectively this means that in single player only the server-side updates this data.
     public boolean shouldUpdateStaticData() {
-        return updateCause == UpdateCause.SERVER_DATA_LOAD || !integratedServer;
+        return true;
     }
 
     /// Fired when tags are updated following a server datapack (re)load
     public static final class ServerDataLoad extends TagsUpdatedEvent {
         private final ReloadableServerResources serverResources;
 
+        @ApiStatus.Internal
         public ServerDataLoad(ReloadableServerResources serverResources, RegistryAccess registries) {
-            super(registries, false, false);
+            super(registries, false);
             this.serverResources = serverResources;
         }
 
@@ -69,14 +70,22 @@ public sealed class TagsUpdatedEvent extends Event {
 
     /// Fired when tags are updated by the client receiving tag data from the server
     public static final class ClientPacketReceived extends TagsUpdatedEvent {
+        private final boolean integratedServer;
+
+        @ApiStatus.Internal
         public ClientPacketReceived(RegistryAccess registries, boolean isIntegratedServerConnection) {
-            super(registries, true, isIntegratedServerConnection);
+            super(registries, true);
+            this.integratedServer = isIntegratedServerConnection;
+        }
+
+        @Override
+        public boolean shouldUpdateStaticData() {
+            return !integratedServer;
         }
     }
 
-    /**
-     * Represents the cause for a tag update.
-     */
+    /// Represents the cause for a tag update.
+    @Deprecated(forRemoval = true, since = "26.1.2")
     public enum UpdateCause {
         /**
          * The tag update is caused by the server loading datapack data. Note that in single player this still happens
