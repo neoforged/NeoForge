9d2a5dbcdafea954724bf8b71d32b29ef38e64e3
Thiakil <thiakil@users.noreply.github.com>
2026-05-17 13:26:24 +0800

    test formatting

--- tests/src/junit/java/net/neoforged/neoforge/unittest/ObjTests.java
+++ tests/src/junit/java/net/neoforged/neoforge/unittest/ObjTests.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) NeoForged and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
 package net.neoforged.neoforge.unittest;
 
 import java.io.IOException;
@@ -9,26 +14,24 @@ import net.minecraft.resources.Identifier;
 import net.neoforged.neoforge.client.model.obj.ObjGeometry;
 import net.neoforged.neoforge.client.model.obj.ObjTokenizer;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class ObjTests {
-
     public static final ObjGeometry.Settings SETTINGS = new ObjGeometry.Settings(Identifier.fromNamespaceAndPath("unused", "unused"), false, false, false, false, null);
 
     @ParameterizedTest(name = "{0}")
     @CsvSource(delimiter = '|', textBlock = """
-    object_inside_groups.obj|group1,group2|group1,group1/group1cube1,group1/group1cube2,group2,group2/group2cube
-    root_groups.obj|group1cube1,group1cube2,group2cube|group1cube1,group1cube2,group2cube
-    root_objects.obj|group1cube1,group1cube2,group2cube|group1cube1,group1cube2,group2cube
-    object_outside_group.obj|group1cube1,group1cube2,group2cube|group1cube1,group1cube1/theGroup,group1cube2,group2cube
-    """)
+            object_inside_groups.obj|group1,group2|group1,group1/group1cube1,group1/group1cube2,group2,group2/group2cube
+            root_groups.obj|group1cube1,group1cube2,group2cube|group1cube1,group1cube2,group2cube
+            root_objects.obj|group1cube1,group1cube2,group2cube|group1cube1,group1cube2,group2cube
+            object_outside_group.obj|group1cube1,group1cube2,group2cube|group1cube1,group1cube1/theGroup,group1cube2,group2cube
+            """)
     void testObj(String objFile, String expectedGroupNamesStr, String expectedComponentsStr) throws IOException {
         List<String> expectedGroupNames = List.of(expectedGroupNamesStr.split(","));
         List<String> expectedComponents = List.of(expectedComponentsStr.split(","));
 
-        try (InputStream objStream = ObjTests.class.getResourceAsStream("/obj/"+objFile)) {
+        try (InputStream objStream = ObjTests.class.getResourceAsStream("/obj/" + objFile)) {
             Assertions.assertNotNull(objStream, "failed to find obj resource");
             ObjGeometry objGeometry = ObjGeometry.parse(new ObjTokenizer(objStream), SETTINGS);
 
