c63082655521b7a5601c5d294c4da49da39a0048
Argon4W <1516775492@qq.com>
2026-06-02 21:19:57 +0800

    Use imports instead of full-qualified names

--- src/client/java/net/neoforged/neoforge/client/event/FlipFrameEvent.java
+++ src/client/java/net/neoforged/neoforge/client/event/FlipFrameEvent.java
@@ -5,13 +5,15 @@
 
 package net.neoforged.neoforge.client.event;
 
+import com.mojang.blaze3d.TracyFrameCapture;
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.renderer.DynamicUniformStorage;
 import net.neoforged.bus.api.Event;
 import net.neoforged.bus.api.ICancellableEvent;
 import net.neoforged.fml.LogicalSide;
 import net.neoforged.neoforge.common.NeoForge;
 
-/// Fired when the frame [`flipped`][com.mojang.blaze3d.systems.RenderSystem#flipFrame(com.mojang.blaze3d.TracyFrameCapture)].
+/// Fired when the frame [`flipped`][RenderSystem#flipFrame(TracyFrameCapture)].
 ///
 /// This event can be used to manage per-frame GPU resources, or rotate custom [`dynamic uniforms`][DynamicUniformStorage].
 ///
