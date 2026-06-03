7d2726dd1149d48b0cb2deef49fd4e1ea3413ff0
DBotThePony <dbotthepony@yandex.ru>
2026-06-03 09:03:19 +0700

    Update docs spacings

--- src/client/java/net/neoforged/neoforge/client/extensions/common/IClientMobEffectExtensions.java
+++ src/client/java/net/neoforged/neoforge/client/extensions/common/IClientMobEffectExtensions.java
@@ -68,14 +68,14 @@ public interface IClientMobEffectExtensions {
     /**
      * Renders the icon of the specified effect in the player's inventory.
      *
-     * @param instance    The effect instance
-     * @param screen      The effect-rendering screen
-     * @param graphics    The gui graphics
-     * @param x           The x coordinate to render at
-     * @param y           The y coordinate to render at
-     * @param width       Available width of canvas to render in
-     * @param height      Available height of canvas to render in
-     * @param color       Color multiplicator of the icon
+     * @param instance The effect instance
+     * @param screen   The effect-rendering screen
+     * @param graphics The gui graphics
+     * @param x        The x coordinate to render at
+     * @param y        The y coordinate to render at
+     * @param width    Available width of canvas to render in
+     * @param height   Available height of canvas to render in
+     * @param color    Color multiplicator of the icon
      * @return true to prevent default rendering, false otherwise
      */
     default boolean extractInventoryIcon(MobEffectInstance instance, AbstractContainerScreen<?> screen, GuiGraphicsExtractor graphics, int x, int y, int width, int height, int color) {
@@ -138,17 +138,17 @@ public interface IClientMobEffectExtensions {
      * Renders the icon of the specified effect on the player's HUD.
      * This can be used to render icons from your own texture sheet.
      *
-     * @param instance    The effect instance
-     * @param gui         The gui
-     * @param guiGraphics The gui graphics
-     * @param x           The x coordinate
-     * @param y           The y coordinate
-     * @param width       Available width of canvas to render in
-     * @param height      Available height of canvas to render in
-     * @param color       Color multiplicator of the icon
+     * @param instance The effect instance
+     * @param gui      The gui
+     * @param graphics The gui graphics
+     * @param x        The x coordinate
+     * @param y        The y coordinate
+     * @param width    Available width of canvas to render in
+     * @param height   Available height of canvas to render in
+     * @param color    Color multiplicator of the icon
      * @return true to prevent default rendering, false otherwise
      */
-    default boolean extractGuiIcon(MobEffectInstance instance, Gui gui, GuiGraphicsExtractor guiGraphics, int x, int y, int width, int height, int color) {
-        return renderGuiIcon(instance, gui, guiGraphics, x - 3, y - 3, 0f, ARGB.alpha(color) / 255f); // -3 for renderGuiIcon() to receive improperly aligned arguments to not break existing mods
+    default boolean extractGuiIcon(MobEffectInstance instance, Gui gui, GuiGraphicsExtractor graphics, int x, int y, int width, int height, int color) {
+        return renderGuiIcon(instance, gui, graphics, x - 3, y - 3, 0f, ARGB.alpha(color) / 255f); // -3 for renderGuiIcon() to receive improperly aligned arguments to not break existing mods
     }
 }
