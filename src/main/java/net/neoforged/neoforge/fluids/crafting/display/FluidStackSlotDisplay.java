9a370a9284d03f6a74606d12ad6a89fae1f992fe
Sara Freimer <sara@freimer.com>
2026-07-10 16:20:37 -0500

    Update FluidStackSlotDisplay to keep track of a FluidStackTemplate to mirror SlotDisplay.ItemStackSlotDisplay

--- src/main/java/net/neoforged/neoforge/fluids/crafting/display/FluidStackSlotDisplay.java
+++ src/main/java/net/neoforged/neoforge/fluids/crafting/display/FluidStackSlotDisplay.java
@@ -14,18 +14,18 @@ import net.minecraft.util.context.ContextMap;
 import net.minecraft.world.item.crafting.display.DisplayContentsFactory;
 import net.minecraft.world.item.crafting.display.SlotDisplay;
 import net.neoforged.neoforge.common.NeoForgeMod;
-import net.neoforged.neoforge.fluids.FluidStack;
+import net.neoforged.neoforge.fluids.FluidStackTemplate;
 
 /**
  * Slot display for a given fluid stack, including fluid amount and data components.
  *
  * @param stack The fluid stack to be displayed.
  */
-public record FluidStackSlotDisplay(FluidStack stack) implements SlotDisplay {
-    public static final MapCodec<FluidStackSlotDisplay> MAP_CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(FluidStack.CODEC.fieldOf("fluid").forGetter(FluidStackSlotDisplay::stack))
-            .apply(instance, FluidStackSlotDisplay::new));
+public record FluidStackSlotDisplay(FluidStackTemplate stack) implements SlotDisplay {
+    public static final MapCodec<FluidStackSlotDisplay> MAP_CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
+            FluidStackTemplate.CODEC.fieldOf("fluid").forGetter(FluidStackSlotDisplay::stack)).apply(instance, FluidStackSlotDisplay::new));
     public static final StreamCodec<RegistryFriendlyByteBuf, FluidStackSlotDisplay> STREAM_CODEC = StreamCodec.composite(
-            FluidStack.STREAM_CODEC, FluidStackSlotDisplay::stack, FluidStackSlotDisplay::new);
+            FluidStackTemplate.STREAM_CODEC, FluidStackSlotDisplay::stack, FluidStackSlotDisplay::new);
 
     @Override
     public SlotDisplay.Type<FluidStackSlotDisplay> type() {
@@ -34,19 +34,6 @@ public record FluidStackSlotDisplay(FluidStack stack) implements SlotDisplay {
 
     @Override
     public <T> Stream<T> resolve(ContextMap context, DisplayContentsFactory<T> factory) {
-        return switch (factory) {
-            case ForFluidStacks<T> fluids -> Stream.of(fluids.forStack(stack));
-            default -> Stream.empty();
-        };
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        if (this == other) {
-            return true;
-        } else {
-            return other instanceof FluidStackSlotDisplay fluidStackDisplay
-                    && FluidStack.matches(this.stack, fluidStackDisplay.stack);
-        }
+        return factory instanceof ForFluidStacks<T> stacks ? Stream.of(stacks.forStack(stack.create())) : Stream.empty();
     }
 }
