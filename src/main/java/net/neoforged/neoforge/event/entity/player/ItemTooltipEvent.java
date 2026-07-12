9d94532ab1b83ddfc1c3db3838204ada7fb6681c
Apex <29412632+ApexModder@users.noreply.github.com>
2026-07-12 22:24:39 +0100

    [26.2] Add missing display property to `ItemTooltipEvent` (#3293)

--- src/main/java/net/neoforged/neoforge/event/entity/player/ItemTooltipEvent.java
+++ src/main/java/net/neoforged/neoforge/event/entity/player/ItemTooltipEvent.java
@@ -11,6 +11,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item.TooltipContext;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.component.TooltipDisplay;
 import org.jspecify.annotations.Nullable;
 
 public class ItemTooltipEvent extends PlayerEvent {
@@ -18,17 +19,19 @@ public class ItemTooltipEvent extends PlayerEvent {
     private final ItemStack itemStack;
     private final List<Component> toolTip;
     private final TooltipContext context;
+    private final TooltipDisplay display;
 
     /**
      * This event is fired in {@link ItemStack#getTooltipLines(TooltipContext, Player, TooltipFlag)}, which in turn is called from its respective GUIContainer.
      * Tooltips are also gathered with a null player during startup by {@link Minecraft#createSearchTrees()}.
      */
-    public ItemTooltipEvent(ItemStack itemStack, @Nullable Player player, List<Component> list, TooltipFlag flags, TooltipContext context) {
+    public ItemTooltipEvent(ItemStack itemStack, @Nullable Player player, List<Component> list, TooltipFlag flags, TooltipContext context, TooltipDisplay display) {
         super(player);
         this.itemStack = itemStack;
         this.toolTip = list;
         this.flags = flags;
         this.context = context;
+        this.display = display;
     }
 
     /**
@@ -66,5 +69,10 @@ public class ItemTooltipEvent extends PlayerEvent {
      */
     public TooltipContext getContext() {
         return context;
+    }
+
+    /// Used to determine what components on the item tooltip should be hidden
+    public TooltipDisplay getDisplay() {
+        return display;
     }
 }
