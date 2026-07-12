0b221360dda2d5b1ca991f267bc6a09e6045f30c
_melikeymen_ <vahapkoksal10@gmail.com>
2026-07-12 18:14:02 +0300

    Enhance DeferredRegistryTest with convenience methods
    Added new convenience methods for item and block item registration in DeferredRegistryTest.

--- tests/src/main/java/net/neoforged/neoforge/oldtest/DeferredRegistryTest.java
+++ tests/src/main/java/net/neoforged/neoforge/oldtest/DeferredRegistryTest.java
@@ -53,20 +53,28 @@ public class DeferredRegistryTest {
     private static final DeferredRegister<Custom> CUSTOMS = DeferredRegister.create(CUSTOM_REGISTRY_KEY, MODID);
     private static final DeferredRegister<Object> DOESNT_EXIST_REG = DeferredRegister.create(Identifier.fromNamespaceAndPath(MODID, "doesnt_exist"), MODID);
     private static final DeferredRegister<RecipeType<?>> RECIPE_TYPES = DeferredRegister.create(BuiltInRegistries.RECIPE_TYPE, MODID);
-    // Vanilla Registry - filled directly after all RegistryEvent.Register events are fired
     private static final DeferredRegister<PosRuleTestType<?>> POS_RULE_TEST_TYPES = DeferredRegister.create(Registries.POS_RULE_TEST, MODID);
 
     private static final DeferredBlock<Block> BLOCK = BLOCKS.registerSimpleBlock("test", props -> props.mapColor(MapColor.STONE));
     private static final DeferredHolder<DataComponentType<?>, DataComponentType<Integer>> COMPONENT_TYPE = COMPONENTS.registerComponentType("test", builder -> builder.persistent(Codec.INT));
-    private static final DeferredItem<BlockItem> ITEM = ITEMS.registerSimpleBlockItem(BLOCK);
+    private static final DeferredItem<BlockItem> ITEM_STANDARD = ITEMS.registerSimpleBlockItem(BLOCK);
     private static final DeferredItem<Item> ITEM_WITH_COMPONENT = ITEMS.registerItem("test_with_component", properties -> new Item(properties.component(COMPONENT_TYPE.get(), 3)));
+
+    // New convenience methods
+    private static final DeferredItem<Item> ITEM_SHORT = ITEMS.registerItem("test_item_short", Item::new);
+    private static final DeferredItem<BlockItem> BLOCK_ITEM_FROM_HOLDER = ITEMS.registerBlockItem(BLOCK);
+    private static final DeferredItem<BlockItem> BLOCK_ITEM_FROM_HOLDER_WITH_PROPS = ITEMS.registerBlockItem(BLOCK, () -> new Item.Properties().stacksTo(32));
+    private static final DeferredItem<BlockItem> BLOCK_ITEM_FROM_HOLDER_WITH_OPERATOR = ITEMS.registerBlockItem(BLOCK, props -> props.stacksTo(64).fireResistant());
+    private static final DeferredItem<BlockItem> BLOCK_ITEM_WITH_NAME = ITEMS.registerBlockItem("test_block_item_with_name", BLOCK::get);
+    private static final DeferredItem<BlockItem> BLOCK_ITEM_WITH_NAME_AND_PROPS = ITEMS.registerBlockItem("test_block_item_with_name_props", BLOCK::get, () -> new Item.Properties().stacksTo(16));
+    private static final DeferredItem<BlockItem> BLOCK_ITEM_WITH_NAME_AND_OPERATOR = ITEMS.registerBlockItem("test_block_item_with_name_operator", BLOCK::get, props -> props.stacksTo(1).fireResistant());
+    private static final DeferredItem<CustomBlockItem> CUSTOM_BLOCK_ITEM = ITEMS.registerCustomBlockItem("test_custom_block_item", CustomBlockItem::new);
+    private static final DeferredItem<CustomBlockItem> CUSTOM_BLOCK_ITEM_WITH_PROPS = ITEMS.registerCustomBlockItem("test_custom_block_item_props", CustomBlockItem::new, () -> new Item.Properties().stacksTo(8));
+
     private static final DeferredHolder<Custom, Custom> CUSTOM = CUSTOMS.register("test", () -> new Custom() {});
-    // Should never be created as the registry doesn't exist - this should silently fail and remain empty
     private static final DeferredHolder<Object, Object> DOESNT_EXIST = DOESNT_EXIST_REG.register("test", Object::new);
     private static final DeferredHolder<RecipeType<?>, RecipeType<?>> RECIPE_TYPE = RECIPE_TYPES.register("test", () -> new RecipeType<>() {});
     private static final DeferredHolder<PosRuleTestType<?>, PosRuleTestType<?>> POS_RULE_TEST_TYPE = POS_RULE_TEST_TYPES.register("test", () -> () -> null);
-
-//    private static final TagKey<Custom> CUSTOM_TAG_KEY = CUSTOMS.createOptionalTagKey("test_tag", Set.of(CUSTOM));
     private static final Registry<Custom> CUSTOM_REG = CUSTOMS.makeRegistry(builder -> builder.onAdd((owner, id, key, obj) -> LOGGER.info("Custom Added: " + id + " " + obj.foo())));
 
     public DeferredRegistryTest(IEventBus modBus) {
@@ -77,32 +85,52 @@ public class DeferredRegistryTest {
         RECIPE_TYPES.register(modBus);
         POS_RULE_TEST_TYPES.register(modBus);
         modBus.addListener(this::gatherData);
-
         NeoForge.EVENT_BUS.addListener(this::serverStarted);
         modBus.addListener(this::addCreative);
     }
 
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS)
-            event.accept(ITEM);
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ITEM_STANDARD);
+            event.accept(ITEM_SHORT);
+            event.accept(BLOCK_ITEM_FROM_HOLDER);
+            event.accept(BLOCK_ITEM_FROM_HOLDER_WITH_PROPS);
+            event.accept(BLOCK_ITEM_WITH_NAME);
+            event.accept(CUSTOM_BLOCK_ITEM);
+        }
     }
 
     public void serverStarted(ServerStartedEvent event) {
-        // Validate all the RegistryObjects are filled / not filled
+        LOGGER.info("=== Testing DeferredRegister Convenience Methods ===");
+
         BLOCK.get();
-        ITEM.get();
         COMPONENT_TYPE.get();
-        CUSTOM.get();
-        if (DOESNT_EXIST.isBound())
+        ITEM_STANDARD.get();
+        ITEM_WITH_COMPONENT.get();
+
+        ITEM_SHORT.get();
+        BLOCK_ITEM_FROM_HOLDER.get();
+        BLOCK_ITEM_FROM_HOLDER_WITH_PROPS.get();
+        BLOCK_ITEM_FROM_HOLDER_WITH_OPERATOR.get();
+        BLOCK_ITEM_WITH_NAME.get();
+        BLOCK_ITEM_WITH_NAME_AND_PROPS.get();
+        BLOCK_ITEM_WITH_NAME_AND_OPERATOR.get();
+        CUSTOM_BLOCK_ITEM.get();
+        CUSTOM_BLOCK_ITEM_WITH_PROPS.get();
+
+        if (DOESNT_EXIST.isBound()) {
             throw new IllegalStateException("DeferredRegistryTest#DOESNT_EXIST should not be present!");
+        }
+
         RECIPE_TYPE.get();
-        //POS_RULE_TEST_TYPE.get();
-        //PLACED_FEATURE.get();
+        POS_RULE_TEST_TYPE.get();
+        CUSTOM.get();
+
+        LOGGER.info("=== All DeferredRegistryTest validations passed! ===");
     }
 
     public void gatherData(GatherDataEvent.Client event) {
         DataGenerator gen = event.getGenerator();
-
         gen.addProvider(true, new ModelProvider(gen.getPackOutput(), MODID) {
             @Override
             protected void registerModels(BlockModelGenerators blockModels, ItemModelGenerators itemModels) {
@@ -114,6 +142,12 @@ public class DeferredRegistryTest {
     public static class Custom {
         public String foo() {
             return this.getClass().getName();
+        }
+    }
+
+    public static class CustomBlockItem extends BlockItem {
+        public CustomBlockItem(Item.Properties properties) {
+            super(BLOCK.get(), properties);
         }
     }
 }
