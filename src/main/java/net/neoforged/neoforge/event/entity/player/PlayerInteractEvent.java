3be2d428ad46dbe766aa48362ec05b868db15380
Dennis C <11262040+XFactHD@users.noreply.github.com>
2026-07-23 22:21:02 +0200

    Update src/main/java/net/neoforged/neoforge/event/entity/player/PlayerInteractEvent.java
    Co-authored-by: sciwhiz12 <sciwhiz12@gmail.com>

--- src/main/java/net/neoforged/neoforge/event/entity/player/PlayerInteractEvent.java
+++ src/main/java/net/neoforged/neoforge/event/entity/player/PlayerInteractEvent.java
@@ -74,11 +74,9 @@ public abstract class PlayerInteractEvent extends PlayerEvent {
             return target;
         }
 
-        /// Returns the local interaction position. This is a 3D vector, where (0, 0, 0) is centered exactly at the
+        /// {@return the local interaction position} This is a 3D vector, where (0, 0, 0) is centered exactly at the
         /// center of the entity's bounding box at their feet. This means the X and Z values will be in the range
-        /// [-width / 2, width / 2] while Y values will be in the range [0, height]
-        ///
-        /// @return The local position
+        /// [-width / 2, width / 2] while Y values will be in the range [0, height].
         public Vec3 getLocation() {
             return location;
         }
