6db768261025469291cdc3d4608ce78ad7856d26
sciwhiz12 <arnoldnunag12@gmail.com>
2026-08-06 23:21:44 +0800

    Use gradleProperty provider instead of Project#findProperty

--- buildSrc/src/main/java/net/neoforged/neodev/NeoDevPlugin.java
+++ buildSrc/src/main/java/net/neoforged/neodev/NeoDevPlugin.java
@@ -423,8 +423,7 @@ public class NeoDevPlugin implements Plugin<Project> {
 
             // This is true by default (see gradle.properties), and needs to be disabled explicitly when building (see release.yml).
             String installerDebugProperty = "include_artifacts_in_installer";
-            Object installerDebugPropertyValue = project.findProperty(installerDebugProperty);
-            if (installerDebugPropertyValue != null && Boolean.parseBoolean(installerDebugPropertyValue.toString())) {
+            if (project.getProviders().gradleProperty(installerDebugProperty).map(Boolean::parseBoolean).getOrElse(false)) {
                 task.from(universalJar.flatMap(AbstractArchiveTask::getArchiveFile), spec -> {
                     spec.into(String.format("/maven/net/neoforged/neoforge/%s/", neoForgeVersion.get()));
                     spec.rename(name -> String.format("neoforge-%s-universal.jar", neoForgeVersion.get()));
