0a38a8a4ba83ccd10225e4a60b481370937f54ed
sciwhiz12 <sciwhiz12@gmail.com>
2026-08-08 19:28:29 +0700

    Rephrase docs

--- src/client/java/net/neoforged/neoforge/client/extensions/common/IClientMobEffectExtensions.java
+++ src/client/java/net/neoforged/neoforge/client/extensions/common/IClientMobEffectExtensions.java
@@ -26,9 +26,8 @@ public interface IClientMobEffectExtensions {
         return ClientExtensionsManager.MOB_EFFECT_EXTENSIONS.getOrDefault(effect, DEFAULT);
     }
 
-    /// Queries whether the given effect should be shown in the player's inventory.
-    ///
-    /// By default, this returns `true`
+    /// {@return whether the given effect should be shown in the player's inventory}
+    /// By default, this returns `true`.
     default boolean isVisibleInInventory(MobEffectInstance instance) {
         return true;
     }
@@ -36,11 +35,13 @@ public interface IClientMobEffectExtensions {
     /// Queries whether the given effect should be shown in the HUD.
     ///
     /// By default, this returns `true`
-    default boolean isVisibleInGui(MobEffectInstance instance) {
+    /// {@return whether the given effect should be shown in the HUD}
+    /// By default, this returns `true`.
         return true;
     }
 
     /// Renders the icon of the specified effect in the player's inventory.
+    ///
     /// @param instance The effect instance
     /// @param screen   The effect-rendering screen
     /// @param graphics The gui graphics
@@ -49,26 +50,28 @@ public interface IClientMobEffectExtensions {
     /// @param width    Available width of canvas to render in
     /// @param height   Available height of canvas to render in
     /// @param color    Color multiplicator of the icon
-    /// @return true to prevent default rendering, false otherwise
+    /// @return `true` to prevent default rendering, `false` otherwise
     default boolean extractInventoryIcon(MobEffectInstance instance, AbstractContainerScreen<?> screen, GuiGraphicsExtractor graphics, int x, int y, int width, int height, int color) {
         return false;
     }
 
     /// Renders the text of the specified effect in the player's inventory.
+	///
     /// @param instance    The effect instance
     /// @param screen      The effect-rendering screen
     /// @param graphics    The gui graphics
     /// @param x           The x coordinate
     /// @param y           The y coordinate
-    /// @param canvasWidth Available pixels, rendering anything wider than this amount of pixels may overlap with other GUI elements or go out of bounds
+    /// @param canvasWidth Available pixels, where rendering anything wider than this amount of pixels may overlap with other GUI elements or go out of bounds
     /// @param color       Desired color of text, serving as a hint of best color to use for it to be distinct from background
-    /// @return true to prevent default rendering, false otherwise
+    /// @return `true` to prevent default rendering, `false` otherwise
     default boolean extractInventoryText(MobEffectInstance instance, AbstractContainerScreen<?> screen, GuiGraphicsExtractor graphics, int x, int y, int canvasWidth, int color) {
         return false;
     }
 
     /// Renders the icon of the specified effect on the player's HUD.
     /// This can be used to render icons from your own texture sheet.
+    ///
     /// @param instance The effect instance
     /// @param hud      The HUD
     /// @param graphics The gui graphics
@@ -77,7 +80,7 @@ public interface IClientMobEffectExtensions {
     /// @param width    Available width of canvas to render in
     /// @param height   Available height of canvas to render in
     /// @param color    Color multiplicator of the icon
-    /// @return true to prevent default rendering, false otherwise
+    /// @return `true` to prevent default rendering, `false` otherwise
     default boolean extractHudIcon(MobEffectInstance instance, Hud hud, GuiGraphicsExtractor graphics, int x, int y, int width, int height, int color) {
         return false;
     }
