fb513ea40b3d07abea00e1da2a122449234208e0
XFactHD <11262040+XFactHD@users.noreply.github.com>
2026-08-10 00:34:51 +0200

    Resolve various client rejects

--- src/client/java/net/neoforged/neoforge/client/event/RegisterRenderPipelinesEvent.java
+++ src/client/java/net/neoforged/neoforge/client/event/RegisterRenderPipelinesEvent.java
@@ -5,8 +5,8 @@
 
 package net.neoforged.neoforge.client.event;
 
-import com.mojang.blaze3d.pipeline.RenderPipeline;
-import java.util.function.Consumer;
+import com.mojang.renderpearl.api.pipeline.RenderPipeline;
+import net.minecraft.client.renderer.oit.OitPipelineSet;
 import net.neoforged.bus.api.Event;
 import net.neoforged.bus.api.ICancellableEvent;
 import net.neoforged.fml.LogicalSide;
@@ -22,19 +22,39 @@ import org.jetbrains.annotations.ApiStatus;
  * <p>This event is fired on the mod-specific event bus, only on the {@linkplain LogicalSide#CLIENT logical client}.</p>
  */
 public class RegisterRenderPipelinesEvent extends Event implements IModBusEvent {
-    private final Consumer<RenderPipeline> registrar;
+    private final Registrar registrar;
 
     @ApiStatus.Internal
-    public RegisterRenderPipelinesEvent(Consumer<RenderPipeline> registrar) {
+    public RegisterRenderPipelinesEvent(Registrar registrar) {
         this.registrar = registrar;
     }
 
-    /**
-     * Registers a {@link RenderPipeline}
-     *
-     * @param pipeline a pipeline
-     */
+    /// Registers a required [RenderPipeline].
+    ///
+    /// @param pipeline a render pipeline
     public void registerPipeline(RenderPipeline pipeline) {
-        registrar.accept(pipeline);
+        registrar.register(pipeline, false);
+    }
+
+    /// Registers an optional [RenderPipeline].
+    ///
+    /// @param pipeline a render pipeline
+    public void registerOptionalPipeline(RenderPipeline pipeline) {
+        registrar.register(pipeline, false);
+    }
+
+    /// Registers an OIT [RenderPipeline] set.
+    ///
+    /// @param pipelineSet an OIT pipeline set
+    public void registerOitPipelineSet(OitPipelineSet pipelineSet) {
+        registrar.register(pipelineSet.depthBoundsPipeline(), false);
+        registrar.register(pipelineSet.transmittancePipeline(), false);
+        registrar.register(pipelineSet.accumulatePipeline(), false);
+    }
+
+    @ApiStatus.Internal
+    @FunctionalInterface
+    public interface Registrar {
+        void register(RenderPipeline pipeline, boolean optional);
     }
 }
