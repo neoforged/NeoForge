2f024c02c6034da765497bc85ffd27475075ee28
IchHabeHunger54 <kingdavidaut@gmail.com>
2026-08-29 18:17:35 +0200

    apply sci's comments

--- src/client/java/net/neoforged/neoforge/client/event/SelectMusicEvent.java
+++ src/client/java/net/neoforged/neoforge/client/event/SelectMusicEvent.java
@@ -14,21 +14,21 @@ import net.neoforged.fml.LogicalSide;
 import net.neoforged.neoforge.common.NeoForge;
 import org.jspecify.annotations.Nullable;
 
-/// Fired when the [net.minecraft.client.sounds.MusicManager] checks what situational music should be used. This fires before the music begins playing.<br>
+/// Fired when the [net.minecraft.client.sounds.MusicManager] checks what situational music should be used. This fires before the music begins playing.
 /// If the music is set to `null` by a modder, it will cancel any music that was already playing.
 ///
-/// Note that the higher priority you make your event listener, the earlier the music will be set.<br>
+/// Note that the higher priority you make your event listener, the earlier the music will be set.
 /// Because of this, if you want your music to take precedence over others (perhaps you want to have seperate nighttime music for a biome for instance) then you may want it to have a lower priority.
 ///
 /// To make your music instantly play rather than waiting for the playing music to stop, set the music to one that [is set to replace the current music.][Music#replaceCurrentMusic()]
 ///
 /// Higher priorities would likely be better suited for biome-based or dimension-based musics, whereas lower priority is likely good for specific structures or situations.
 ///
-/// This event is [cancellable][ICancellableEvent].<br>
-/// Cancelling the event **does not prevent the music from being changed**, rather it stops propagation to further event handlers, and uses whatever the music was set to in the event.<br>
-/// When cancelling the event, it is therefore preferred to call the [SelectMusicEvent#overrideMusic(Music)][SelectMusicEvent#overrideMusic(Music)] helper with your music, which automatically calls [SelectMusicEvent#setCanceled(boolean)][SelectMusicEvent#setCanceled(boolean)] for you.
+/// This event is [cancellable][ICancellableEvent].
+/// Cancelling the event **does not prevent the music from being changed**; it stops propagation to further event handlers, and uses whatever the music was set to in the event.
+/// It is therefore preferred to call the [SelectMusicEvent#overrideMusic(Music)][SelectMusicEvent#overrideMusic(Music)] helper with your music, which automatically cancels the event for you.
 ///
-/// This event is fired on the [main Forge event bus][NeoForge#EVENT_BUS],<br>
+/// This event is fired on the [main Forge event bus][NeoForge#EVENT_BUS],
 /// only on the [logical client][LogicalSide#CLIENT].
 public class SelectMusicEvent extends Event implements ICancellableEvent {
     private @Nullable Music music;
