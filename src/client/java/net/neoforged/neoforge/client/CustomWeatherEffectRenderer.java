ab08525a28d40afca702fd38ef281e895c6d26bf
XFactHD <11262040+XFactHD@users.noreply.github.com>
2026-09-05 17:06:54 +0200

    Fix custom sky/clouds/weather renderers

--- src/client/java/net/neoforged/neoforge/client/CustomWeatherEffectRenderer.java
+++ src/client/java/net/neoforged/neoforge/client/CustomWeatherEffectRenderer.java
@@ -5,8 +5,11 @@
 
 package net.neoforged.neoforge.client;
 
+import com.mojang.renderpearl.api.commands.RenderPass;
 import net.minecraft.client.Camera;
 import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.WeatherEffectRenderer;
+import net.minecraft.client.renderer.oit.OitStage;
 import net.minecraft.client.renderer.state.level.LevelRenderState;
 import net.minecraft.client.renderer.state.level.WeatherRenderState;
 import net.minecraft.world.phys.Vec3;
@@ -24,20 +27,34 @@ import net.neoforged.neoforge.client.event.RegisterCustomEnvironmentEffectRender
  * @see net.neoforged.neoforge.common.world.NeoForgeEnvironmentAttributes#CUSTOM_WEATHER_EFFECTS
  */
 public interface CustomWeatherEffectRenderer {
-    /**
-     * Renders the snow and rain effects of this dimension.
-     *
-     * @return true to prevent vanilla snow and rain rendering
-     */
-    default boolean renderSnowAndRain(LevelRenderState levelRenderState, WeatherRenderState weatherRenderState, Vec3 camPos) {
+    /// Prepare the geometry and other buffers to be rendered in [#renderSnowAndRain(LevelRenderState, WeatherRenderState, Vec3, RenderPass)]
+    /// or [#renderSnowAndRainOit(LevelRenderState, WeatherRenderState, Vec3, OitStage, RenderPass)].
+    ///
+    /// @see WeatherEffectRenderer#prepare(Vec3, WeatherRenderState)
+    default void prepare(LevelRenderState levelRenderState, WeatherRenderState weatherRenderState, Vec3 camPos) {
+    }
+
+    /// Renders the snow and rain effects of this dimension.
+    ///
+    /// @return true to prevent vanilla snow and rain rendering
+    ///
+    /// @see WeatherEffectRenderer#render(WeatherRenderState, RenderPass)
+    default boolean renderSnowAndRain(LevelRenderState levelRenderState, WeatherRenderState weatherRenderState, Vec3 camPos, RenderPass renderPass) {
         return false;
     }
 
-    /**
-     * Ticks the rain of this dimension.
-     *
-     * @return true to prevent vanilla rain ticking
-     */
+    /// Renders the snow and rain effects of this dimension with order-independent translucency.
+    ///
+    /// @return true to prevent vanilla snow and rain rendering
+    ///
+    /// @see WeatherEffectRenderer#renderOit(OitStage, WeatherRenderState, RenderPass)
+    default boolean renderSnowAndRainOit(LevelRenderState levelRenderState, WeatherRenderState weatherRenderState, Vec3 camPos, OitStage stage, RenderPass renderPass) {
+        return false;
+    }
+
+    /// Ticks the rain of this dimension.
+    ///
+    /// @return true to prevent vanilla rain ticking
     default boolean tickRain(ClientLevel level, long ticks, Camera camera) {
         return false;
     }
