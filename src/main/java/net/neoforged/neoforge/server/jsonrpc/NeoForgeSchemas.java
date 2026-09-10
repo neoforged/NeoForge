2a15339a62e7e72515a6ec9133fdc120f3d6e42e
Minecraftschurli <minecraftschurli@gmail.com>
2026-09-10 23:46:51 +0200

    Add registry sync information to registry query rpc method

--- src/main/java/net/neoforged/neoforge/server/jsonrpc/NeoForgeSchemas.java
+++ src/main/java/net/neoforged/neoforge/server/jsonrpc/NeoForgeSchemas.java
@@ -17,6 +17,7 @@ public final class NeoForgeSchemas {
     static final SchemaComponent<RegistryInfo> REGISTRY_SCHEMA = new SchemaComponent<>(Identifier.fromNamespaceAndPath(NeoForgeMod.MOD_ID, "registry"), Schema.record(RegistryInfo.CODEC)
             .withField("registryName", Schema.STRING_SCHEMA)
             .withField("size", Schema.INT_SCHEMA)
+            .withField("synced", Schema.BOOL_SCHEMA)
             .withField("entries", Schema.STRING_SCHEMA.asArray()));
     static final SchemaComponent<ModRecord> MOD_SCHEMA = new SchemaComponent<>(Identifier.fromNamespaceAndPath(NeoForgeMod.MOD_ID, "mod"), Schema.record(ModRecord.CODEC)
             .withField("modId", Schema.STRING_SCHEMA)
--- src/main/java/net/neoforged/neoforge/server/jsonrpc/RegistryInfo.java
+++ src/main/java/net/neoforged/neoforge/server/jsonrpc/RegistryInfo.java
@@ -8,18 +8,21 @@ package net.neoforged.neoforge.server.jsonrpc;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import java.util.List;
+import java.util.Optional;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
+import net.neoforged.neoforge.registries.IRegistryExtension;
 
-public record RegistryInfo(Identifier registryName, long size, List<Identifier> entries) {
+public record RegistryInfo(Identifier registryName, long size, Optional<Boolean> synced, List<Identifier> entries) {
     public static final Codec<RegistryInfo> CODEC = RecordCodecBuilder.create(inst -> inst.group(
             Identifier.CODEC.fieldOf("registryName").forGetter(RegistryInfo::registryName),
             Codec.LONG.fieldOf("size").forGetter(RegistryInfo::size),
+            Codec.BOOL.optionalFieldOf("synced").forGetter(RegistryInfo::synced),
             Identifier.CODEC.listOf().fieldOf("entries").forGetter(RegistryInfo::entries)).apply(inst, RegistryInfo::new));
 
     static RegistryInfo withEntries(HolderLookup.RegistryLookup<?> lookup) {
         List<Identifier> list = lookup.listElementIds().map(ResourceKey::identifier).toList();
-        return new RegistryInfo(lookup.key().identifier(), list.size(), list);
+        return new RegistryInfo(lookup.key().identifier(), list.size(), lookup instanceof IRegistryExtension<?> ext ? Optional.of(ext.doesSync()) : Optional.empty(), list);
     }
 }
